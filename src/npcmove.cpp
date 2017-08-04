214f5c57b416d721e8e6d7bab82f5db1fdaba946
Coolthulhu <Coolthulhu@gmail.com>
2017-08-04 23:30:25 +0200

    Prevent NPCs from attacking zlaves

--- src/npcmove.cpp
+++ src/npcmove.cpp
@@ -770,7 +770,7 @@ void npc::choose_target()
             continue;
         }
 
-        if( att == MATT_FPASSIVE ) {
+        if( att == MATT_FPASSIVE || att == MATT_ZLAVE ) {
             continue;
         }
 
