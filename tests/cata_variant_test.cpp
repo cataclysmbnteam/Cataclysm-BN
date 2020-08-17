bc70d7cc702b19bf6a1f468c44c448d5ec124a50
John Bytheway <jbytheway@gmail.com>
2020-08-17 16:53:42 -0400

    Tests for new cata_variant features
    Test cata_variant::from_string and cata_variant_type_for.

--- tests/cata_variant_test.cpp
+++ tests/cata_variant_test.cpp
@@ -96,6 +96,21 @@ TEST_CASE( "variant_deserialization", "[variant]" )
     CHECK( v == cata_variant( mtype_id( "zombie" ) ) );
 }
 
+TEST_CASE( "variant_from_string" )
+{
+    cata_variant v = cata_variant::from_string( cata_variant_type::mtype_id, "mon_zombie" );
+    CHECK( v == cata_variant( mtype_id( "mon_zombie" ) ) );
+}
+
+TEST_CASE( "variant_type_for", "[variant]" )
+{
+    CHECK( cata_variant_type_for<bool>() == cata_variant_type::bool_ );
+    CHECK( cata_variant_type_for<int>() == cata_variant_type::int_ );
+    CHECK( cata_variant_type_for<skill_id>() == cata_variant_type::skill_id );
+    CHECK( cata_variant_type_for<trait_id>() == cata_variant_type::trait_id );
+    CHECK( cata_variant_type_for<ter_id>() == cata_variant_type::ter_id );
+}
+
 TEST_CASE( "variant_is_valid", "[variant]" )
 {
     // A string_id
