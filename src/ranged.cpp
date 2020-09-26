19ec5b9adffceb9657139d71606ac96a62df47a8
firestorm01x2 <firestorm01x2gmail.com>
2020-09-26 19:45:28 +0300

    Astyle
    Astyle

--- src/ranged.cpp
+++ src/ranged.cpp
@@ -476,8 +476,9 @@ int player::fire_gun( const tripoint &target, int shots, item &gun )
         const vehicle *in_veh = has_effect( effect_on_roof ) ? veh_pointer_or_null( g->m.veh_at(
                                     pos() ) ) : nullptr;
         projectile projectile = make_gun_projectile( gun );
-        if( is_fake() /* And something else. */ )
+        if( is_fake() /* And something else. */ ) {
             projectile.proj_effects.insert( "NO_CRIT" );
+        }
         auto shot = projectile_attack( projectile, pos(), aim, dispersion, this, in_veh );
         curshot++;
 
