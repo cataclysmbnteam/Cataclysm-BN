d34b417e9a466596c8e7ecbba01de51c07431f60
Roy Berube <royberube1965@gmail.com>
2020-12-29 18:28:24 -0700

    Refactor autodrive and not allow unsafe autodrive speed
    Co-authored-by: actual-nh <74678550+actual-nh@users.noreply.github.com>
    Co-authored-by: Anton Burmistrov <Night_Pryanik@mail.ru>
    
    (cherry picked from commit 4b6150778f5a01a33752b1d246d8b5a71da5b849)

--- src/activity_handlers.cpp
+++ src/activity_handlers.cpp
@@ -319,7 +319,6 @@ activity_handlers::do_turn_functions = {
     { ACT_BUTCHER, butcher_do_turn },
     { ACT_BUTCHER_FULL, butcher_do_turn },
     { ACT_TRAVELLING, travel_do_turn },
-    { ACT_AUTODRIVE, drive_do_turn },
     { ACT_FIELD_DRESS, butcher_do_turn },
     { ACT_SKIN, butcher_do_turn },
     { ACT_QUARTER, butcher_do_turn },
@@ -2976,39 +2975,6 @@ void activity_handlers::adv_inventory_do_turn( player_activity *, player *p )
 {
     p->cancel_activity();
     create_advanced_inv();
-}
-
-void activity_handlers::drive_do_turn( player_activity *act, player *p )
-{
-    vehicle *player_veh = veh_pointer_or_null( g->m.veh_at( p->pos() ) );
-    if( !player_veh ) {
-        act->set_to_null();
-        p->cancel_activity();
-        return;
-    }
-    if( p->in_vehicle && p->controlling_vehicle && player_veh->is_autodriving &&
-        !g->u.omt_path.empty() && !player_veh->omt_path.empty() ) {
-        player_veh->do_autodrive();
-        if( g->u.global_omt_location() == g->u.omt_path.back() ) {
-            g->u.omt_path.pop_back();
-        }
-        p->moves = 0;
-    } else {
-        p->add_msg_if_player( m_info, _( "Auto-drive canceled." ) );
-        if( !player_veh->omt_path.empty() ) {
-            player_veh->omt_path.clear();
-        }
-        player_veh->is_autodriving = false;
-        act->set_to_null();
-        p->cancel_activity();
-        return;
-    }
-    if( player_veh->omt_path.empty() ) {
-        act->set_to_null();
-        player_veh->is_autodriving = false;
-        p->add_msg_if_player( m_info, _( "You have reached your destination." ) );
-        p->cancel_activity();
-    }
 }
 
 void activity_handlers::travel_do_turn( player_activity *act, player *p )
