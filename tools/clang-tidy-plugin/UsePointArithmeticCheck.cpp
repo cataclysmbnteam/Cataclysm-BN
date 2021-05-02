afa88c18df669b87106e6cf8905a12cc4f57ccd5
John Bytheway <jbytheway@gmail.com>
2021-05-02 16:43:55 -0400

    (partial cherry-pick) Port clang-tidy-plugin to LLVM 12
    (cherry picked from commit 9eb6d8d58aab1d0942eafc8e174346aadf4ef62f)

--- tools/clang-tidy-plugin/UsePointArithmeticCheck.cpp
+++ tools/clang-tidy-plugin/UsePointArithmeticCheck.cpp
@@ -93,8 +93,9 @@ struct ExpressionComponent {
         coefficient( 1 ),
         isMember( false ),
         isArrowRef( false ),
-        isTripoint( false )
-    {}
+        isTripoint( false ) {
+        complete_init();
+    }
 
     ExpressionComponent( const MatchFinder::MatchResult &Result, const Expr *E,
                          const CXXRecordDecl *MemberOf, bool IsArrowRef ) :
@@ -102,8 +103,15 @@ struct ExpressionComponent {
         coefficient( 1 ),
         isMember( true ),
         isArrowRef( IsArrowRef ),
-        isTripoint( MemberOf->getName() == "tripoint" )
-    {}
+        isTripoint( MemberOf->getName() == "tripoint" ) {
+        complete_init();
+    }
+
+    void complete_init() {
+        if( StringRef( objectRef ).endswith( "->" ) ) {
+            objectRef.erase( objectRef.end() - 2, objectRef.end() );
+        }
+    }
 
     std::string objectRef;
     int coefficient;
@@ -264,7 +272,7 @@ static std::vector<ExpressionComponent> decomposeExpr( const Expr *E, const std:
         }
         case Stmt::MaterializeTemporaryExprClass: {
             const MaterializeTemporaryExpr *Temp = cast<MaterializeTemporaryExpr>( E );
-            return decomposeExpr( Temp->GetTemporaryExpr(), Member, Result );
+            return decomposeExpr( Temp->getSubExpr(), Member, Result );
         }
         case Stmt::MemberExprClass: {
             const MemberExpr *MemEx = cast<MemberExpr>( E );
@@ -368,11 +376,11 @@ static void appendCoefficient( std::string &Result, int coefficient )
     }
 }
 
-static std::string writeConstructor( const std::string &TypeName,
+static std::string writeConstructor( const StringRef TypeName,
                                      const std::set<std::string> &Keys,
                                      std::map<std::string, std::string> Args )
 {
-    std::string Result = TypeName + "( ";
+    std::string Result = TypeName.str() + "( ";
     bool AnyLeftovers = false;
     for( const auto &Key : Keys ) {
         std::string &Leftover = Args[Key];
@@ -610,7 +618,7 @@ static void CheckConstructor( UsePointArithmeticCheck &Check,
         Keys.insert( Component.first );
     }
 
-    std::string TargetTypeName;
+    StringRef TargetTypeName;
     if( Leftovers["z"].empty() ) {
         TargetTypeName = "point";
         Keys.erase( "z" );
