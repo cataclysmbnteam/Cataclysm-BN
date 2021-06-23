1bf82daaa326a5d2d423ba348c30ce0aad4ce2d4
Coolthulhu <Coolthulhu@gmail.com>
2021-06-23 10:39:36 +0200

    Passing tests for tokenizing

--- src/drop_token.cpp
+++ src/drop_token.cpp
@@ -1,3 +1,4 @@
+#include "calendar.h"
 #include "drop_token.h"
 
 namespace drop_token
@@ -6,12 +7,14 @@ namespace drop_token
 item_drop_token make_next()
 {
     // TODO: Implement properly
-    static time_point last_turn;
+    static time_point last_turn = time_point::from_turn( -1 );
     static int last_drop = 0;
     if( calendar::turn != last_turn ) {
         last_turn = calendar::turn;
         last_drop = 0;
     }
+
+    last_drop++;
 
     return item_drop_token{ calendar::turn, last_drop, last_drop };
 }
