48d8efe0928f86bd887ba84d6214da4181464a6b
olanti-p <olanti-p@yandex.ru>
2021-06-30 01:12:22 +0300

    Fix clang-tidy readability-inconsistent-declaration-parameter-name

--- src/fragment_cloud.h
+++ src/fragment_cloud.h
@@ -4,13 +4,13 @@
 
 enum class quadrant;
 
-float shrapnel_calc( const float &initial,
-                     const float &cloud,
+float shrapnel_calc( const float &intensity,
+                     const float &last_obstacle,
                      const int &distance );
-bool shrapnel_check( const float &cloud, const float &intensity );
-void update_fragment_cloud( float &update, const float &new_value, quadrant );
-float accumulate_fragment_cloud( const float &cumulative_cloud,
-                                 const float &current_cloud,
+bool shrapnel_check( const float &obstacle, const float &intensity );
+void update_fragment_cloud( float &output, const float &new_intensity, quadrant );
+float accumulate_fragment_cloud( const float &cumulative_obstacle,
+                                 const float &current_obstacle,
                                  const int &distance );
 
 #endif // CATA_SRC_FRAGMENT_CLOUD_H
