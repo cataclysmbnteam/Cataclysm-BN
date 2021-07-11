e9e20e1f28f6bb5c4629fb65b0c5a74cada27518
Trioct <dbaumgartner21@gmail.com>
2021-07-11 09:25:25 -0500

    Fix overmap ui cursor handling with tiles enabled
    Co-authored-by: Curtis Merrill <curtis.r.merrill@gmail.com>
    (cherry picked from commit 893e4a34f9b9d53bbcb27a859b799f90c0153601)

--- src/game.cpp
+++ src/game.cpp
@@ -3290,6 +3290,11 @@ shared_ptr_fast<ui_adaptor> game::create_or_get_main_ui_adaptor()
     return ui;
 }
 
+void game::swap_main_ui_adaptor( weak_ptr_fast<ui_adaptor> &ui )
+{
+    main_ui_adaptor.swap( ui );
+}
+
 void game::invalidate_main_ui_adaptor() const
 {
     shared_ptr_fast<ui_adaptor> ui = main_ui_adaptor.lock();
