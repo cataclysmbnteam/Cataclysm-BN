f5562e1787467e015c598c38de811983a18ceee5
John Bytheway <52664+jbytheway@users.noreply.github.com>
2021-09-14 13:31:46 -0400

    Allow random number of overmaps in mutable special
    Rather than always having a fixed number of overmaps for a particular
    rule during placement, allow a random number.  Currently this can only
    be a poisson-distributed number (or a fixed value, as was already
    supported).

---
+++ tests/distribution_test.cpp
@@ -0,0 +1,16 @@
+#include "catch/catch.hpp"
+
+#include "distribution.h"
+#include "json.h"
+
+TEST_CASE( "poisson_distribution", "[distribution]" )
+{
+    std::string s = R"({ "poisson": 10 })";
+    std::istringstream is( s );
+    JsonIn jin( is );
+    int_distribution d;
+    d.deserialize( jin );
+
+    CHECK( d.description() == "Poisson(10)" );
+    CHECK( d.minimum() == 0 );
+}
