c124364e977e4a2f674db57560025fda61280687
Chaosvolt <chaosvolt@users.noreply.github.com>
2021-10-12 13:10:13 -0500

    Persuade NPCs to stop treating helmets as godly superweapons (#1029)
    * Persuade NPCs to stop treating helmets as godly superweapons
    
    * Okay fine we can do this hardcore
    
    * Now I think I've got it

--- src/melee.cpp
+++ src/melee.cpp
@@ -2220,8 +2220,14 @@ double player::weapon_value( const item &weap, int ammo ) const
     const double more = std::max( val_gun, val_melee );
     const double less = std::min( val_gun, val_melee );
 
+    // discourage wielding helmets, but not worn firearms
+    int armor_penalty = 1;
+    if( weap.is_armor() && !weap.is_gun() ) {
+        armor_penalty = 0;
+    }
+
     // A small bonus for guns you can also use to hit stuff with (bayonets etc.)
-    const double my_val = more + ( less / 2.0 );
+    const double my_val = ( more + ( less / 2.0 ) ) * armor_penalty;
     add_msg( m_debug, "%s (%ld ammo) sum value: %.1f", weap.type->get_id().str(), ammo, my_val );
     if( is_wielding( weap ) ) {
         cached_info.emplace( "weapon_value", my_val );
