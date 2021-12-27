e369806ee0b88a948cf9c0d94d45bb1769fb9839
Chaosvolt <chaosvolt@users.noreply.github.com>
2021-12-26 20:11:37 -0600

    Allow Mycus characters to use bandages in both menus

--- src/consumption.cpp
+++ src/consumption.cpp
@@ -136,6 +136,7 @@ static const std::string flag_LUPINE( "LUPINE" );
 static const std::string flag_MYCUS_OK( "MYCUS_OK" );
 static const std::string flag_NEGATIVE_MONOTONY_OK( "NEGATIVE_MONOTONY_OK" );
 static const std::string flag_NO_BLOAT( "NO_BLOAT" );
+static const std::string flag_NO_INGEST( "NO_INGEST" );
 static const std::string flag_NO_PARASITES( "NO_PARASITES" );
 static const std::string flag_NO_RELOAD( "NO_RELOAD" );
 static const std::string flag_NUTRIENT_OVERRIDE( "NUTRIENT_OVERRIDE" );
@@ -684,7 +685,8 @@ ret_val<edible_rating> Character::can_eat( const item &food ) const
     }
 
     // For all those folks who loved eating marloss berries.  D:< mwuhahaha
-    if( has_trait( trait_M_DEPENDENT ) && !food.has_flag( flag_MYCUS_OK ) ) {
+    if( has_trait( trait_M_DEPENDENT ) && !food.has_flag( flag_MYCUS_OK ) &&
+        !food.has_flag( flag_NO_INGEST ) ) {
         return ret_val<edible_rating>::make_failure( edible_rating::inedible_mutation,
                 _( "We can't eat that.  It's not right for us." ) );
     }
