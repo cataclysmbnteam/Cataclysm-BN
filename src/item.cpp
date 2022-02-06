d8d6d401515c1817261d5d96711ae52b32f9cb2f
Chaosvolt <chaosvolt@users.noreply.github.com>
2022-02-06 14:03:04 -0600

    Suggestion per feedback

--- src/item.cpp
+++ src/item.cpp
@@ -4441,7 +4441,7 @@ std::string item::tname( unsigned int quantity, bool with_prefix, unsigned int t
             tagtext += _( " (unread)" );
         }
     }
-    if( has_flag( flag_CBM_SCANNED ) ) {
+    if( has_var( "bionics_scanned_by" ) && has_flag( flag_CBM_SCANNED ) ) {
         tagtext += _( " (bionic detected)" );
     }
     if( has_flag( flag_ETHEREAL_ITEM ) ) {
