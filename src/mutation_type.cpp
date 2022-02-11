7778aba0524515859f0c7e0f7c4a85cb446409c6
Coolthulhu <Coolthulhu@gmail.com>
2022-02-11 11:13:13 +0100

    Merge pull request #48094 from anothersimulacrum/trait-opt (#1333)
    Don't copy mutation branches unecessarily
    
    Co-authored-by: Zhilkin Serg <ZhilkinSerg@users.noreply.github.com>

--- src/mutation_type.cpp
+++ src/mutation_type.cpp
@@ -29,7 +29,7 @@ bool mutation_type_exists( const std::string &id )
 std::vector<trait_id> get_mutations_in_type( const std::string &id )
 {
     std::vector<trait_id> ret;
-    for( auto it : mutation_branch::get_all() ) {
+    for( const mutation_branch &it : mutation_branch::get_all() ) {
         if( it.types.find( id ) != it.types.end() ) {
             ret.push_back( it.id );
         }
