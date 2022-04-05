9ea667747bf07b37fbd2587e42620afcd4bd2d84
Chaosvolt <chaosvolt@users.noreply.github.com>
2022-04-05 15:26:25 -0500

    Update bionics.h

--- src/bionics.h
+++ src/bionics.h
@@ -81,7 +81,7 @@ struct bionic_data {
     /**Amount of cut protection offered by this bionic*/
     std::map<bodypart_str_id, int> cut_protec;
     /**Amount of bullet protection offered by this bionic*/
-    std::map<bodypart_str_id, size_t> bullet_protec;
+    std::map<bodypart_str_id, int> bullet_protec;
     /**
      * Body part slots used to install this bionic, mapped to the amount of space required.
      */
