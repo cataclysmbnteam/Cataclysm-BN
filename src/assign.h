250703cbf45b496477cce82a0a0b99238295540e
Chaosvolt <chaosvolt@users.noreply.github.com>
2022-04-12 22:26:16 +0000

    Port over ballistic damage type

--- src/assign.h
+++ src/assign.h
@@ -943,12 +943,6 @@ inline bool assign( const JsonObject &jo, const std::string &name, damage_instan
     // What we'll eventually be returning for the damage instance
     damage_instance out;
 
-    std::string id_err = "no id found";
-    // Grab the id for good error reporting
-    if( jo.has_string( "id" ) ) {
-        id_err = jo.get_string( "id" );
-    }
-
     if( jo.has_array( name ) ) {
         out = load_damage_instance_inherit( jo.get_array( name ), val );
     } else if( jo.has_object( name ) ) {
@@ -975,9 +969,6 @@ inline bool assign( const JsonObject &jo, const std::string &name, damage_instan
         }
 
         if( with_legacy ) {
-            // Give a load warning, it's likely anything loading damage this way
-            // is a gun, and as such is using the wrong damage type
-            debugmsg( "Warning: %s loads damage using legacy methods - damage type may be wrong", id_err );
             out.add_damage( DT_STAB, amount, arpen, 1.0f, dmg_mult );
         }
     }
@@ -1022,10 +1013,6 @@ inline bool assign( const JsonObject &jo, const std::string &name, damage_instan
             dmg_mult = relative.get_float( "prop_damage" );
         }
 
-        // Give a load warning, it's likely anything loading damage this way
-        // is a gun, and as such is using the wrong damage type
-        debugmsg( "Warning: %s loads damage using legacy methods - damage type may be wrong", id_err );
-
         assign_dmg_relative( out, val, damage_instance( DT_STAB, amt, arpen, 1.0f, dmg_mult ), strict );
     } else if( proportional.has_member( name ) || proportional.has_member( "pierce" ) ||
                proportional.has_member( "prop_damage" ) ) {
@@ -1045,10 +1032,6 @@ inline bool assign( const JsonObject &jo, const std::string &name, damage_instan
         if( proportional.has_member( "prop_damage" ) ) {
             dmg_mult = proportional.get_float( "prop_damage" );
         }
-
-        // Give a load warning, it's likely anything loading damage this way
-        // is a gun, and as such is using the wrong damage type
-        debugmsg( "Warning: %s loads damage using legacy methods - damage type may be wrong", id_err );
 
         assign_dmg_proportional( proportional, name, out, val, damage_instance( DT_STAB, amt, arpen, 1.0f,
                                  dmg_mult ), strict );
