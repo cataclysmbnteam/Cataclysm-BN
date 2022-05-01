6909fbab76d8810e25cdfe5cc0b06a503dc7c328
Chaosvolt <chaosvolt@users.noreply.github.com>
2022-04-30 22:35:26 -0500

    Allow using stronger bows at penalty

--- src/ranged.cpp
+++ src/ranged.cpp
@@ -143,6 +143,7 @@ static const std::string flag_PRIMITIVE_RANGED_WEAPON( "PRIMITIVE_RANGED_WEAPON"
 static const std::string flag_PYROMANIAC_WEAPON( "PYROMANIAC_WEAPON" );
 static const std::string flag_RELOAD_AND_SHOOT( "RELOAD_AND_SHOOT" );
 static const std::string flag_RESTRICT_HANDS( "RESTRICT_HANDS" );
+static const std::string flag_STR_DRAW( "STR_DRAW" );
 static const std::string flag_UNDERWATER_GUN( "UNDERWATER_GUN" );
 static const std::string flag_VEHICLE( "VEHICLE" );
 
@@ -858,6 +859,10 @@ int player::fire_gun( const tripoint &target, const int max_shots, item &gun )
         const vehicle *in_veh = has_effect( effect_on_roof ) ? veh_pointer_or_null( g->m.veh_at(
                                     pos() ) ) : nullptr;
         projectile projectile = make_gun_projectile( gun );
+
+        // Damage reduction from insufficient strength, if using a STR_DRAW weapon.
+        projectile.impact.mult_damage( ranged::get_str_draw_penalty( gun, *this ) );
+
         if( has_trait( trait_NORANGEDCRIT ) ) {
             projectile.add_effect( ammo_effect_NO_CRIT );
         }
@@ -971,6 +976,18 @@ int throw_cost( const player &c, const item &to_throw )
     move_cost *= c.mutation_value( "attackcost_modifier" );
 
     return std::max( 25, move_cost );
+}
+
+float ranged::get_str_draw_penalty( const item &it, const Character &p )
+{
+    // We only care if weapon has STR_DRAW, and that the user is weaker than required strength.
+    // Also avoid dividing by zero, and skip if we'd just get a result of 1 anyway.
+    if( !it.has_flag( flag_STR_DRAW ) || p.get_str() >= it.get_min_str() || it.get_min_str() <= 1 ) {
+        return 1.0f;
+    }
+    // We also don't want to actually reduce values to zero, even if user is debuffed to zero strength.
+    float archer_str = std::max( 1, p.get_str() );
+    return ( archer_str / it.get_min_str() );
 }
 
 int throw_dispersion_per_dodge( const Character &c, bool add_encumbrance )
@@ -1834,6 +1851,9 @@ dispersion_sources player::get_weapon_dispersion( const item &obj ) const
     if( has_bionic( bio_targeting ) ) {
         dispersion.add_multiplier( 0.75 );
     }
+
+    // If using a bow you lack the strength for, increase based on how much weaker shooter is.
+    dispersion.add_multiplier( 1 / ranged::get_str_draw_penalty( obj, *this ) );
 
     // Range is effectively four times longer when shooting unflagged/flagged guns underwater/out of water.
     if( is_underwater() != obj.has_flag( flag_UNDERWATER_GUN ) ) {
