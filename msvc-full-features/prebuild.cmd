f7fbeec21809b4c12122b5744a142fc560881c8e
Coolthulhu <Coolthulhu@gmail.com>
2022-10-04 23:11:30 +0200

    Manual release workflow + Makefile (+friends) respecting VERSION

--- msvc-full-features/prebuild.cmd
+++ msvc-full-features/prebuild.cmd
@@ -5,9 +5,11 @@ cd ..\msvc-full-features
 echo Done
 
 echo Generating "version.h"...
-for /F "tokens=*" %%i in ('git describe --tags --always --dirty --match "[0-9]*.*"') do set VERSION=%%i
 if "%VERSION%"=="" (
-set VERSION=Please install `git` to generate VERSION
+for /F "tokens=*" %%i in ('git describe --tags --always --dirty --match "[0-9]*.*"') do set VERSION=%%i
+)
+if "%VERSION%"=="" (
+set VERSION=Please install `git` to generate VERSION, or set the variable manually
 )
 echo VERSION defined as "%VERSION%"
 >..\src\version.h echo #define VERSION "%VERSION%"
