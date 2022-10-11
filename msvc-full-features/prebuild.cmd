ab7d2d76de654db105c832183daae78f747f5bff
scarf <greenscarf005@gmail.com>
2022-10-11 11:19:48 +0900

    build: bump msvc version
    Co-authored-by: AngelicosPhosphoros <angelicos.phosphoros@protonmail.com>

--- msvc-full-features/prebuild.cmd
+++ msvc-full-features/prebuild.cmd
@@ -5,11 +5,9 @@ cd ..\msvc-full-features
 echo Done
 
 echo Generating "version.h"...
-if "%VERSION%"=="" (
 for /F "tokens=*" %%i in ('git describe --tags --always --dirty --match "[0-9]*.*"') do set VERSION=%%i
-)
 if "%VERSION%"=="" (
-set VERSION=Please install `git` to generate VERSION, or set the variable manually
+set VERSION=Please install `git` to generate VERSION
 )
 echo VERSION defined as "%VERSION%"
 >..\src\version.h echo #define VERSION "%VERSION%"
