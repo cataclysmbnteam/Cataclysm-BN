4d78b0fce442afbba88e6c34cefaf653dfafa38a
scarf <greenscarf005@gmail.com>
2022-10-14 01:32:14 +0900

    docs: url_utility.h

--- src/url_utility.h
+++ src/url_utility.h
@@ -4,7 +4,25 @@
 
 #include <string>
 
+/**
+ * open the given url in the default browser using std::system()
+ *
+ * the URL must be vaild to be opened.
+ * use open_url() to check if convert a string to a valid URL.
+ *
+ * @param url the URL to open.
+ * @return void
+ */
 auto open_url( const std::string &url ) -> void;
+
+/**
+ * @brief encode a string into URL encoding.
+ *
+ * check: https://www.w3schools.com/tags/ref_urlencode.ASP
+ *
+ * @param text the string to encode.
+ * @return URL encoded string.
+ */
 auto encode_url( const std::string &text ) -> std::string;
 
 #endif
