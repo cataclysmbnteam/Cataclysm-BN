68397b46fe54daec5be3e222cec561f3cd2bded0
scarf <greenscarf005@gmail.com>
2022-10-14 11:48:39 +0900

    Revert "fix: GCC unreachability intrinstic on false branch of assert"
    This reverts commit 492dde19558297b578354609ce9c53c4c4c67ec8.

--- src/vpart_range.h
+++ src/vpart_range.h
@@ -12,17 +12,6 @@
 #include "vpart_position.h"
 #include "vehicle.h"
 
-#if defined(__GNUC__)
-// HACK: See https://github.com/CleverRaven/Cataclysm-DDA/pull/58210 for details
-#   undef assert
-#   define assert( expr ) \
-    do { \
-        if( !( expr ) ) { \
-            __builtin_unreachable(); \
-        } \
-    } while( false )
-#endif
-
 enum class part_status_flag : int;
 
 /**
