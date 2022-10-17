0a7cb2585066907a3edc74199c01a0329f2335b6
LeoCottret <71428793+leoCottret@users.noreply.github.com>
2022-10-17 04:49:50 +0200

    improve code with a struct

--- src/sdlsound.cpp
+++ src/sdlsound.cpp
@@ -78,13 +78,16 @@ static std::map<std::string, music_playlist> playlists;
 static std::string current_soundpack_path;
 
 /** The ambient sound we're currently playing **/
-static std::string current_ambient_id;
-static std::string current_ambient_variant;
-static int current_ambient_volume;
-static sfx::channel current_ambient_channel;
-static int current_ambient_fade_in_duration;
-static double current_ambient_pitch;
-static int current_ambient_loops;
+struct ambient_parameters {
+    std::string id;
+    std::string variant;
+    int volume;
+    sfx::channel channel;
+    int fade_in_duration;
+    double pitch;
+    int loops;
+};
+static ambient_parameters current_ambient;
 
 static std::unordered_map<std::string, int> unique_paths;
 static sfx_resources_t sfx_resources;
@@ -305,7 +308,7 @@ void update_volumes()
 
     // Stop channels playing (different ambient sounds)
     // Then start back the last saved ambient sound with the new volume (fetched in the function)
-    if( current_ambient_id.empty() ) {
+    if( !current_ambient.id.empty() ) {
         // Stop currently playing channels
         for( int i = 0; i < static_cast<int>( sfx::channel::MAX_CHANNEL ); i++ ) {
             if( sfx::is_channel_playing( static_cast<sfx::channel>( i ) ) ) {
@@ -313,9 +316,9 @@ void update_volumes()
             }
         }
         // Start the last playing channel with updated volume
-        sfx::play_ambient_variant_sound( current_ambient_id, current_ambient_variant,
-                                         current_ambient_volume, current_ambient_channel, current_ambient_fade_in_duration,
-                                         current_ambient_pitch, current_ambient_loops );
+        sfx::play_ambient_variant_sound( current_ambient.id, current_ambient.variant,
+                                         current_ambient.volume, current_ambient.channel, current_ambient.fade_in_duration,
+                                         current_ambient.pitch, current_ambient.loops );
     }
 }
 
@@ -633,13 +636,13 @@ void sfx::play_ambient_variant_sound( const std::string &id, const std::string &
             cleanup_when_channel_finished( ch, effect_to_play );
         }
     }
-    current_ambient_id = id;
-    current_ambient_variant = variant;
-    current_ambient_volume = volume;
-    current_ambient_channel = channel;
-    current_ambient_fade_in_duration = fade_in_duration;
-    current_ambient_pitch = pitch;
-    current_ambient_loops = loops;
+    current_ambient.id = id;
+    current_ambient.variant = variant;
+    current_ambient.volume = volume;
+    current_ambient.channel = channel;
+    current_ambient.fade_in_duration = fade_in_duration;
+    current_ambient.pitch = pitch;
+    current_ambient.loops = loops;
 }
 
 void load_soundset()
