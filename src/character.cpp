akrieger/Cataclysm-DDA

Possibility to turn distractions on and off (#58371)
* Added switches for 11 distraction types

* Pain and attacked

* Hostile spotted

* Talked to

* Hostile is dangerously close, dangerous field, hunger, and thirst

* Hostile spotted

* Noise

* Asthma

* Weather changed

* Created distraction manager

* Replaced check for game option with check for uistate value

* Removed distraction options

* point( 0, 0 ) -> point_zero

Also removed double register_action.

* Removed excessive string formatting

* Translation

* Moved uistate stuff to inventory_ui

* Astyle

src/character.cpp
@@ -1217,7 +1217,7 @@ void Character::react_to_felt_pain( int intensity )
     if( intensity <= 0 ) {
         return;
     }
-    if( is_avatar() && intensity >= 2 ) {
+    if( uistate.distraction_pain && is_avatar() && intensity >= 2 ) {
         g->cancel_activity_or_ignore_query( distraction_type::pain, _( "Ouch, something hurts!" ) );
     }
     // Only a large pain burst will actually wake people while sleeping.
@@ -7572,7 +7572,7 @@ void Character::on_hurt( Creature *source, bool disturb /*= true*/ )
         if( has_effect( effect_sleep ) && !has_bionic( bio_sleep_shutdown ) ) {
             wake_up();
         }
-        if( !is_npc() && !has_effect( effect_narcosis ) ) {
+        if( uistate.distraction_attack && !is_npc() && !has_effect( effect_narcosis ) ) {
             if( source != nullptr ) {
                 if( sees( *source ) ) {
                     g->cancel_activity_or_ignore_query( distraction_type::attacked,

cataclysmbnteam/Cataclysm-BN

src/character.cpp
@@ -855,7 +855,7 @@ void Character::react_to_felt_pain( int intensity )
     if( intensity <= 0 ) {
         return;
     }
-    if( is_player() && intensity >= 2 ) {
+    if( uistate.distraction_pain && is_player() && intensity >= 2 ) {
         g->cancel_activity_or_ignore_query( distraction_type::pain, _( "Ouch, something hurts!" ) );
     }
     // Only a large pain burst will actually wake people while sleeping.
@@ -8697,7 +8697,7 @@ void Character::on_hurt( Creature *source, bool disturb /*= true*/ )
         if( has_effect( effect_sleep ) && !has_effect( effect_narcosis ) ) {
             wake_up();
         }
-        if( !is_npc() && !has_effect( effect_narcosis ) ) {
+        if( uistate.distraction_attack && !is_npc() && !has_effect( effect_narcosis ) ) {
             if( source != nullptr ) {
                 g->cancel_activity_or_ignore_query( distraction_type::attacked,
                                                     string_format( _( "You were attacked by %s!" ),