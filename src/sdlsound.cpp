7e166902892e1bb6da63a96778c5268197479e6f
leoCottret <71428793+leoCottret@users.noreply.github.com>
2022-10-30 16:34:11 +0100

    Fix ambient sound not updating in game on setting options change (#2030)
    * fix ambient volume not updating after options change
    
    * fix possible duplicated effect
    
    * update comments
    
    * remove unused variable
    
    * clang tidy
    
    * improve code with a struct

--- src/sdlsound.cpp
+++ src/sdlsound.cpp
@@ -77,6 +77,18 @@ static bool sound_init_success = false;
 static std::map<std::string, music_playlist> playlists;
 static std::string current_soundpack_path;
 
+/** The ambient sound we're currently playing **/
+struct ambient_parameters {
+    std::string id;
+    std::string variant;
+    int volume;
+    sfx::channel channel;
+    int fade_in_duration;
+    double pitch;
+    int loops;
+};
+static ambient_parameters current_ambient;
+
 static std::unordered_map<std::string, int> unique_paths;
 static sfx_resources_t sfx_resources;
 static std::vector<id_and_variant> sfx_preload;
@@ -270,7 +282,7 @@ void stop_music()
     absolute_playlist_at = 0;
 }
 
-void update_music_volume()
+void update_volumes()
 {
     if( test_mode ) {
         return;
@@ -281,7 +293,10 @@ void update_music_volume()
         return;
     }
 
+    // Change volume of current music
     Mix_VolumeMusic( current_music_track_volume * get_option<int>( "MUSIC_VOLUME" ) / 100 );
+
+
     // Start playing music, if we aren't already doing so (if
     // SOUND_ENABLED was toggled.)
 
@@ -289,6 +304,22 @@ void update_music_volume()
     // #28018 is resolved, as this function may be called from places
     // other than the main menu.
     play_music( "title" );
+
+
+    // Stop channels playing (different ambient sounds)
+    // Then start back the last saved ambient sound with the new volume (fetched in the function)
+    if( !current_ambient.id.empty() ) {
+        // Stop currently playing channels
+        for( int i = 0; i < static_cast<int>( sfx::channel::MAX_CHANNEL ); i++ ) {
+            if( sfx::is_channel_playing( static_cast<sfx::channel>( i ) ) ) {
+                Mix_HaltChannel( i );
+            }
+        }
+        // Start the last playing channel with updated volume
+        sfx::play_ambient_variant_sound( current_ambient.id, current_ambient.variant,
+                                         current_ambient.volume, current_ambient.channel, current_ambient.fade_in_duration,
+                                         current_ambient.pitch, current_ambient.loops );
+    }
 }
 
 // Allocate new Mix_Chunk as a null-chunk. Results in a valid, but empty chunk
@@ -605,6 +636,13 @@ void sfx::play_ambient_variant_sound( const std::string &id, const std::string &
             cleanup_when_channel_finished( ch, effect_to_play );
         }
     }
+    current_ambient.id = id;
+    current_ambient.variant = variant;
+    current_ambient.volume = volume;
+    current_ambient.channel = channel;
+    current_ambient.fade_in_duration = fade_in_duration;
+    current_ambient.pitch = pitch;
+    current_ambient.loops = loops;
 }
 
 void load_soundset()
