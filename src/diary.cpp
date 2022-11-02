d01ff98aaa7d710e1b347fe6da6398f5c24b3fff
scarf <greenscarf005@gmail.com>
2022-11-02 17:09:48 +0900

    Loop element hotfix (#2149)
    * fix: make loop element reference
    
    failed to build because each element was being copied
    
    * refactor: use string_view
    
    can be also used on char const*
    
    * refactor: add more consts

--- src/diary.cpp
+++ src/diary.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <fstream>
 #include <algorithm>
+#include <string_view>
 
 #include "avatar.h"
 #include "bionics.h"
@@ -744,12 +745,12 @@ void diary::export_to_md( bool last_export )
         // add "## " before each "Entry" line to visually differentiate each page
         left_diary_text[0] = "## " + left_diary_text[0];
 
-        for( std::string &str : left_diary_text ) {
+        for( const std::string &str : left_diary_text ) {
             myfile << remove_color_tags( str ) + "\n";
         }
-        std::vector<std::string> folded_text = foldstring( page.m_text, 50 );
-        for( const auto folded_text_char : folded_text ) {
-            myfile << folded_text_char + "\n";
+        const std::vector<std::string> folded_texts = foldstring( page.m_text, 50 );
+        for( const std::string_view folded_text : folded_texts ) {
+            myfile << folded_text << "\n";
         }
         myfile << "\n\n\n";
     }
