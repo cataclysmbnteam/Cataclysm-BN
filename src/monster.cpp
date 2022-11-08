18aaa715ed68ca3b0bf05dc5a917902b83587f57
leoCottret <71428793+leoCottret@users.noreply.github.com>
2022-11-08 06:41:12 +0100

    astyle

--- src/monster.cpp
+++ src/monster.cpp
@@ -2960,8 +2960,8 @@ void monster::hear_sound( const tripoint &source, const int vol, const int dist 
 
     int wander_turns = volume * ( goodhearing ? 6 : 1 );
     // only trigger this if the monster is not friendly or the source isn't the player
-    if (friendly >= 0 || source != g->u.pos()) {
-        process_trigger(mon_trigger::SOUND, volume);
+    if( friendly >= 0 || source != g->u.pos() ) {
+        process_trigger( mon_trigger::SOUND, volume );
     }
     if( morale >= 0 && anger >= 10 ) {
         // TODO: Add a proper check for fleeing attitude
