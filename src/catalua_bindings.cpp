90012b294048d8fac3a5cd669f7534ca48134a25
Olanti <olanti-p@yandex.ru>
2022-12-06 01:59:19 +0300

    Hack: pairs() for read-only tables

--- src/catalua_bindings.cpp
+++ src/catalua_bindings.cpp
@@ -23,12 +23,22 @@ static int deny_table_readonly( sol::this_state L )
     return luaL_error( L.lua_state(), "This table is read-only." );
 }
 
+std::tuple<sol::object, sol::object, sol::nil_t>
+reimpl_default_pairs( sol::this_state L, sol::table t )
+{
+    // TODO: check how broken this HACK is
+    sol::state_view lua( L.lua_state() );
+    // Forward to the table we're reading from
+    return std::make_tuple( lua["next"], t[sol::meta_function::index], sol::nil );
+}
+
 sol::table make_readonly_table( sol::state &lua, sol::table read_from )
 {
     sol::table ret = lua.create_table();
 
     read_from[sol::meta_function::index] = read_from;
     read_from[sol::meta_function::new_index] = deny_table_readonly;
+    read_from[sol::meta_function::pairs] = reimpl_default_pairs;
 
     ret[sol::metatable_key] = read_from;
     return ret;
@@ -44,6 +54,7 @@ sol::table make_readonly_table( sol::state &lua, sol::table read_from,
     read_from[sol::meta_function::new_index] = [copy]( sol::this_state L ) {
         return luaL_error( L.lua_state(), copy.c_str() );
     };
+    read_from[sol::meta_function::pairs] = reimpl_default_pairs;
 
     ret[sol::metatable_key] = read_from;
     return ret;
