7ab30eda1f0662208debd27db495b74c90d286f5
Olanti <olanti-p@yandex.ru>
2022-12-06 19:34:22 +0300

    Add Smart House Remotes mod

---
+++ data/mods/smart_house_remotes/finalize.lua
@@ -0,0 +1,4 @@
+log_info("SHR: finalize.")
+
+-- We're not finalizing anything.
+-- Besides, there's no finalizing API available yet.
---
+++ data/mods/smart_house_remotes/preload.lua
@@ -0,0 +1,16 @@
+log_info("SHR: preload.")
+
+-- Have to register iuse before data loading.
+-- Actual implementation (function mod.iuse_function) will be defined later.
+
+local mod = game.mod_runtime[ game.current_mod ]
+
+-- Register our map post-process hook
+game.on_mapgen_postprocess_hooks[ #game.on_mapgen_postprocess_hooks + 1 ] = function(...)
+    return mod.on_mapgen_postprocess_hook(...)
+end
+
+-- Register our item use function
+game.iuse_functions[ "SMART_HOUSE_REMOTE" ] = function(...)
+    return mod.iuse_function(...)
+end
