9fd8097ba280e3a5997591a8d187bace819d7a38
Olanti <olanti-p@yandex.ru>
2023-03-25 00:44:22 +0300

    Remove randomization

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -126,7 +126,7 @@ then
         {
             if [ -n "$1" ]
             then
-                echo "$1" | shuf | \
+                echo "$1" | \
                     xargs -P "$num_jobs" -n 1 ./build-scripts/clang-tidy-wrapper.sh -quiet
             else
                 echo "No files to analyze"
