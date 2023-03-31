cataclysmbnteam/Cataclysm-BN

commit 5a69bd07ade98701eaa459a3f0907bebfc4f5403

Port over and update 'Streamline aim and fire experience (#59977)' (#2468)
* Streamline aim and fire experience (#59977)

* Re-enter aiming ui after shooting

* Calm down player's view snapping around while aiming

* Temporary workaround for RAS bugs

* Option to disable reaiming

* Update options.cpp

Fix translation

* Fix RELOAD_AND_SHOOT weapons

Now displays loading cost and unloading costs.

First turn changed to indicate whether the player has chosen to aim or use the aim_and_shoot function.

* Properly set first_turn for non-ras weapons.

Was causing aim_and_shoot actions to abort prematurely.

* Reorder options

It should appear under the SNAP_TO_TARGET instead of under the disassemble query.

* Redo move calculation of RAS weapons.

Had to move the reload into `fire_gun()` otherwise the item location becomes a nullpointer and the game crashes.

* Convert all item locations in this chain to not be references.

Fix test

* Fix Ammo Display

* Fixes

Use class instead of item_location.h Add read arguments for arguments added to save.

---------

Co-authored-by: Alexey <irwiss@users.noreply.github.com>

src/ranged.cpp
@@ -161,7 +161,7 @@ static const trait_id trait_NORANGEDCRIT( "NO_RANGED_CRIT" );
 static constexpr int AIF_DURATION_LIMIT = 10;
 
 static projectile make_gun_projectile( const item &gun );
-int time_to_attack( const Character &p, const itype &firing );
+int time_to_attack( const Character &p, const item &firing, const item_location loc );
 static void cycle_action( item &weap, const tripoint &pos );
 bool can_use_bipod( const map &m, const tripoint &pos );
 dispersion_sources calculate_dispersion( const map &m, const Character &who, const item &gun,
@@ -442,6 +442,7 @@ target_handler::trajectory target_handler::mode_throw( avatar &you, item &releva
     ui.relevant = &relevant;
     ui.range = you.throw_range( relevant );
 
+    restore_on_out_of_scope<tripoint> view_offset_prev( you.view_offset );
     return ui.run();
 }
 
@@ -453,6 +454,7 @@ target_handler::trajectory target_handler::mode_reach( avatar &you, item &weapon
     ui.relevant = &weapon;
     ui.range = weapon.reach_range( you );
 
+    restore_on_out_of_scope<tripoint> view_offset_prev( you.view_offset );
     return ui.run();
 }
 
@@ -464,6 +466,7 @@ target_handler::trajectory target_handler::mode_turret_manual( avatar &you, turr
     ui.turret = &turret;
     ui.relevant = &*turret.base();
 
+    restore_on_out_of_scope<tripoint> view_offset_prev( you.view_offset );
     return ui.run();
 }
 
@@ -493,6 +496,7 @@ target_handler::trajectory target_handler::mode_turrets( avatar &you, vehicle &v
     ui.vturrets = &turrets;
     ui.range = range_total;
 
+    restore_on_out_of_scope<tripoint> view_offset_prev( you.view_offset );
     return ui.run();
 }
 
@@ -507,6 +511,7 @@ target_handler::trajectory target_handler::mode_spell( avatar &you, spell &casti
     ui.no_fail = no_fail;
     ui.no_mana = no_mana;
 
+    restore_on_out_of_scope<tripoint> view_offset_prev( you.view_offset );
     return ui.run();
 }
 
@@ -811,15 +816,24 @@ dispersion_sources calculate_dispersion( const map &m, const Character &who, con
 
 int ranged::fire_gun( Character &who, const tripoint &target, int shots )
 {
-    return fire_gun( who, target, shots, who.weapon );
+    return fire_gun( who, target, shots, who.weapon, item_location() );
 }
 
-int ranged::fire_gun( Character &who, const tripoint &target, const int max_shots, item &gun )
+int ranged::fire_gun( Character &who, const tripoint &target, int max_shots, item &gun,
+                      item_location ammo )
 {
+    int attack_moves = time_to_attack( who, gun, ammo );
+
     if( !gun.is_gun() ) {
         debugmsg( "%s tried to fire non-gun (%s).", who.name, gun.tname() );
         return 0;
     }
+
+    if( gun.ammo_required() > 0 && !gun.ammo_remaining() && !ammo ) {
+        debugmsg( "%s's gun %s is empty and has no ammo for reloading.", gun.tname() );
+        return 0;
+    }
+
     bool is_mech_weapon = false;
     if( who.is_mounted() && who.mounted_creature->has_flag( MF_RIDEABLE_MECH ) ) {
         is_mech_weapon = true;
@@ -827,8 +841,10 @@ int ranged::fire_gun( Character &who, const tripoint &target, const int max_shot
 
     int shots = max_shots;
     // Number of shots to fire is limited by the amount of remaining ammo
-    if( gun.ammo_required() ) {
+    if( gun.ammo_required() && !ammo ) {
         shots = std::min( shots, static_cast<int>( gun.ammo_remaining() / gun.ammo_required() ) );
+    } else {
+        shots = std::min( shots, ammo.get_item()->count() / gun.ammo_required() );
     }
 
     // cap our maximum burst size by the amount of UPS power left
@@ -858,6 +874,9 @@ int ranged::fire_gun( Character &who, const tripoint &target, const int max_shot
     int curshot = 0;
     int hits = 0; // total shots on target
     while( curshot != shots ) {
+        if( !!ammo && !gun.ammo_remaining() ) {
+            gun.reload( get_avatar(), std::move( ammo ), 1 );
+        }
         if( gun.faults.count( fault_gun_chamber_spent ) && curshot == 0 ) {
             who.moves -= 50;
             gun.faults.erase( fault_gun_chamber_spent );
@@ -958,7 +977,7 @@ int ranged::fire_gun( Character &who, const tripoint &target, const int max_shot
     }
 
     // Use different amounts of time depending on the type of gun and our skill
-    who.moves -= time_to_attack( who, *gun.type );
+    who.moves -= attack_moves;
 
     // Practice the base gun skill proportionally to number of hits, but always by one.
     who.as_player()->practice( skill_gun, ( hits + 1 ) * 5 );
@@ -1548,7 +1567,8 @@ static double calculate_aim_cap( const Character &p, const tripoint &target )
 
 static int print_aim( const Character &p, const catacurses::window &w, int line_number,
                       input_context &ctxt, const item &weapon,
-                      const double target_size, const tripoint &pos, double predicted_recoil )
+                      const double target_size, const tripoint &pos, double predicted_recoil,
+                      item_location load_loc )
 {
     // This is absolute accuracy for the player.
     // TODO: push the calculations duplicated from Creature::deal_projectile_attack() and
@@ -1584,7 +1604,7 @@ static int print_aim( const Character &p, const catacurses::window &w, int line_
     const auto cost_fun = [&]( const ranged::aim_type & at ) {
         int at_recoil = at.has_threshold ? at.threshold : static_cast<int>( predicted_recoil );
         return ranged::gun_engagement_moves( p, weapon, at_recoil, p.recoil ) +
-               time_to_attack( p, *weapon.type );
+               time_to_attack( p, weapon, load_loc );
     };
     const double range = rl_dist( p.pos(), pos );
     line_number = print_steadiness( w, line_number, steadiness );
@@ -1714,12 +1734,23 @@ static projectile make_gun_projectile( const item &gun )
     return proj;
 }
 
-int time_to_attack( const Character &p, const itype &firing )
+int time_to_attack( const Character &p, const item &firing, const item_location loc )
 {
-    const skill_id &skill_used = firing.gun->skill_used;
+    const skill_id &skill_used = firing.type->gun->skill_used;
     const time_info_t &info = skill_used->time_to_attack();
+    int RAS_time = 0;
+    if( !loc ) {
+        RAS_time = 0;
+    } else {
+        // RAS Weapon, calculate move cost.
+        // At low stamina levels, firing starts getting slow.
+        const int sta_percent = ( 100 * p.get_stamina() ) / p.get_stamina_max();
+        const int reload_stamina_penalty = ( sta_percent < 25 ) ? ( ( 25 - sta_percent ) * 2 ) : 0;
+        item_reload_option opt = item_reload_option( p.as_player(), &firing, &firing, loc );
+        RAS_time = opt.moves() + reload_stamina_penalty;
+    }
     return std::max( info.min_time,
-                     info.base_time - info.time_reduction_per_level * p.get_skill_level( skill_used ) );
+                     info.base_time - info.time_reduction_per_level * p.get_skill_level( skill_used ) + RAS_time );
 }
 
 static void cycle_action( item &weap, const tripoint &pos )
@@ -1977,7 +2008,7 @@ double npc_ai::gun_value( const Character &who, const item &weap, int ammo )
         damage_factor += 0.5f * gun_damage.damage_units.front().res_pen;
     }
 
-    int move_cost = time_to_attack( who, *weap.type );
+    int move_cost = time_to_attack( who, weap, item_location() );
     if( gun.clip != 0 && gun.clip < 10 ) {
         // TODO: RELOAD_ONE should get a penalty here
         int reload_cost = gun.reload_time + who.encumb( bp_hand_l ) + who.encumb( bp_hand_r );
@@ -2182,7 +2213,6 @@ target_handler::trajectory target_ui::run()
     on_out_of_scope cleanup( [&here, &player_character]() {
         here.invalidate_map_cache( player_character.pos().z + player_character.view_offset.z );
     } );
-    restore_on_out_of_scope<tripoint> view_offset_prev( player_character.view_offset );
 
     shared_ptr_fast<game::draw_callback_t> target_ui_cb = make_shared_fast<game::draw_callback_t>(
     [&]() {
@@ -2206,7 +2236,7 @@ target_handler::trajectory target_ui::run()
     bool attack_was_confirmed = false;
     bool reentered = false;
     bool resume_critter = false;
-    if( mode == TargetMode::Fire && !activity->first_turn ) {
+    if( mode == TargetMode::Fire && !activity->action.empty() ) {
         // We were in this UI during previous turn...
         reentered = true;
         std::string act_data = activity->action;
@@ -2228,6 +2258,14 @@ target_handler::trajectory target_ui::run()
     src = you->pos();
     update_target_list();
 
+    if( activity && activity->abort_if_no_targets && targets.empty() ) {
+        // this branch is taken when already shot once and re-entered
+        // aiming, if no targets are available we want to abort so
+        // players don't arrive at aiming ui with nothing to shoot at.
+        activity->aborted = true;
+        traj.clear();
+        return traj;
+    }
     tripoint initial_dst = src;
     if( reentered ) {
         if( !try_reacquire_target( resume_critter, initial_dst ) ) {
@@ -2252,7 +2290,7 @@ target_handler::trajectory target_ui::run()
             action.clear();
             attack_was_confirmed = false;
         }
-        if( !activity->first_turn && !action.empty() && !prompt_friendlies_in_lof() ) {
+        if( !action.empty() && !prompt_friendlies_in_lof() ) {
             // A friendly creature moved into line of fire during aim-and-shoot,
             // and player decided to stop aiming
             action.clear();
@@ -3056,15 +3094,19 @@ void target_ui::update_ammo_range_from_gun_mode()
     if( mode == TargetMode::TurretManual ) {
         itype_id ammo_current = turret->ammo_current();
         // Test no-ammo and not a UPS weapon
-        if( !ammo_current && ( relevant->get_gun_ups_drain() == 0 ) ) {
+        if( !ammo_current && !activity->reload_loc && ( relevant->get_gun_ups_drain() == 0 ) ) {
             ammo = nullptr;
             range = 0;
+        } else if( !!activity->reload_loc ) {
+            ammo = activity->reload_loc.get_item()->type;
+            range = turret->range();
         } else {
             ammo = &*ammo_current;
             range = turret->range();
         }
     } else {
-        ammo = relevant->gun_current_mode().target->ammo_data();
+        ammo = activity->reload_loc ? activity->reload_loc.get_item()->type :
+               relevant->gun_current_mode().target->ammo_data();
         range = relevant->gun_current_mode().target->gun_range( you );
     }
 }
@@ -3603,7 +3645,6 @@ void target_ui::panel_fire_mode_aim( int &text_y )
     // TODO: saving & restoring pc.recoil may actually be unnecessary
     double saved_pc_recoil = you->recoil;
     you->recoil = predicted_recoil;
-
     double predicted_recoil = you->recoil;
     int predicted_delay = 0;
     if( aim_mode->has_threshold && aim_mode->threshold < you->recoil ) {
@@ -3622,8 +3663,9 @@ void target_ui::panel_fire_mode_aim( int &text_y )
     const double target_size = dst_critter ? dst_critter->ranged_target_size() :
                                occupied_tile_fraction( m_size::MS_MEDIUM );
 
+    item_location load_loc = activity->reload_loc;
     text_y = print_aim( *you, w_target, text_y, ctxt, *relevant->gun_current_mode(),
-                        target_size, dst, predicted_recoil );
+                        target_size, dst, predicted_recoil, load_loc );
 
     if( aim_mode->has_threshold ) {
         mvwprintw( w_target, point( 1, text_y++ ), _( "%s Delay: %i" ), aim_mode->name,

src/activity_actor.cpp
@@ -116,7 +116,7 @@ void aim_activity_actor::do_turn( player_activity &act, Character &who )
     }
 
     gun_mode gun = weapon->gun_current_mode();
-    if( first_turn && gun->has_flag( flag_RELOAD_AND_SHOOT ) && !gun->ammo_remaining() ) {
+    if( !gun->ammo_remaining() && !reload_loc && gun->has_flag( flag_RELOAD_AND_SHOOT ) ) {
         if( !load_RAS_weapon() ) {
             aborted = true;
             act.moves_left = 0;
@@ -145,10 +145,6 @@ void aim_activity_actor::do_turn( player_activity &act, Character &who )
             fin_trajectory = trajectory;
             act.moves_left = 0;
         }
-        // If aborting on the first turn, keep 'first_turn' as 'true'.
-        // This allows refunding moves spent on unloading RELOAD_AND_SHOOT weapons
-        // to simulate avatar not loading them in the first place
-        first_turn = false;
 
         // Allow interrupting activity only during 'aim and fire'.
         // Prevents '.' key for 'aim for 10 turns' from conflicting with '.' key for 'interrupt activity'
@@ -161,24 +157,25 @@ void aim_activity_actor::do_turn( player_activity &act, Character &who )
 void aim_activity_actor::finish( player_activity &act, Character &who )
 {
     act.set_to_null();
-    restore_view();
     item *weapon = get_weapon();
     if( !weapon ) {
+        restore_view();
         return;
     }
     if( aborted ) {
-        unload_RAS_weapon();
         if( reload_requested ) {
             // Reload the gun / select different arrows
             // May assign ACT_RELOAD
             avatar_action::reload_wielded( true );
         }
+        restore_view();
         return;
     }
 
     // Fire!
     gun_mode gun = weapon->gun_current_mode();
-    int shots_fired = ranged::fire_gun( who, fin_trajectory.back(), gun.qty, *gun );
+
+    int shots_fired = ranged::fire_gun( who, fin_trajectory.back(), gun.qty, *gun, reload_loc );
 
     if( shots_fired > 0 ) {
         // TODO: bionic power cost of firing should be derived from a value of the relevant weapon.
@@ -189,12 +186,30 @@ void aim_activity_actor::finish( player_activity &act, Character &who )
             who.mod_stamina( -stamina_cost_per_shot * shots_fired );
         }
     }
+
+    if( !get_option<bool>( "AIM_AFTER_FIRING" ) ) {
+        restore_view();
+        return;
+    }
+
+    // re-enter aiming UI with same parameters
+    aim_activity_actor aim_actor;
+    aim_actor.abort_if_no_targets = true;
+    aim_actor.fake_weapon = this->fake_weapon;
+    aim_actor.bp_cost_per_shot = this->bp_cost_per_shot;
+    aim_actor.initial_view_offset = this->initial_view_offset;
+
+    // if invalid target or it's dead - reset it so a new one is acquired
+    shared_ptr_fast<Creature> last_target = who.last_target.lock();
+    if( !last_target || last_target->is_dead_state() ) {
+        who.last_target.reset();
+    }
+    who.assign_activity( player_activity( aim_actor ), false );
 }
 
 void aim_activity_actor::canceled( player_activity &/*act*/, Character &/*who*/ )
 {
     restore_view();
-    unload_RAS_weapon();
 }
 
 void aim_activity_actor::serialize( JsonOut &jsout ) const
@@ -204,7 +219,6 @@ void aim_activity_actor::serialize( JsonOut &jsout ) const
     jsout.member( "fake_weapon", fake_weapon );
     jsout.member( "bp_cost_per_shot", bp_cost_per_shot );
     jsout.member( "stamina_cost_per_shot", stamina_cost_per_shot );
-    jsout.member( "first_turn", first_turn );
     jsout.member( "action", action );
     jsout.member( "aif_duration", aif_duration );
     jsout.member( "aiming_at_critter", aiming_at_critter );
@@ -212,6 +226,10 @@ void aim_activity_actor::serialize( JsonOut &jsout ) const
     jsout.member( "shifting_view", shifting_view );
     jsout.member( "initial_view_offset", initial_view_offset );
     jsout.member( "loaded_RAS_weapon", loaded_RAS_weapon );
+    jsout.member( "reload_loc", reload_loc );
+    jsout.member( "aborted", aborted );
+    jsout.member( "reload_requested", reload_requested );
+    jsout.member( "abort_if_no_targets", abort_if_no_targets );
 
     jsout.end_object();
 }
@@ -225,7 +243,6 @@ std::unique_ptr<activity_actor> aim_activity_actor::deserialize( JsonIn &jsin )
     data.read( "fake_weapon", actor.fake_weapon );
     data.read( "bp_cost_per_shot", actor.bp_cost_per_shot );
     data.read( "stamina_cost_per_shot", actor.stamina_cost_per_shot );
-    data.read( "first_turn", actor.first_turn );
     data.read( "action", actor.action );
     data.read( "aif_duration", actor.aif_duration );
     data.read( "aiming_at_critter", actor.aiming_at_critter );
@@ -233,6 +250,10 @@ std::unique_ptr<activity_actor> aim_activity_actor::deserialize( JsonIn &jsin )
     data.read( "shifting_view", actor.shifting_view );
     data.read( "initial_view_offset", actor.initial_view_offset );
     data.read( "loaded_RAS_weapon", actor.loaded_RAS_weapon );
+    data.read( "reload_loc", actor.reload_loc );
+    data.read( "aborted", actor.aborted );
+    data.read( "reload_requested", actor.reload_requested );
+    data.read( "abort_if_no_targets", actor.abort_if_no_targets );
 
     return actor.clone();
 }
@@ -271,13 +292,10 @@ bool aim_activity_actor::load_RAS_weapon()
     // Will burn (0.2% max base stamina * the strength required to fire)
     stamina_cost_per_shot = gun->get_min_str() * static_cast<int>
                             ( 0.002f * get_option<int>( "PLAYER_MAX_STAMINA" ) );
-    const int sta_percent = ( 100 * you.get_stamina() ) / you.get_stamina_max();
     if( you.get_stamina() < stamina_cost_per_shot ) {
         you.add_msg_if_player( m_bad, _( "You're too tired to draw your %s." ), weapon->tname() );
         return false;
     }
-    // At low stamina levels, firing starts getting slow.
-    const int reload_stamina_penalty = ( sta_percent < 25 ) ? ( ( 25 - sta_percent ) * 2 ) : 0;
 
     const auto ammo_location_is_valid = [&]() -> bool {
         you.ammo_location.make_dirty();
@@ -302,41 +320,10 @@ bool aim_activity_actor::load_RAS_weapon()
         // Menu canceled
         return false;
     }
-    const int reload_time = reload_stamina_penalty + opt.moves();
-    if( !gun->reload( you, std::move( opt.ammo ), 1 ) ) {
-        // Reload not allowed
-        return false;
-    }
 
-    you.moves -= reload_time;
+    reload_loc = opt.ammo;
     loaded_RAS_weapon = true;
     return true;
-}
-
-void aim_activity_actor::unload_RAS_weapon()
-{
-    // Unload reload-and-shoot weapons to avoid leaving bows pre-loaded with arrows
-    avatar &you = get_avatar();
-    item *weapon = get_weapon();
-    if( !weapon || !loaded_RAS_weapon ) {
-        return;
-    }
-
-    gun_mode gun = weapon->gun_current_mode();
-    if( gun->has_flag( flag_RELOAD_AND_SHOOT ) ) {
-        int moves_before_unload = you.moves;
-
-        // Note: this code works only for avatar
-        item_location loc = item_location( you, gun.target );
-        avatar_funcs::unload_item( you, loc );
-
-        // Give back time for unloading as essentially nothing has been done.
-        if( first_turn ) {
-            you.moves = moves_before_unload;
-        }
-    }
-
-    loaded_RAS_weapon = false;
 }
 
 void autodrive_activity_actor::start( player_activity &act, Character &who )

src/activity_actor_definitions.h
@@ -26,7 +26,6 @@ class aim_activity_actor : public activity_actor
         std::vector<tripoint> fin_trajectory;
 
     public:
-        bool first_turn = true;
         std::string action = "";
         int aif_duration = 0; // Counts aim-and-fire duration
         bool aiming_at_critter = false; // Whether aiming at critter or a tile
@@ -37,6 +36,10 @@ class aim_activity_actor : public activity_actor
         bool aborted = false;
         /** RELOAD_AND_SHOOT weapon is kept loaded by the activity */
         bool loaded_RAS_weapon = false;
+        /* Item location for RAS weapon reload */
+        item_location reload_loc = item_location();
+        /** if true abort if no targets are available when re-entering aiming ui after shooting */
+        bool abort_if_no_targets = false;
         /**
          * Target UI requested to abort aiming and reload weapon
          * Implies aborted = true

src/npcmove.cpp
@@ -1113,7 +1113,7 @@ void npc::execute_action( npc_action action )
                 pretend_fire( this, mode.qty, *mode );
             } else {
                 add_msg( m_debug, "%s recoil on firing: %s", name, recoil );
-                ranged::fire_gun( *this, tar, mode.qty, *mode );
+                ranged::fire_gun( *this, tar, mode.qty, *mode, item_location() );
                 // "discard" the fake bio weapon after shooting it
                 if( cbm_weapon_index >= 0 ) {
                     discharge_cbm_weapon();

src/options.cpp
@@ -1516,6 +1516,11 @@ void options_manager::add_options_interface()
          false
        );
 
+    add( "AIM_AFTER_FIRING", "interface", translate_marker( "Reaim after firing" ),
+         translate_marker( "If true, after firing automatically aim again if targets are available." ),
+         true
+       );
+
     add( "QUERY_DISASSEMBLE", "interface", translate_marker( "Query on disassembly while butchering" ),
          translate_marker( "If true, will query before disassembling items while butchering." ),
          true

src/ranged.h
@@ -15,6 +15,7 @@ class Creature;
 class dispersion_sources;
 class gun_mode;
 class item;
+class item_location;
 class map;
 class player;
 class spell;
@@ -181,7 +182,8 @@ int fire_gun( Character &who, const tripoint &target, int shots = 1 );
  * @param gun Item to fire (which does not necessary have to be in the characters possession)
  * @return Number of shots actually fired
  */
-int fire_gun( Character &who, const tripoint &target, int shots, item &gun );
+int fire_gun( Character &who, const tripoint &target, int shots, item &gun,
+              item_location ammo );
 
 /**
  * Execute a throw.

src/turret.cpp
@@ -298,7 +298,7 @@ int turret_data::fire( player &p, const tripoint &target )
     auto mode = base()->gun_current_mode();
 
     prepare_fire( p );
-    shots = ranged::fire_gun( p, target, mode.qty, *mode );
+    shots = ranged::fire_gun( p, target, mode.qty, *mode, item_location() );
     post_fire( p, shots );
     return shots;
 }

tests/ranged_aoe_test.cpp
@@ -124,7 +124,7 @@ TEST_CASE( "character using birdshot against another character", "[shape][ranged
     const int target_hp_total_before = target->get_hp();
     REQUIRE( target->get_hp() >= 100 );
     shooter.wield( gun );
-    int shots_fired = ranged::fire_gun( shooter, target_pos, 1, shooter.weapon );
+    int shots_fired = ranged::fire_gun( shooter, target_pos, 1, shooter.weapon, item_location() );
 
     REQUIRE( shots_fired > 0 );
     CHECK( target->get_hp() < target_hp_total_before );

commit 9fc83b65af76cda0cde8aff177d76dea6d739ec6

Fix possible segfault caused by #2468 when guns are fired (#2516)
* Update ranged.cpp

Fix dumb code I made

* Update src/ranged.cpp

Co-authored-by: scarf <greenscarf005@gmail.com>

---------

Co-authored-by: scarf <greenscarf005@gmail.com>

src/ranged.cpp
@@ -841,10 +841,9 @@ int ranged::fire_gun( Character &who, const tripoint &target, int max_shots, ite
 
     int shots = max_shots;
     // Number of shots to fire is limited by the amount of remaining ammo
-    if( gun.ammo_required() && !ammo ) {
-        shots = std::min( shots, static_cast<int>( gun.ammo_remaining() / gun.ammo_required() ) );
-    } else {
-        shots = std::min( shots, ammo.get_item()->count() / gun.ammo_required() );
+    if( gun.ammo_required() ) {
+        const int ammo_left = ammo ? ammo.get_item()->count() : gun.ammo_remaining();
+        shots = std::min( shots, ammo_left / gun.ammo_required() );
     }
 
     // cap our maximum burst size by the amount of UPS power left