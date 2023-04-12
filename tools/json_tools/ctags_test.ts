d5054aa76f174d8feec054bfd35cfe7e99bc85bf
scarf <greenscarf005@gmail.com>
2023-04-12 23:32:06 +0900

    docs: mention how to initially test ctags

--- tools/json_tools/ctags_test.ts
+++ tools/json_tools/ctags_test.ts
@@ -3,8 +3,9 @@ import { assertEquals } from "https://deno.land/std@0.182.0/testing/asserts.ts"
 import { PROJECT } from "./cata_paths.ts"
 import { catatags } from "./ctags.ts"
 
+/** generate your own .tags.bak file when running this test first. */
 Deno.test("regression test with previous tags.bak file", async () => {
-  const tagsBak = await Deno.readTextFile(join(PROJECT.ROOT, "tags.bak"))
+  const tagsBak = await Deno.readTextFile(join(PROJECT.VSCODE, ".tags.bak"))
   const tags = await catatags()
 
   assertEquals(tagsBak, tags.join("\n"))
