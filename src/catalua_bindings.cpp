2c470c5ff56f487ba256dde1308079a1b0b1dc94
Olanti <olanti-p@yandex.ru>
2023-04-13 19:00:18 +0300

    Fix msvc build, again

--- src/catalua_bindings.cpp
+++ src/catalua_bindings.cpp
@@ -502,7 +502,7 @@ void cata::detail::reg_game_api( sol::state &lua )
     luna::set_fx( lib, "current_turn", []() -> time_point { return calendar::turn; } );
     luna::set_fx( lib, "turn_zero", []() -> time_point { return calendar::turn_zero; } );
     luna::set_fx( lib, "before_time_starts", []() -> time_point { return calendar::before_time_starts; } );
-    luna::set_fx( lib, "rng", &rng );
+    luna::set_fx( lib, "rng", sol::resolve<int( int, int )>( &rng ) );
 
     luna::finalize_lib( lib );
 }
