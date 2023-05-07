f14531bf6ea85612f3586132cd638522e5fe4014
scarf <greenscarf005@gmail.com>
2023-05-07 17:57:12 +0900

    perf: improve formatting time for small number of files

--- tools/json_tools/json_fmt.ts
+++ tools/json_tools/json_fmt.ts
@@ -1,6 +1,20 @@
-export const fmtJsonRecursively = async () => {
-  const cwd = (await import("./cata_paths.ts")).ROOT
-  const command = new Deno.Command("make", { cwd, args: ["style-all-json-parallel"] })
+import { join } from "https://deno.land/std@0.182.0/path/mod.ts"
+import { asynciter } from "https://deno.land/x/asynciter@0.0.15/asynciter.ts"
 
-  return command.output()
+const getExecPath = async () => {
+  const isMsys2 = Deno.build.os === "windows" && Deno.env.get("MSYSTEM")
+  const isMingw32 = Deno.build.os === "windows" && Deno.env.get("CROSS")?.includes("mingw32")
+  const JSON_FORMATTER_BIN = `tools/format/json_formatter.${(isMsys2 || isMingw32) ? "exe" : "cgi"}`
+
+  const cwd = (await import("./cata_paths.ts")).ROOT
+
+  return join(cwd, JSON_FORMATTER_BIN)
+}
+
+export const fmtJsonRecursively = async (paths: string[]): Promise<void> => {
+  const execPath = await getExecPath()
+
+  await asynciter(paths)
+    .concurrentUnorderedMap((path) => new Deno.Command(execPath, { args: [path] }).output())
+    .collect()
 }
