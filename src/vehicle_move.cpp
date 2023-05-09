72bdb756236a54e09eafa5f956188b7f5a00a4f7
joveeater <joveasarus@gmail.com>
2023-05-09 21:57:19 +0100

    Update vehicle_move.cpp

--- src/vehicle_move.cpp
+++ src/vehicle_move.cpp
@@ -1615,7 +1615,7 @@ float map::vehicle_wheel_traction( const vehicle &veh,
         const tripoint &pp = veh.global_part_pos3( p );
         const int wheel_area = veh.cpart( p ).wheel_area();
 
-        const auto &tr = ter( pp ).obj();
+        const auto tr = ter( pp ).obj();
         // Deep water and air
         if( tr.has_flag( TFLAG_DEEP_WATER ) || tr.has_flag( TFLAG_NO_FLOOR ) ) {
             // No traction from wheel in water or air
