6fb6aa4f2eba7255a811166d4eae5df4e7820cde
AniDemi <AniDemi49@gmail.com>
2023-05-25 18:37:53 +0200

    fix tests again

--- tests/iteminfo_test.cpp
+++ tests/iteminfo_test.cpp
@@ -387,9 +387,9 @@ TEST_CASE( "nutrients in food", "[item][iteminfo][food]" )
             i, q,
             "--\n"
             "Nutrition will <color_cyan>vary with chosen ingredients</color>.\n"
-            "<color_c_white>Calories (kcal)</color>: <color_c_yellow>318</color>-"
+            "<color_c_white>Calories (kcal)</color>: <color_c_yellow>282</color>-"
             "<color_c_yellow>469</color>  Quench: <color_c_yellow>0</color>\n"
-            "Vitamins (RDA): Calcium (7-26%), Iron (0-83%), "
+            "Vitamins (RDA): Calcium (7-28%), Iron (0-83%), "
             "Vitamin A (3-11%), Vitamin B12 (2-6%), and Vitamin C (1-85%)\n" );
     }
 }
