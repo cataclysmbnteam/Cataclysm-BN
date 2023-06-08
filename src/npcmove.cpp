450e35f12fc26c52fdad8c5e6e5cc62e1bfaac03
KheirFerrum <102964889+KheirFerrum@users.noreply.github.com>
2023-06-08 18:09:08 +0100

    Rework `wield_better_weapon` considerations (#2930)
    * Rework wield_better_weapon considerations
    
    NPCs stop wielding non weapons they have worn (likely armor/accessories)
    
    They will now wield non-weapons if they have nothing better and it's not worn. So we now return to the glorious days of whacking people with helmets (assuming it's better as a weapon than anything they have equipped)
    
    * npc_throw now clears the compare weapon cache
    
    After you throw your weapon you'll logically look for a new one.

--- src/npcmove.cpp
+++ src/npcmove.cpp
@@ -3497,7 +3497,11 @@ bool npc::wield_better_weapon()
     // to have NPCs wield weapons with shorter ranges than dist in preparation
     // if they don't have a weapon with appropriate range/ammo.
     visit_items( [&compare_weapon, this ]( item * node ) {
-        // Only compare melee weapons, guns, or holstered items
+        // For worn items, only compare if they have a weapon category defined.
+        if( is_worn( *node ) && node->type->weapon_category.empty() ) {
+            return VisitResponse::SKIP;
+        }
+        // Otherwise, compare any melee usable item, guns or holstered items
         if( node->is_melee() || node->is_gun() ) {
             compare_weapon( *node );
         } else if( node->get_use( "holster" ) && !node->contents.empty() && node != &primary_weapon() ) {
@@ -3604,6 +3608,7 @@ static void npc_throw( npc &np, item &it, int index, const tripoint &pos )
     } else {
         it.charges = stack_size - 1;
     }
+    np.clear_npc_ai_info_cache( npc_ai_info::range );
 }
 
 bool npc::alt_attack()
