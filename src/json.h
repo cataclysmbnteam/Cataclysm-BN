b5c73b7384a876e0bb5ac6815b06a5df01acb7e1
Olanti <olanti-p@yandex.ru>
2023-07-23 21:46:13 +0300

    Remove old commented code

--- src/json.h
+++ src/json.h
@@ -528,34 +528,6 @@ class JsonIn
             return true;
         }
 
-        // special case for colony as it uses `insert()` instead of `push_back()`
-        // and therefore doesn't fit with vector/deque/list
-        /*template <typename T>
-        bool read( cata::colony<T> &v, bool throw_on_error = false ) {
-            if( !test_array() ) {
-                return error_or_false( throw_on_error, "Expected json array" );
-            }
-            try {
-                start_array();
-                v.clear();
-                while( !end_array() ) {
-                    T element;
-                    if( read( element, throw_on_error ) ) {
-                        v.insert( std::move( element ) );
-                    } else {
-                        skip_value();
-                    }
-                }
-            } catch( const JsonError & ) {
-                if( throw_on_error ) {
-                    throw;
-                }
-                return false;
-            }
-
-            return true;
-        }*/
-
         // object ~> containers with unmatching key_type and value_type
         // map, unordered_map ~> object
         template < typename T, typename std::enable_if <
