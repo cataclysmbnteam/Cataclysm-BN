396822c9546d2974ab9ad62b9267b0fb31584657
scarf <greenscarf005@gmail.com>
2023-09-17 15:53:54 +0900

    docs: do not try to fix absolute URL

--- doc/remark.ts
+++ doc/remark.ts
@@ -1,13 +1,17 @@
-import { isAbsolute, join } from "node:path"
+import { join } from "node:path"
 
 import type { Root } from "mdast"
 import { visit } from "unist-util-visit"
 
+/** Check if a string starts with one of `http://` or `https://`. */
+export const isAbsolute = (link: string) => /^https?:\/\//.test(link)
+
 // starts with ./ or ../
 const relativePath = new RegExp(`^\\.\\.?/`)
 
-const relativeUrl = (url: string) =>
-  (!isAbsolute(url) && relativePath.test(url)) ? join("..", url) : url
+const isRelative = (url: string) => !isAbsolute(url) && relativePath.test(url)
+
+const relativeUrl = (url: string) => join("..", url)
 
 const suffix = new RegExp(`\\.(md|mdx)$`)
 const suffixAnchor = new RegExp(`\\.(md|mdx)#`)
@@ -19,5 +23,7 @@ const removeSuffix = (url: string) =>
 
 export const fixRelativeLinks = () => (tree: Root) =>
   visit(tree, "link", (node) => {
+    if (!isRelative(node.url)) return
+
     node.url = relativeUrl(removeSuffix(node.url)).toLowerCase()
   })
