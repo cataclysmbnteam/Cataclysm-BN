b7a3d4dcf5e5db0f19551b29f7f15116e4c5a248
Olanti <olanti-p@yandex.ru>
2023-09-22 14:55:51 +0300

    Split up long message lines

--- src/version.cmake
+++ src/version.cmake
@@ -4,10 +4,9 @@ if (GIT_EXECUTABLE)
         OUTPUT_VARIABLE VERSION
         OUTPUT_STRIP_TRAILING_WHITESPACE)
 else ()
-    message(
-        WARNING
-            "Git binary not found. Build version will be set to NULL. Install Git package or use -DGIT_BINARY to set path to git binary."
-    )
+    message(WARNING "Git binary not found. \
+            Build version will be set to NULL. \
+            Install Git package or use -DGIT_BINARY to set path to git binary.")
     set(VERSION "NULL")
 endif ()
 
