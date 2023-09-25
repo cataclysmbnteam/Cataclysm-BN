dbdca164ccacf8639b8cc47b6177db13ad6321aa
scarf <greenscarf005@gmail.com>
2023-09-25 22:12:05 +0900

    ci: remove EXTRA_TEST_OPTS
    not used ATM

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -9,7 +9,7 @@ num_jobs=3
 function run_tests
 {
     # The grep suppresses lines that begin with "0.0## s:", which are timing lines for tests with a very short duration.
-    $WINE "$@" -d yes --use-colour yes --rng-seed time --error-format=github-action $EXTRA_TEST_OPTS | grep -Ev "^0\.0[0-9]{2} s:"
+    $WINE "$@" -d yes --use-colour yes --rng-seed time --error-format=github-action | grep -Ev "^0\.0[0-9]{2} s:"
 }
 
 # We might need binaries installed via pip, so ensure that our personal bin dir is on the PATH
