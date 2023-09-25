b4cbc2fece339a47d6d54754f3f9a0ff2597ab19
scarf <greenscarf005@gmail.com>
2023-09-25 23:36:50 +0900

    ci: build only and test only script
    see: https://github.com/CleverRaven/Cataclysm-DDA/pull/54435
    
    Co-authored-by: Brett Dong <brett.browning.dong@gmail.com>
    Co-authored-by: Alexey <irwiss@users.noreply.github.com>

---
+++ build-scripts/gha_compile_only.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Script made specifically for compiling without running tests on GitHub Actions
+
+echo "Using bash version $BASH_VERSION"
+set -exo pipefail
+
+num_jobs=3
+
+# We might need binaries installed via pip, so ensure that our personal bin dir is on the PATH
+export PATH=$HOME/.local/bin:$PATH
+
+if [ -n "$TEST_STAGE" ]
+then
+    build-scripts/lint-json.sh
+    make style-all-json-parallel RELEASE=1
+
+    tools/dialogue_validator.py data/json/npcs/* data/json/npcs/*/* data/json/npcs/*/*/*
+elif [ -n "$JUST_JSON" ]
+then
+    echo "Early exit on just-json change"
+    exit 0
+fi
+
+ccache --zero-stats
+# Increase cache size because debug builds generate large object files
+ccache -M 5G
+ccache --show-stats
+
+if [ "$CMAKE" = "1" ]
+then
+    if [ "$RELEASE" = "1" ]
+    then
+        build_type=MinSizeRel
+    else
+        build_type=Debug
+    fi
+
+    mkdir build
+    cd build
+    cmake \
+        -DBACKTRACE=ON \
+        ${COMPILER:+-DCMAKE_CXX_COMPILER=$COMPILER} \
+        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+        -DCMAKE_BUILD_TYPE="$build_type" \
+        -DTILES=${TILES:-0} \
+        -DSOUND=${SOUND:-0} \
+        ..
+
+    make -j$num_jobs
+else
+    make -j "$num_jobs" RELEASE=1 CCACHE=1 CROSS="$CROSS_COMPILATION" LINTJSON=0 FRAMEWORK=1
+
+    # For CI on macOS, patch the test binary so it can find SDL2 libraries.
+    if [[ ! -z "$OS" && "$OS" = "macos-12" ]]
+    then
+        file tests/cata_test
+        install_name_tool -add_rpath $HOME/Library/Frameworks tests/cata_test
+    fi
+fi
+
+# vim:tw=0
