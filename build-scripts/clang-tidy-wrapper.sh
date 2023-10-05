9b501fb9097de4182a2f80bef28591b9f7fad34e
scarf <greenscarf005@gmail.com>
2023-10-05 15:01:31 +0900

    ci: disable most of the new checks

--- build-scripts/clang-tidy-wrapper.sh
+++ build-scripts/clang-tidy-wrapper.sh
@@ -5,7 +5,8 @@ set -o pipefail
 
 plugin=build/tools/clang-tidy-plugin/libCataAnalyzerPlugin.so
 
-LD_PRELOAD=$plugin clang-tidy \
+clang-tidy \
   --load $plugin \
+  --config-file=build-scripts/.clang-tidy \
   --enable-check-profile \
   --store-check-profile=clang-tidy-trace "$@"
