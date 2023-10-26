akrieger/Cataclysm-DDA

Set neighbours according to oter rotation
Previously the mapgen feature of placing chunks based on neighbours used
the absolute cardinal directions on the map.  This is unhelpful when the
map is rotated.

Rotate the neighbours to match the rotation of the map so that these
checks make sense for the mapgen.

src/mapgen.cpp
@@ -7106,12 +7106,7 @@ std::pair<std::map<ter_id, int>, std::map<furn_id, int>> get_changed_ids_from_up
 
     fake_map tmp_map( t_dirt );
 
-    oter_id any = oter_id( "field" );
-    // just need a variable here, it doesn't need to be valid
-    const regional_settings dummy_settings;
-
-    mapgendata fake_md( any, any, any, any, any, any, any, any, any, any, 0, dummy_settings,
-                        tmp_map, any, {}, 0.0f, calendar::turn, nullptr );
+    mapgendata fake_md( tmp_map, mapgendata::dummy_settings );
 
     if( update_function->second.funcs()[0]->update_map( fake_md ) ) {
         for( const tripoint &pos : tmp_map.points_on_zlevel( fake_map::fake_map_z ) ) {

cataclysmbnteam/Cataclysm-BN

src/mapgen.cpp
@@ -6547,12 +6547,7 @@ std::pair<std::map<ter_id, int>, std::map<furn_id, int>> get_changed_ids_from_up
 
     ::fake_map fake_map( f_null, t_dirt, tr_null, fake_map_z );
 
-    oter_id any = oter_id( "field" );
-    // just need a variable here, it doesn't need to be valid
-    const regional_settings dummy_settings;
-
-    mapgendata fake_md( any, any, any, any, any, any, any, any,
-                        any, any, 0, dummy_settings, fake_map, any, 0.0f, calendar::turn, nullptr );
+    mapgendata fake_md( fake_map, mapgendata::dummy_settings );
 
     if( update_function->second[0]->update_map( fake_md ) ) {
         for( const tripoint &pos : fake_map.points_on_zlevel( fake_map_z ) ) {