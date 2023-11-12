deee27af9dff3c65fe18ec33b3ab8055206e33e7
Chaosvolt <chaosvolt@users.noreply.github.com>
2023-11-11 23:49:16 -0600

    Fix save/load breaking effect of relic stat boosts

--- src/character_turn.cpp
+++ src/character_turn.cpp
@@ -788,6 +788,20 @@ void Character::reset_stats()
         int_cur = 0;
     }
 
+    mod_str_bonus( enchantment_cache->calc_bonus( enchant_vals::mod::STRENGTH, get_str_base(), true ) );
+    mod_dex_bonus( enchantment_cache->calc_bonus( enchant_vals::mod::DEXTERITY, get_dex_base(),
+                   true ) );
+    mod_per_bonus( enchantment_cache->calc_bonus( enchant_vals::mod::PERCEPTION, get_per_base(),
+                   true ) );
+    mod_int_bonus( enchantment_cache->calc_bonus( enchant_vals::mod::INTELLIGENCE, get_int_base(),
+                   true ) );
+
+    mod_num_dodges_bonus( enchantment_cache->calc_bonus(
+                              enchant_vals::mod::BONUS_DODGE,
+                              get_num_dodges_base(),
+                              true
+                          ) );
+
     recalc_sight_limits();
     recalc_speed_bonus();
 }
