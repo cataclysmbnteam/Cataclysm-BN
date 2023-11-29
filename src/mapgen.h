CleverRaven/Cataclysm-DDA

Mapgen params to map special scope and palettes
Previously mapgen parameters only applied to a single OMT, and could not
be in palettes.  Remove both those limitations.

Now a parameter can have map_special scope (indeed, that's the default)
so that the same value is chosen consistently across any specific
overmap_special.

Moreover, parameters can be placed in palettes, and are automatically
extracted from all the palettes relevant to a particular piece of
mapgen.

src/mapgen.h
@@ -25,6 +25,7 @@ class map;
 template <typename Id> class mapgen_value;
 class mapgendata;
 class mission;
+struct mapgen_arguments;
 
 using building_gen_pointer = void ( * )( mapgendata & );
 
@@ -43,6 +44,9 @@ class mapgen_function
         virtual void setup() { } // throws
         virtual void check() const { }
         virtual void generate( mapgendata & ) = 0;
+        virtual mapgen_parameters get_mapgen_params( mapgen_parameter_scope ) const {
+            return {};
+        }
 };
 
 /////////////////////////////////////////////////////////////////////////////////
@@ -136,20 +140,6 @@ struct spawn_data {
     std::vector<point> patrol_points_rel_ms;
 };
 
-class mapgen_parameter
-{
-    public:
-        void deserialize( JsonIn & );
-
-        cata_variant_type type() const;
-        cata_variant get( const mapgendata &md ) const;
-    private:
-        cata_variant_type type_;
-        // Using a pointer here mostly to move the definition of mapgen_value to the
-        // cpp file
-        std::shared_ptr<const mapgen_value<std::string>> default_;
-};
-
 /**
  * Basic mapgen object. It is supposed to place or do something on a specific square on the map.
  * Inherit from this class and implement the @ref apply function.
@@ -180,8 +170,7 @@ class jmapgen_piece
             return false;
         }
         /** Sanity-check this piece */
-        virtual void check( const std::string &/*context*/,
-                            const std::unordered_map<std::string, mapgen_parameter> & ) const { }
+        virtual void check( const std::string &/*context*/, const mapgen_parameters & ) const { }
         /** Place something on the map from mapgendata &dat, at (x,y). */
         virtual void apply( const mapgendata &dat, const jmapgen_int &x, const jmapgen_int &y
                           ) const = 0;
@@ -240,6 +229,8 @@ class mapgen_palette
     public:
         palette_id id;
 
+        mapgen_parameters parameters;
+
         /**
          * The mapping from character (key) to a list of things that should be placed. This is
          * similar to objects, but it uses key to get the actual position where to place things
@@ -263,7 +254,8 @@ class mapgen_palette
         /**
          * Loads a palette object and returns it. Doesn't save it anywhere.
          */
-        static mapgen_palette load_temp( const JsonObject &jo, const std::string &src );
+        static mapgen_palette load_temp( const JsonObject &jo, const std::string &src,
+                                         const std::string &context );
         /**
          * Load a palette object and adds it to the global set of palettes.
          * If "palette" field is specified, those palettes will be loaded recursively.
@@ -279,15 +271,16 @@ class mapgen_palette
 
         static void reset();
     private:
-        static mapgen_palette load_internal( const JsonObject &jo, const std::string &src, bool require_id,
-                                             bool allow_recur );
+        static mapgen_palette load_internal(
+            const JsonObject &jo, const std::string &src, const std::string &context,
+            bool require_id, bool allow_recur );
 
         /**
          * Adds a palette to this one. New values take preference over the old ones.
          *
          */
-        void add( const palette_id &rh );
-        void add( const mapgen_palette &rh );
+        void add( const palette_id &rh, const std::string &context = {} );
+        void add( const mapgen_palette &rh, const std::string &context = {} );
 };
 
 struct jmapgen_objects {
@@ -314,8 +307,7 @@ struct jmapgen_objects {
         void load_objects( const JsonObject &jsi, const std::string &member_name,
                            const std::string &context );
 
-        void check( const std::string &context,
-                    const std::unordered_map<std::string, mapgen_parameter> & ) const;
+        void check( const std::string &context, const mapgen_parameters & ) const;
 
         void apply( const mapgendata &dat ) const;
         void apply( const mapgendata &dat, const point &offset ) const;
@@ -346,8 +338,6 @@ class mapgen_function_json_base
     private:
         json_source_location jsrcloc;
         std::string context_;
-
-        std::unordered_map<std::string, mapgen_parameter> parameters;
     protected:
         mapgen_function_json_base( const json_source_location &jsrcloc, const std::string &context );
         virtual ~mapgen_function_json_base();
@@ -361,8 +351,7 @@ class mapgen_function_json_base
 
         void check_common() const;
 
-        std::unordered_map<std::string, cata_variant>
-        get_param_values( const mapgendata &md ) const;
+        mapgen_arguments get_args( const mapgendata &md, mapgen_parameter_scope ) const;
 
         bool is_ready;
 
@@ -371,6 +360,8 @@ class mapgen_function_json_base
         std::vector<jmapgen_setmap> setmap_points;
 
         jmapgen_objects objects;
+
+        mapgen_parameters parameters;
 };
 
 class mapgen_function_json : public mapgen_function_json_base, public virtual mapgen_function
@@ -379,6 +370,7 @@ class mapgen_function_json : public mapgen_function_json_base, public virtual ma
         void setup() override;
         void check() const override;
         void generate( mapgendata & ) override;
+        mapgen_parameters get_mapgen_params( mapgen_parameter_scope ) const override;
         mapgen_function_json( const json_source_location &jsrcloc, int w, const std::string &context,
                               const point &grid_offset = point_zero );
         ~mapgen_function_json() override = default;

src/mapgendata.cpp
@@ -8,12 +8,21 @@
 #include "point.h"
 #include "regional_settings.h"
 
+void mapgen_arguments::merge( const mapgen_arguments &other )
+{
+    for( const std::pair<const std::string, cata_variant> &p : other.map ) {
+        map[p.first] = p.second;
+    }
+}
+
 mapgendata::mapgendata( oter_id north, oter_id east, oter_id south, oter_id west,
                         oter_id northeast, oter_id southeast, oter_id southwest, oter_id northwest,
-                        oter_id up, oter_id down, int z, const regional_settings &rsettings, map &mp,
-                        const oter_id &terrain_type, const float density, const time_point &when,
+                        oter_id up, oter_id down, int z, const regional_settings &rsettings,
+                        map &mp, const oter_id &terrain_type, const mapgen_arguments &args,
+                        const float density, const time_point &when,
                         ::mission *const miss )
-    : terrain_type_( terrain_type ), density_( density ), when_( when ), mission_( miss ), zlevel_( z )
+    : terrain_type_( terrain_type ), density_( density ), when_( when ), mission_( miss )
+    , zlevel_( z ), mapgen_args_( args )
     , t_nesw{ north, east, south, west, northeast, southeast, southwest, northwest }
     , t_above( up )
     , t_below( down )
@@ -37,8 +46,25 @@ mapgendata::mapgendata( const tripoint_abs_omt &over, map &m, const float densit
                   overmap_buffer.ter( over + tripoint_above ),
                   overmap_buffer.ter( over + tripoint_below ),
                   over.z(), overmap_buffer.get_settings( over ), m,
-                  overmap_buffer.ter( over ), density, when, miss )
+                  overmap_buffer.ter( over ), mapgen_arguments(), density,
+                  when, miss )
 {
+    if( cata::optional<mapgen_arguments> *maybe_args = overmap_buffer.mapgen_args( over ) ) {
+        if( *maybe_args ) {
+            mapgen_args_ = **maybe_args;
+        } else {
+            // We are the first omt from this overmap_special to be generated,
+            // so now is the time to generate the arguments
+            if( cata::optional<overmap_special_id> s = overmap_buffer.overmap_special_at( over ) ) {
+                const overmap_special &special = **s;
+                *maybe_args = special.get_args( *this );
+                mapgen_args_ = **maybe_args;
+            } else {
+                debugmsg( "mapgen params expected but no overmap special found for terrain %s",
+                          terrain_type_.id().str() );
+            }
+        }
+    }
 }
 
 mapgendata::mapgendata( const mapgendata &other, const oter_id &other_id ) : mapgendata( other )
@@ -47,10 +73,10 @@ mapgendata::mapgendata( const mapgendata &other, const oter_id &other_id ) : map
 }
 
 mapgendata::mapgendata( const mapgendata &other,
-                        const std::unordered_map<std::string, cata_variant> &mapgen_params ) :
+                        const mapgen_arguments &mapgen_args ) :
     mapgendata( other )
 {
-    mapgen_params_ = mapgen_params;
+    mapgen_args_.merge( mapgen_args );
 }
 
 void mapgendata::set_dir( int dir_in, int val )

src/overmap.h
@@ -19,6 +19,7 @@
 #include "coordinates.h"
 #include "enums.h"
 #include "game_constants.h"
+#include "mapgendata.h"
 #include "memory_fast.h"
 #include "mongroup.h"
 #include "monster.h"
@@ -244,6 +245,7 @@ class overmap
 
         void ter_set( const tripoint_om_omt &p, const oter_id &id );
         const oter_id &ter( const tripoint_om_omt &p ) const;
+        cata::optional<mapgen_arguments> *mapgen_args( const tripoint_om_omt & );
         bool &seen( const tripoint_om_omt &p );
         bool seen( const tripoint_om_omt &p ) const;
         bool &explored( const tripoint_om_omt &p );
@@ -368,6 +370,12 @@ class overmap
         // Records location where mongroups are not allowed to spawn during worldgen.
         std::unordered_set<tripoint_om_omt> safe_at_worldgen;
 
+        // Records mapgen parameters required at the overmap special level
+        // These are lazily evaluated; empty optional means that they have yet
+        // to be evaluated.
+        cata::colony<cata::optional<mapgen_arguments>> mapgen_arg_storage;
+        std::unordered_map<tripoint_om_omt, cata::optional<mapgen_arguments> *> mapgen_args_index;
+
         regional_settings settings;
 
         oter_id get_default_terrain( int z ) const;
@@ -467,6 +475,7 @@ class overmap
                        const tripoint_om_omt &p ) const;
         bool check_overmap_special_type( const overmap_special_id &id,
                                          const tripoint_om_omt &location ) const;
+        cata::optional<overmap_special_id> overmap_special_at( const tripoint_om_omt &p ) const;
         void chip_rock( const tripoint_om_omt &p );
 
         void polish_river();

src/overmapbuffer.h
@@ -28,6 +28,7 @@ class npc;
 class overmap;
 class overmap_special_batch;
 class vehicle;
+struct mapgen_arguments;
 struct mongroup;
 struct om_vehicle;
 struct radio_tower;
@@ -145,6 +146,7 @@ class overmapbuffer
          */
         const oter_id &ter( const tripoint_abs_omt &p );
         void ter_set( const tripoint_abs_omt &p, const oter_id &id );
+        cata::optional<mapgen_arguments> *mapgen_args( const tripoint_abs_omt & );
         /**
          * Uses global overmap terrain coordinates.
          */
@@ -515,6 +517,7 @@ class overmapbuffer
         bool check_ot( const std::string &otype, ot_match_type match_type,
                        const tripoint_abs_omt &p );
         bool check_overmap_special_type( const overmap_special_id &id, const tripoint_abs_omt &loc );
+        cata::optional<overmap_special_id> overmap_special_at( const tripoint_abs_omt & );
 
         /**
         * These versions of the check_* methods will only check existing overmaps, and

cataclysmbnteam/Cataclysm-BN

src/mapgen.h
@@ -15,6 +15,7 @@
 #include "coordinates.h"
 #include "json.h"
 #include "memory_fast.h"
+#include "mapgen_parameter.h"
 #include "point.h"
 #include "regional_settings.h"
 #include "type_id.h"
@@ -46,6 +47,9 @@ class mapgen_function
         virtual void setup() { } // throws
         virtual void check( const std::string & /*oter_name*/ ) const { }
         virtual void generate( mapgendata & ) = 0;
+        virtual mapgen_parameters get_mapgen_params( mapgen_parameter_scope ) const {
+            return {};
+        }
 };
 
 /////////////////////////////////////////////////////////////////////////////////
@@ -138,20 +142,6 @@ struct spawn_data {
     std::vector<point> patrol_points_rel_ms;
 };
 
-class mapgen_parameter
-{
-    public:
-        void deserialize( JsonIn & );
-
-        cata_variant_type type() const;
-        cata_variant get( const mapgendata &md ) const;
-    private:
-        cata_variant_type type_;
-        // Using a pointer here mostly to move the definition of mapgen_value to the
-        // cpp file
-        std::shared_ptr<const mapgen_value<std::string>> default_;
-};
-
 /**
  * Basic mapgen object. It is supposed to place or do something on a specific square on the map.
  * Inherit from this class and implement the @ref apply function.
@@ -182,8 +172,7 @@ class jmapgen_piece
             return false;
         }
         /** Sanity-check this piece */
-        virtual void check( const std::string &/*oter_name*/,
-                            const std::unordered_map<std::string, mapgen_parameter> & ) const { }
+        virtual void check( const std::string &/*oter_name*/, const mapgen_parameters & ) const { }
         /** Place something on the map from mapgendata &dat, at (x,y). */
         virtual void apply( const mapgendata &dat, const jmapgen_int &x, const jmapgen_int &y
                           ) const = 0;
@@ -242,6 +231,8 @@ class mapgen_palette
     public:
         palette_id id;
 
+        mapgen_parameters parameters;
+
         /**
          * The mapping from character (key) to a list of things that should be placed. This is
          * similar to objects, but it uses key to get the actual position where to place things
@@ -266,7 +257,8 @@ class mapgen_palette
         /**
          * Loads a palette object and returns it. Doesn't save it anywhere.
          */
-        static mapgen_palette load_temp( const JsonObject &jo, const std::string &src );
+        static mapgen_palette load_temp( const JsonObject &jo, const std::string &src,
+                                         const std::string &context );
         /**
          * Load a palette object and adds it to the global set of palettes.
          * If "palette" field is specified, those palettes will be loaded recursively.
@@ -282,15 +274,16 @@ class mapgen_palette
 
         static void check_definitions();
     private:
-        static mapgen_palette load_internal( const JsonObject &jo, const std::string &src, bool require_id,
-                                             bool allow_recur );
+        static mapgen_palette load_internal(
+            const JsonObject &jo, const std::string &src, const std::string &context,
+            bool require_id, bool allow_recur );
 
         /**
          * Adds a palette to this one. New values take preference over the old ones.
          *
          */
-        void add( const palette_id &rh );
-        void add( const mapgen_palette &rh );
+        void add( const palette_id &rh, const std::string &context = {} );
+        void add( const mapgen_palette &rh, const std::string &context = {} );
 };
 
 struct jmapgen_objects {
@@ -316,8 +309,7 @@ struct jmapgen_objects {
         template<typename PieceType>
         void load_objects( const JsonObject &jsi, const std::string &member_name );
 
-        void check( const std::string &oter_name,
-                    const std::unordered_map<std::string, mapgen_parameter> & ) const;
+        void check( const std::string &oter_name, const mapgen_parameters & ) const;
 
         void apply( const mapgendata &dat ) const;
         void apply( const mapgendata &dat, const point &offset ) const;
@@ -349,7 +341,6 @@ class mapgen_function_json_base
     private:
         pimpl<json_source_location> jsrcloc;
 
-        std::unordered_map<std::string, mapgen_parameter> parameters;
     protected:
         explicit mapgen_function_json_base( const json_source_location &jsrcloc );
         virtual ~mapgen_function_json_base();
@@ -363,8 +354,7 @@ class mapgen_function_json_base
 
         void check_common( const std::string &oter_name ) const;
 
-        std::unordered_map<std::string, cata_variant>
-        get_param_values( const mapgendata &md ) const;
+        mapgen_arguments get_args( const mapgendata &md, mapgen_parameter_scope ) const;
 
         bool is_ready;
 
@@ -374,6 +364,8 @@ class mapgen_function_json_base
         std::vector<jmapgen_setmap> setmap_points;
 
         jmapgen_objects objects;
+
+        mapgen_parameters parameters;
 };
 
 class mapgen_function_json : public mapgen_function_json_base, public virtual mapgen_function
@@ -382,6 +374,7 @@ class mapgen_function_json : public mapgen_function_json_base, public virtual ma
         void setup() override;
         void check( const std::string &oter_name ) const override;
         void generate( mapgendata & ) override;
+        mapgen_parameters get_mapgen_params( mapgen_parameter_scope ) const override;
         mapgen_function_json( const json_source_location &jsrcloc, int w, point grid_offset,
                               point grid_total );
         ~mapgen_function_json() override = default;

src/mapgendata.cpp
@@ -6,11 +6,19 @@
 #include "map.h"
 #include "mapdata.h"
 #include "omdata.h"
+#include "overmap_special.h"
 #include "overmapbuffer.h"
 #include "point.h"
 #include "regional_settings.h"
 
 static const regional_settings dummy_regional_settings;
+
+void mapgen_arguments::merge( const mapgen_arguments &other )
+{
+    for( const std::pair<const std::string, cata_variant> &p : other.map ) {
+        map[p.first] = p.second;
+    }
+}
 
 mapgendata::mapgendata( map &mp, dummy_settings_t )
     : density_( 0 )
@@ -59,6 +67,22 @@ mapgendata::mapgendata( const tripoint_abs_omt &over, map &mp, const float densi
             joins.emplace( rotated_dir, *join );
         }
     }
+    if( std::optional<mapgen_arguments> *maybe_args = overmap_buffer.mapgen_args( over ) ) {
+        if( *maybe_args ) {
+            mapgen_args_ = **maybe_args;
+        } else {
+            // We are the first omt from this overmap_special to be generated,
+            // so now is the time to generate the arguments
+            if( std::optional<overmap_special_id> s = overmap_buffer.overmap_special_at( over ) ) {
+                const overmap_special &special = **s;
+                *maybe_args = special.get_args( *this );
+                mapgen_args_ = **maybe_args;
+            } else {
+                debugmsg( "mapgen params expected but no overmap special found for terrain %s",
+                          terrain_type_.id().str() );
+            }
+        }
+    }
 }
 
 mapgendata::mapgendata( const mapgendata &other, const oter_id &other_id ) : mapgendata( other )
@@ -67,10 +91,10 @@ mapgendata::mapgendata( const mapgendata &other, const oter_id &other_id ) : map
 }
 
 mapgendata::mapgendata( const mapgendata &other,
-                        const std::unordered_map<std::string, cata_variant> &mapgen_params ) :
+                        const mapgen_arguments &mapgen_args ) :
     mapgendata( other )
 {
-    mapgen_params_ = mapgen_params;
+    mapgen_args_.merge( mapgen_args );
 }
 
 void mapgendata::set_dir( int dir_in, int val )

src/overmap.h
@@ -23,6 +23,7 @@
 #include "enums.h"
 #include "enum_conversions.h"
 #include "game_constants.h"
+#include "mapgendata.h"
 #include "memory_fast.h"
 #include "mongroup.h"
 #include "omdata.h"
@@ -231,6 +232,7 @@ class overmap
         void ter_set( const tripoint_om_omt &p, const oter_id &id );
         const oter_id &ter( const tripoint_om_omt &p ) const;
         std::string *join_used_at( const om_pos_dir & );
+        std::optional<mapgen_arguments> *mapgen_args( const tripoint_om_omt & );
         bool &seen( const tripoint_om_omt &p );
         bool seen( const tripoint_om_omt &p ) const;
         bool &explored( const tripoint_om_omt &p );
@@ -370,6 +372,11 @@ class overmap
         // Records the joins that were chosen during placement of a mutable
         // special, so that it can be queried later by mapgen
         std::unordered_map<om_pos_dir, std::string> joins_used;
+        // Records mapgen parameters required at the overmap special level
+        // These are lazily evaluated; empty optional means that they have yet
+        // to be evaluated.
+        std::vector<std::optional<mapgen_arguments>> mapgen_arg_storage;
+        std::unordered_map<tripoint_om_omt, std::optional<mapgen_arguments> *> mapgen_args_index;
 
         oter_id get_default_terrain( int z ) const;
 
@@ -458,6 +465,7 @@ class overmap
                        const tripoint_om_omt &p ) const;
         bool check_overmap_special_type( const overmap_special_id &id,
                                          const tripoint_om_omt &location ) const;
+        std::optional<overmap_special_id> overmap_special_at( const tripoint_om_omt &p ) const;
 
         void polish_rivers( const overmap *north, const overmap *east, const overmap *south,
                             const overmap *west );

src/overmapbuffer.h
@@ -31,6 +31,7 @@ class overmap_special;
 class overmap_special_batch;
 class throbber_popup;
 class vehicle;
+struct mapgen_arguments;
 struct mongroup;
 struct om_vehicle;
 struct radio_tower;
@@ -191,6 +192,7 @@ class overmapbuffer
         const oter_id &ter_existing( const tripoint_abs_omt &p );
         void ter_set( const tripoint_abs_omt &p, const oter_id &id );
         std::string *join_used_at( const std::pair<tripoint_abs_omt, cube_direction> & );
+        std::optional<mapgen_arguments> *mapgen_args( const tripoint_abs_omt & );
         /**
          * Uses global overmap terrain coordinates.
          */
@@ -564,6 +566,7 @@ class overmapbuffer
         bool check_ot( const std::string &otype, ot_match_type match_type,
                        const tripoint_abs_omt &p );
         bool check_overmap_special_type( const overmap_special_id &id, const tripoint_abs_omt &loc );
+        std::optional<overmap_special_id> overmap_special_at( const tripoint_abs_omt & );
 
         /**
         * These versions of the check_* methods will only check existing overmaps, and