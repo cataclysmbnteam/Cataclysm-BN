akrieger/Cataclysm-DDA

Overwrite mapgen args when placing special
Previously when placing an overmap special on top of an existing overmap
special (e.g. via the debug menu) the parameters for the old one would
persist and the new one would lack its parameters.  Instead, overwrite
the old with the new.

src/overmap.cpp
@@ -4162,7 +4162,7 @@ void overmap::place_special(
 
         overmap_special_placements[location] = special.id;
         ter_set( location, tid );
-        mapgen_args_index.emplace( location, mapgen_args_p );
+        mapgen_args_index[location] = mapgen_args_p;
         if( is_safe_zone ) {
             safe_at_worldgen.emplace( location );
         }

cataclysmbnteam/Cataclysm-BN

src/overmap.cpp
@@ -5434,7 +5434,7 @@ std::vector<tripoint_om_omt> overmap::place_special(
     mapgen_arg_storage.emplace_back();
     // Link grid
     for( const tripoint_om_omt &location : result ) {
-        mapgen_args_index.emplace( location, args_index );
+        mapgen_args_index[location] = args_index;
         overmap_special_placements[location] = special.id;
         if( grid ) {
             for( size_t i = 0; i < six_cardinal_directions.size(); i++ ) {