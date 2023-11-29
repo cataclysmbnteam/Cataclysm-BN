akrieger/Cataclysm-DDA

Support more neighbours in jmapgen_nested (#51804)
Previously only the cardinal directions and above were supported; now
all eight horizontal neighbours, above, and below are.

src/line.cpp
@@ -383,6 +383,52 @@ std::vector<tripoint> continue_line( const std::vector<tripoint> &line, const in
     return line_to( line.back(), move_along_line( line.back(), line, distance ) );
 }
 
+namespace io
+{
+
+template<>
+std::string enum_to_string<direction>( direction data )
+{
+    switch( data ) {
+        // *INDENT-OFF*
+        case direction::ABOVENORTHWEST: return "above_north_west";
+        case direction::NORTHWEST: return "north_west";
+        case direction::BELOWNORTHWEST: return "below_north_west";
+        case direction::ABOVENORTH: return "above_north";
+        case direction::NORTH: return "north";
+        case direction::BELOWNORTH: return "below_north";
+        case direction::ABOVENORTHEAST: return "above_north_east";
+        case direction::NORTHEAST: return "north_east";
+        case direction::BELOWNORTHEAST: return "below_north_east";
+
+        case direction::ABOVEWEST: return "above_west";
+        case direction::WEST: return "west";
+        case direction::BELOWWEST: return "below_west";
+        case direction::ABOVECENTER: return "above";
+        case direction::CENTER: return "center";
+        case direction::BELOWCENTER: return "below";
+        case direction::ABOVEEAST: return "above_east";
+        case direction::EAST: return "east";
+        case direction::BELOWEAST: return "below_east";
+
+        case direction::ABOVESOUTHWEST: return "above_south_west";
+        case direction::SOUTHWEST: return "south_west";
+        case direction::BELOWSOUTHWEST: return "below_south_west";
+        case direction::ABOVESOUTH: return "above_south";
+        case direction::SOUTH: return "south";
+        case direction::BELOWSOUTH: return "below_south";
+        case direction::ABOVESOUTHEAST: return "above_south_east";
+        case direction::SOUTHEAST: return "south_east";
+        case direction::BELOWSOUTHEAST: return "below_south_east";
+        // *INDENT-ON*
+        case direction::last:
+            break;
+    }
+    cata_fatal( "Invalid direction" );
+}
+
+} // namespace io
+
 direction direction_from( const point &p ) noexcept
 {
     return static_cast<direction>( make_xyz( tripoint( p, 0 ) ) );
@@ -442,6 +488,8 @@ point direction_XY( const direction dir )
         case direction::ABOVESOUTHEAST:
         case direction::BELOWSOUTHEAST:
             return point_south_east;
+        case direction::last:
+            cata_fatal( "Invalid direction" );
     }
 
     return point_zero;

src/mapgen.cpp
@@ -15,6 +15,7 @@
 #include <type_traits>
 #include <unordered_map>
 
+#include "all_enum_values.h"
 #include "calendar.h"
 #include "cata_assert.h"
 #include "catacharset.h"
@@ -2689,53 +2690,38 @@ class jmapgen_nested : public jmapgen_piece
         class neighborhood_check
         {
             private:
-                // To speed up the most common case: no checks
-                bool has_any = false;
-                std::array<std::set<oter_str_id>, om_direction::size> neighbors;
-                std::set<oter_str_id> above;
+                std::unordered_map<direction, cata::flat_set<oter_str_id>> neighbors;
             public:
                 explicit neighborhood_check( const JsonObject &jsi ) {
-                    for( om_direction::type dir : om_direction::all ) {
-                        int index = static_cast<int>( dir );
-                        neighbors[index] = jsi.get_tags<oter_str_id>( om_direction::id( dir ) );
-                        has_any |= !neighbors[index].empty();
-
-                        above = jsi.get_tags<oter_str_id>( "above" );
-                        has_any |= !above.empty();
+                    for( direction dir : all_enum_values<direction>() ) {
+                        cata::flat_set<oter_str_id> dir_neighbours =
+                            jsi.get_tags<oter_str_id, cata::flat_set<oter_str_id>>(
+                                io::enum_to_string( dir ) );
+                        if( !dir_neighbours.empty() ) {
+                            neighbors[dir] = std::move( dir_neighbours );
+                        }
                     }
                 }
 
                 bool test( const mapgendata &dat ) const {
-                    if( !has_any ) {
-                        return true;
-                    }
+                    for( const std::pair<const direction, cata::flat_set<oter_str_id>> &p :
+                         neighbors ) {
+                        const direction dir = p.first;
+                        const cata::flat_set<oter_str_id> &allowed_neighbors = p.second;
 
-                    bool all_directions_match  = true;
-                    for( om_direction::type dir : om_direction::all ) {
-                        int index = static_cast<int>( dir );
-                        const std::set<oter_str_id> &allowed_neighbors = neighbors[index];
-
-                        if( allowed_neighbors.empty() ) {
-                            continue;  // no constraints on this direction, skip.
-                        }
+                        cata_assert( !allowed_neighbors.empty() );
 
                         bool this_direction_matches = false;
                         for( const oter_str_id &allowed_neighbor : allowed_neighbors ) {
-                            this_direction_matches |= is_ot_match( allowed_neighbor.str(), dat.neighbor_at( dir ).id(),
-                                                                   ot_match_type::contains );
+                            this_direction_matches |=
+                                is_ot_match( allowed_neighbor.str(), dat.neighbor_at( dir ).id(),
+                                             ot_match_type::contains );
                         }
-                        all_directions_match &= this_direction_matches;
-                    }
-
-                    if( !above.empty() ) {
-                        bool above_matches = false;
-                        for( const oter_str_id &allowed_neighbor : above ) {
-                            above_matches |= is_ot_match( allowed_neighbor.str(), dat.above().id(), ot_match_type::contains );
+                        if( !this_direction_matches ) {
+                            return false;
                         }
-                        all_directions_match &= above_matches;
                     }
-
-                    return all_directions_match;
+                    return true;
                 }
         };
 

cataclysmbnteam/Cataclysm-BN

src/line.cpp
@@ -13,6 +13,7 @@
 #include "string_formatter.h"
 #include "string_utils.h"
 #include "enums.h"
+#include "enum_conversions.h"
 #include "point_float.h"
 
 double iso_tangent( double distance, units::angle vertex )
@@ -410,6 +411,53 @@ std::vector<tripoint> continue_line( const std::vector<tripoint> &line, const in
     return line_to( line.back(), move_along_line( line.back(), line, distance ) );
 }
 
+namespace io
+{
+
+template<>
+std::string enum_to_string<direction>( direction data )
+{
+    switch( data ) {
+        // *INDENT-OFF*
+        case direction::ABOVENORTHWEST: return "above_north_west";
+        case direction::NORTHWEST: return "north_west";
+        case direction::BELOWNORTHWEST: return "below_north_west";
+        case direction::ABOVENORTH: return "above_north";
+        case direction::NORTH: return "north";
+        case direction::BELOWNORTH: return "below_north";
+        case direction::ABOVENORTHEAST: return "above_north_east";
+        case direction::NORTHEAST: return "north_east";
+        case direction::BELOWNORTHEAST: return "below_north_east";
+
+        case direction::ABOVEWEST: return "above_west";
+        case direction::WEST: return "west";
+        case direction::BELOWWEST: return "below_west";
+        case direction::ABOVECENTER: return "above";
+        case direction::CENTER: return "center";
+        case direction::BELOWCENTER: return "below";
+        case direction::ABOVEEAST: return "above_east";
+        case direction::EAST: return "east";
+        case direction::BELOWEAST: return "below_east";
+
+        case direction::ABOVESOUTHWEST: return "above_south_west";
+        case direction::SOUTHWEST: return "south_west";
+        case direction::BELOWSOUTHWEST: return "below_south_west";
+        case direction::ABOVESOUTH: return "above_south";
+        case direction::SOUTH: return "south";
+        case direction::BELOWSOUTH: return "below_south";
+        case direction::ABOVESOUTHEAST: return "above_south_east";
+        case direction::SOUTHEAST: return "south_east";
+        case direction::BELOWSOUTHEAST: return "below_south_east";
+        // *INDENT-ON*
+        case direction::last:
+            break;
+    }
+    debugmsg( "Invalid direction" );
+    abort();
+}
+
+} // namespace io
+
 direction direction_from( point p ) noexcept
 {
     return static_cast<direction>( make_xyz( tripoint( p, 0 ) ) );
@@ -487,6 +535,9 @@ tripoint displace( direction dir )
             return point_south_east + tripoint_above;
         case direction::BELOWSOUTHEAST:
             return point_south_east + tripoint_below;
+        case direction::last:
+            debugmsg( "Invalid direction" );
+            abort();
     }
 
     return tripoint_zero;
@@ -531,6 +582,9 @@ point displace_XY( const direction dir )
         case direction::ABOVESOUTHEAST:
         case direction::BELOWSOUTHEAST:
             return point_south_east;
+        case direction::last:
+            debugmsg( "Invalid direction" );
+            abort();
     }
 
     return point_zero;

src/mapgen.cpp
@@ -2665,25 +2665,22 @@ class jmapgen_nested : public jmapgen_piece
         class neighbor_oter_check
         {
             private:
-                // To speed up the most common case: no checks
-                bool has_any = false;
-                std::array<std::set<oter_type_str_id>, om_direction::size> neighbors;
-                std::set<oter_type_str_id> above;
+                std::unordered_map<direction, cata::flat_set<oter_type_str_id>> neighbors;
             public:
-                neighbor_oter_check( const JsonObject &jsi ) {
-                    for( om_direction::type dir : om_direction::all ) {
-                        int index = static_cast<int>( dir );
-                        neighbors[index] = jsi.get_tags<oter_type_str_id>( io::enum_to_string( dir ) );
-                        has_any |= !neighbors[index].empty();
-
-                        above = jsi.get_tags<oter_type_str_id>( "above" );
-                        has_any |= !above.empty();
+                explicit neighbor_oter_check( const JsonObject &jsi ) {
+                    for( direction dir : all_enum_values<direction>() ) {
+                        cata::flat_set<oter_type_str_id> dir_neighbours =
+                            jsi.get_tags<oter_type_str_id, cata::flat_set<oter_type_str_id>>(
+                                io::enum_to_string( dir ) );
+                        if( !dir_neighbours.empty() ) {
+                            neighbors[dir] = std::move( dir_neighbours );
+                        }
                     }
                 }
 
                 void check( const std::string &oter_name ) const {
-                    for( const std::set<oter_type_str_id> &p : neighbors ) {
-                        for( const oter_type_str_id &id : p ) {
+                    for( const auto &p : neighbors ) {
+                        for( const oter_type_str_id &id : p.second ) {
                             if( !id.is_valid() ) {
                                 debugmsg( "Invalid oter_type_str_id '%s' in %s", id.str(), oter_name );
                             }
@@ -2692,36 +2689,24 @@ class jmapgen_nested : public jmapgen_piece
                 }
 
                 bool test( const mapgendata &dat ) const {
-                    if( !has_any ) {
-                        return true;
-                    }
+                    for( const std::pair<const direction, cata::flat_set<oter_type_str_id>> &p :
+                         neighbors ) {
+                        const direction dir = p.first;
+                        const cata::flat_set<oter_type_str_id> &allowed_neighbors = p.second;
 
-                    bool all_directions_match  = true;
-                    for( om_direction::type dir : om_direction::all ) {
-                        int index = static_cast<int>( dir );
-                        const std::set<oter_type_str_id> &allowed_neighbors = neighbors[index];
-
-                        if( allowed_neighbors.empty() ) {
-                            continue;  // no constraints on this direction, skip.
-                        }
+                        assert( !allowed_neighbors.empty() );
 
                         bool this_direction_matches = false;
                         for( const oter_type_str_id &allowed_neighbor : allowed_neighbors ) {
-                            this_direction_matches |= is_ot_match( allowed_neighbor.str(), dat.neighbor_at( dir ).id(),
-                                                                   ot_match_type::contains );
+                            this_direction_matches |=
+                                is_ot_match( allowed_neighbor.str(), dat.neighbor_at( dir ).id(),
+                                             ot_match_type::contains );
                         }
-                        all_directions_match &= this_direction_matches;
-                    }
-
-                    if( !above.empty() ) {
-                        bool above_matches = false;
-                        for( const oter_type_str_id &allowed_neighbor : above ) {
-                            above_matches |= is_ot_match( allowed_neighbor.str(), dat.above().id(), ot_match_type::contains );
+                        if( !this_direction_matches ) {
+                            return false;
                         }
-                        all_directions_match &= above_matches;
                     }
-
-                    return all_directions_match;
+                    return true;
                 }
         };
 