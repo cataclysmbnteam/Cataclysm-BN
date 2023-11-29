CleverRaven/Cataclysm-DDA

Fix a couple of serialization bugs
Two issues with serialization:
- If the entire stream was just a single integer then it would cause an
  infinite loop when reading.
- When reading a sequence of optionals, if one optional was empty, it
  would cause the sequence to grow until memory was exhausted without
  ever consuming that input.

Fix both these issues, and add unit tests.

tests/json_test.cpp
@@ -815,3 +815,23 @@ TEST_CASE( "item_colony_ser_deser", "[json][item]" )
         }
     }
 }
+
+TEST_CASE( "serialize_optional", "[json]" )
+{
+    SECTION( "simple_empty_optional" ) {
+        cata::optional<int> o;
+        test_serialization( o, "null" );
+    }
+    SECTION( "optional_of_int" ) {
+        cata::optional<int> o( 7 );
+        test_serialization( o, "7" );
+    }
+    SECTION( "vector_of_empty_optional" ) {
+        std::vector<cata::optional<int>> v( 3 );
+        test_serialization( v, "[null,null,null]" );
+    }
+    SECTION( "vector_of_optional_of_int" ) {
+        std::vector<cata::optional<int>> v{ { 1 }, { 2 }, { 3 } };
+        test_serialization( v, "[1,2,3]" );
+    }
+}

cataclysmbnteam/Cataclysm-BN

tests/json_test.cpp
@@ -461,3 +461,23 @@ TEST_CASE( "jsonin_get_string", "[json]" )
             R"(       ar")" "\n" ),
         R"("foo\nbar")", 5 );
 }
+
+TEST_CASE( "serialize_optional", "[json]" )
+{
+    SECTION( "simple_empty_optional" ) {
+        std::optional<int> o;
+        test_serialization( o, "null" );
+    }
+    SECTION( "optional_of_int" ) {
+        std::optional<int> o( 7 );
+        test_serialization( o, "7" );
+    }
+    SECTION( "vector_of_empty_optional" ) {
+        std::vector<std::optional<int>> v( 3 );
+        test_serialization( v, "[null,null,null]" );
+    }
+    SECTION( "vector_of_optional_of_int" ) {
+        std::vector<std::optional<int>> v{ { 1 }, { 2 }, { 3 } };
+        test_serialization( v, "[1,2,3]" );
+    }
+}
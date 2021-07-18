7f8f0e32eb63baadf4aa6105f5c2dcfa8965e751
John Bytheway <52664+jbytheway@users.noreply.github.com>
2021-07-17 20:48:14 -0400

    Fix a couple of serialization bugs
    Two issues with serialization:
    - If the entire stream was just a single integer then it would cause an
      infinite loop when reading.
    - When reading a sequence of optionals, if one optional was empty, it
      would cause the sequence to grow until memory was exhausted without
      ever consuming that input.
    
    Fix both these issues, and add unit tests.

--- src/json.h
+++ src/json.h
@@ -266,6 +266,7 @@ class JsonIn
         // optionally-fatal reading into values by reference
         // returns true if the data was read successfully, false otherwise
         // if throw_on_error then throws JsonError rather than returning false.
+        bool read_null( bool throw_on_error = false );
         bool read( bool &b, bool throw_on_error = false );
         bool read( char &c, bool throw_on_error = false );
         bool read( signed char &c, bool throw_on_error = false );
@@ -1542,7 +1543,7 @@ void serialize( const std::optional<T> &obj, JsonOut &jsout )
 template<typename T>
 void deserialize( std::optional<T> &obj, JsonIn &jsin )
 {
-    if( jsin.test_null() ) {
+    if( jsin.read_null() ) {
         obj.reset();
     } else {
         obj.emplace();
