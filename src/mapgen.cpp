CleverRaven/Cataclysm-DDA

Support mapgen parameters in nested mapgen
We want mapgen parameters to work inside nested mapgen, at all scopes.
To make this work we lift all parameters from nests to overmap terrains
before lifting again from overmap terrains to overmap specials.

At mapgen time, the arguments chosen for these parameters cascade down
in the opposite direction.

src/mapgen.cpp
@@ -270,6 +270,11 @@ class mapgen_basic_container
             // Not needed anymore, pointers are now stored in weights_ (or not used at all)
             mapgens_.clear();
         }
+        void finalize_parameters() {
+            for( auto &mapgen_function_ptr : weights_ ) {
+                mapgen_function_ptr.obj->finalize_parameters();
+            }
+        }
         void check_consistency() {
             for( auto &mapgen_function_ptr : weights_ ) {
                 mapgen_function_ptr.obj->check();
@@ -321,8 +326,14 @@ class mapgen_factory
                 omw.second.setup();
                 inp_mngr.pump_events();
             }
-            // Dummy entry, overmap terrain null should never appear and is therefore never generated.
+            // Dummy entry, overmap terrain null should never appear and is
+            // therefore never generated.
             mapgens_.erase( "null" );
+        }
+        void finalize_parameters() {
+            for( std::pair<const std::string, mapgen_basic_container> &omw : mapgens_ ) {
+                omw.second.finalize_parameters();
+            }
         }
         void check_consistency() {
             // Cache all strings that may get looked up here so we don't have to go through
@@ -394,7 +405,21 @@ void calculate_mapgen_weights()   // TODO: rename as it runs jsonfunction setup 
             inp_mngr.pump_events();
         }
     }
-
+    // Having set up all the mapgens we can now perform a second
+    // pass of finalizing their parameters
+    oter_mapgen.finalize_parameters();
+    for( auto &pr : nested_mapgen ) {
+        for( weighted_object<int, std::shared_ptr<mapgen_function_json_nested>> &ptr : pr.second ) {
+            ptr.obj->finalize_parameters();
+            inp_mngr.pump_events();
+        }
+    }
+    for( auto &pr : update_mapgen ) {
+        for( auto &ptr : pr.second ) {
+            ptr->finalize_parameters();
+            inp_mngr.pump_events();
+        }
+    }
 }
 
 void check_mapgen_definitions()
@@ -599,6 +624,13 @@ static bool common_check_bounds( const jmapgen_int &x, const jmapgen_int &y,
     return true;
 }
 
+void mapgen_function_json_base::merge_non_nest_parameters_into(
+    mapgen_parameters &params, const std::string &outer_context ) const
+{
+    const std::string context = string_format( "%s within %s", context_, outer_context );
+    params.check_and_merge( parameters, context, mapgen_parameter_scope::nest );
+}
+
 bool mapgen_function_json_base::check_inbounds( const jmapgen_int &x, const jmapgen_int &y,
         const JsonObject &jso ) const
 {
@@ -799,6 +831,11 @@ void mapgen_function_json_base::setup_setmap( const JsonArray &parray )
         tmpval.clear();
     }
 
+}
+
+void mapgen_function_json_base::finalize_parameters_common()
+{
+    objects.merge_parameters_into( parameters, context_ );
 }
 
 mapgen_arguments mapgen_function_json_base::get_args(
@@ -1111,13 +1148,16 @@ mapgen_arguments mapgen_parameters::get_args(
 }
 
 void mapgen_parameters::check_and_merge( const mapgen_parameters &other,
-        const std::string &context )
+        const std::string &context, mapgen_parameter_scope up_to_scope )
 {
     for( const std::pair<const std::string, mapgen_parameter> &p : other.map ) {
+        const mapgen_parameter &other_param = p.second;
+        if( other_param.scope() >= up_to_scope ) {
+            continue;
+        }
         auto insert_result = map.insert( p );
         if( !insert_result.second ) {
             const std::string &name = p.first;
-            const mapgen_parameter &other_param = p.second;
             const mapgen_parameter &this_param = insert_result.first->second;
             this_param.check_consistent_with(
                 other_param, string_format( "parameter %s in %s", name, context ) );
@@ -2265,6 +2305,31 @@ class jmapgen_nested : public jmapgen_piece
         int phase() const override {
             return 1;
         }
+        void merge_parameters_into( mapgen_parameters &params,
+                                    const std::string &outer_context ) const override {
+            auto merge_from = [&]( const std::string & name ) {
+                if( name == "null" ) {
+                    return;
+                }
+                const auto iter = nested_mapgen.find( name );
+                if( iter == nested_mapgen.end() ) {
+                    debugmsg( "Unknown nested mapgen function id %s", name );
+                    return;
+                }
+                using Obj = weighted_object<int, std::shared_ptr<mapgen_function_json_nested>>;
+                for( const Obj &nested : iter->second ) {
+                    nested.obj->merge_non_nest_parameters_into( params, outer_context );
+                }
+            };
+
+            for( const weighted_object<int, std::string> &name : entries ) {
+                merge_from( name.obj );
+            }
+
+            for( const weighted_object<int, std::string> &name : else_entries ) {
+                merge_from( name.obj );
+            }
+        }
         void apply( const mapgendata &dat, const jmapgen_int &x, const jmapgen_int &y
                   ) const override {
             const std::string *res = neighbors.test( dat ) ? entries.pick() : else_entries.pick();
@@ -2762,6 +2827,21 @@ void update_mapgen_function_json::setup()
     setup_common();
 }
 
+void mapgen_function_json::finalize_parameters()
+{
+    finalize_parameters_common();
+}
+
+void mapgen_function_json_nested::finalize_parameters()
+{
+    finalize_parameters_common();
+}
+
+void update_mapgen_function_json::finalize_parameters()
+{
+    finalize_parameters_common();
+}
+
 /*
  * Parse json, pre-calculating values for stuff, then cheerfully throw json away. Faster than regular mapf, in theory
  */
@@ -2968,6 +3048,14 @@ void jmapgen_objects::check( const std::string &context, const mapgen_parameters
 {
     for( const jmapgen_obj &obj : objects ) {
         obj.second->check( context, parameters );
+    }
+}
+
+void jmapgen_objects::merge_parameters_into( mapgen_parameters &params,
+        const std::string &outer_context ) const
+{
+    for( const jmapgen_obj &obj : objects ) {
+        obj.second->merge_parameters_into( params, outer_context );
     }
 }
 

cataclysmbnteam/Cataclysm-BN

src/mapgen.cpp
@@ -284,6 +284,11 @@ class mapgen_basic_container
             // Not needed anymore, pointers are now stored in weights_ (or not used at all)
             mapgens_.clear();
         }
+        void finalize_parameters() {
+            for( auto &mapgen_function_ptr : weights_ ) {
+                mapgen_function_ptr.obj->finalize_parameters();
+            }
+        }
         void check_consistency( const std::string &key ) {
             for( auto &mapgen_function_ptr : weights_ ) {
                 mapgen_function_ptr.obj->check( key );
@@ -335,8 +340,14 @@ class mapgen_factory
                 omw.second.setup();
                 inp_mngr.pump_events();
             }
-            // Dummy entry, overmap terrain null should never appear and is therefor never generated.
+            // Dummy entry, overmap terrain null should never appear and is
+            // therefore never generated.
             mapgens_.erase( "null" );
+        }
+        void finalize_parameters() {
+            for( std::pair<const std::string, mapgen_basic_container> &omw : mapgens_ ) {
+                omw.second.finalize_parameters();
+            }
         }
         void check_consistency() {
             // Cache all strings that may get looked up here so we don't have to go through
@@ -408,7 +419,21 @@ void calculate_mapgen_weights()   // TODO: rename as it runs jsonfunction setup 
             inp_mngr.pump_events();
         }
     }
-
+    // Having set up all the mapgens we can now perform a second
+    // pass of finalizing their parameters
+    oter_mapgen.finalize_parameters();
+    for( auto &pr : nested_mapgen ) {
+        for( weighted_object<int, std::shared_ptr<mapgen_function_json_nested>> &ptr : pr.second ) {
+            ptr.obj->finalize_parameters();
+            inp_mngr.pump_events();
+        }
+    }
+    for( auto &pr : update_mapgen ) {
+        for( auto &ptr : pr.second ) {
+            ptr->finalize_parameters();
+            inp_mngr.pump_events();
+        }
+    }
 }
 
 void check_mapgen_definitions()
@@ -620,6 +645,12 @@ static bool common_check_bounds( const jmapgen_int &x, const jmapgen_int &y,
     return true;
 }
 
+void mapgen_function_json_base::merge_non_nest_parameters_into(
+    mapgen_parameters &params, const std::string &outer_context ) const
+{
+    params.check_and_merge( parameters, outer_context, mapgen_parameter_scope::nest );
+}
+
 bool mapgen_function_json_base::check_inbounds( const jmapgen_int &x, const jmapgen_int &y,
         const JsonObject &jso ) const
 {
@@ -820,6 +851,11 @@ void mapgen_function_json_base::setup_setmap( const JsonArray &parray )
         tmpval.clear();
     }
 
+}
+
+void mapgen_function_json_base::finalize_parameters_common()
+{
+    objects.merge_parameters_into( parameters, "" );
 }
 
 mapgen_arguments mapgen_function_json_base::get_args(
@@ -1132,13 +1168,16 @@ mapgen_arguments mapgen_parameters::get_args(
 }
 
 void mapgen_parameters::check_and_merge( const mapgen_parameters &other,
-        const std::string &context )
+        const std::string &context, mapgen_parameter_scope up_to_scope )
 {
     for( const std::pair<const std::string, mapgen_parameter> &p : other.map ) {
+        const mapgen_parameter &other_param = p.second;
+        if( other_param.scope() >= up_to_scope ) {
+            continue;
+        }
         auto insert_result = map.insert( p );
         if( !insert_result.second ) {
             const std::string &name = p.first;
-            const mapgen_parameter &other_param = p.second;
             const mapgen_parameter &this_param = insert_result.first->second;
             this_param.check_consistent_with(
                 other_param, string_format( "parameter %s in %s", name, context ) );
@@ -2344,6 +2383,31 @@ class jmapgen_nested : public jmapgen_piece
         int phase() const override {
             return 1;
         }
+        void merge_parameters_into( mapgen_parameters &params,
+                                    const std::string &outer_context ) const override {
+            auto merge_from = [&]( const std::string & name ) {
+                if( name == "null" ) {
+                    return;
+                }
+                const auto iter = nested_mapgen.find( name );
+                if( iter == nested_mapgen.end() ) {
+                    debugmsg( "Unknown nested mapgen function id %s", name );
+                    return;
+                }
+                using Obj = weighted_object<int, std::shared_ptr<mapgen_function_json_nested>>;
+                for( const Obj &nested : iter->second ) {
+                    nested.obj->merge_non_nest_parameters_into( params, outer_context );
+                }
+            };
+
+            for( const weighted_object<int, std::string> &name : entries ) {
+                merge_from( name.obj );
+            }
+
+            for( const weighted_object<int, std::string> &name : else_entries ) {
+                merge_from( name.obj );
+            }
+        }
         void apply( const mapgendata &dat, const jmapgen_int &x, const jmapgen_int &y
                   ) const override {
             const std::string *res = get_entries( dat ).pick();
@@ -2839,6 +2903,21 @@ void update_mapgen_function_json::setup()
     setup_common();
 }
 
+void mapgen_function_json::finalize_parameters()
+{
+    finalize_parameters_common();
+}
+
+void mapgen_function_json_nested::finalize_parameters()
+{
+    finalize_parameters_common();
+}
+
+void update_mapgen_function_json::finalize_parameters()
+{
+    finalize_parameters_common();
+}
+
 /*
  * Parse json, pre-calculating values for stuff, then cheerfully throw json away. Faster than regular mapf, in theory
  */
@@ -3058,6 +3137,14 @@ void jmapgen_objects::check( const std::string &oter_name, const mapgen_paramete
 {
     for( const jmapgen_obj &obj : objects ) {
         obj.second->check( oter_name, parameters );
+    }
+}
+
+void jmapgen_objects::merge_parameters_into( mapgen_parameters &params,
+        const std::string &outer_context ) const
+{
+    for( const jmapgen_obj &obj : objects ) {
+        obj.second->merge_parameters_into( params, outer_context );
     }
 }
 