CleverRaven/Cataclysm-DDA

Serialize mapgen args as stored in the overmap
For mapgen to be consistent across reloads, we need to store the arg
choices.

src/savegame.cpp
@@ -716,6 +716,15 @@ void overmap::unserialize( std::istream &fin )
                     }
                 }
             }
+        } else if( name == "mapgen_arg_storage" ) {
+            jsin.read( mapgen_arg_storage, true );
+        } else if( name == "mapgen_arg_index" ) {
+            std::vector<std::pair<tripoint_om_omt, int>> flat_index;
+            jsin.read( flat_index, true );
+            for( const std::pair<tripoint_om_omt, int> &p : flat_index ) {
+                auto it = mapgen_arg_storage.get_iterator_from_index( p.second );
+                mapgen_args_index.emplace( p.first, &*it );
+            }
         }
     }
 }
@@ -1111,6 +1120,22 @@ void overmap::serialize( std::ostream &fout ) const
         json.end_object();
         json.end_array();
         json.end_object();
+    }
+    json.end_array();
+    fout << std::endl;
+
+    json.member( "mapgen_arg_storage", mapgen_arg_storage );
+    fout << std::endl;
+    json.member( "mapgen_arg_index" );
+    json.start_array();
+    for( const std::pair<const tripoint_om_omt, cata::optional<mapgen_arguments> *> &p :
+         mapgen_args_index ) {
+        json.start_array();
+        json.write( p.first );
+        auto it = mapgen_arg_storage.get_iterator_from_pointer( p.second );
+        int index = mapgen_arg_storage.get_index_from_iterator( it );
+        json.write( index );
+        json.end_array();
     }
     json.end_array();
     fout << std::endl;

cataclysmbnteam/Cataclysm-BN

src/savegame.cpp
@@ -683,6 +683,14 @@ void overmap::unserialize( std::istream &fin, const std::string &file_path )
             for( const std::pair<om_pos_dir, std::string> &p : flat_index ) {
                 joins_used.insert( p );
             }
+        } else if( name == "mapgen_arg_storage" ) {
+            jsin.read( mapgen_arg_storage, true );
+        } else if( name == "mapgen_arg_index" ) {
+            std::vector<std::pair<tripoint_om_omt, int>> flat_index;
+            jsin.read( flat_index, true );
+            for( const std::pair<tripoint_om_omt, int> &p : flat_index ) {
+                mapgen_args_index.emplace( p.first, &mapgen_arg_storage[p.second] );
+            }
         } else {
             jsin.skip_value();
         }
@@ -1103,6 +1111,23 @@ void overmap::serialize( std::ostream &fout ) const
     std::vector<std::pair<om_pos_dir, std::string>> flattened_joins_used(
                 joins_used.begin(), joins_used.end() );
     json.member( "joins_used", flattened_joins_used );
+    json.member( "mapgen_arg_storage", mapgen_arg_storage );
+    fout << std::endl;
+    json.member( "mapgen_arg_index" );
+    json.start_array();
+    for( const std::pair<const tripoint_om_omt, std::optional<mapgen_arguments> *> &p :
+         mapgen_args_index ) {
+        json.start_array();
+        json.write( p.first );
+        for( size_t i = 0; i < mapgen_arg_storage.size(); i++ ) {
+            if( p.second == &mapgen_arg_storage[i] ) {
+                json.write( i );
+                break;
+            }
+        }
+        json.end_array();
+    }
+    json.end_array();
     fout << std::endl;
 
     json.end_object();