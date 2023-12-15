40cb39bdee8de1fe6e9cfe2f4693dec8fe058d5a
Volch <volk_pk@mail.ru>
2023-12-15 11:57:54 +0300

    Controlled rotorcrafts shouldn't fall

--- src/vehicle_move.cpp
+++ src/vehicle_move.cpp
@@ -1578,6 +1578,12 @@ void vehicle::check_falling_or_floating()
         deep_water_tiles += here.has_flag( TFLAG_DEEP_WATER, p ) ? 1 : 0;
         water_tiles += here.has_flag( TFLAG_SWIMMABLE, p ) ? 1 : 0;
     }
+
+    if( is_falling && is_rotorcraft() ) {
+        is_falling = false;
+        is_flying = true;
+    }
+
     // floating if 2/3rds of the vehicle is in deep water
     is_floating = 3 * deep_water_tiles >= 2 * pts.size();
     // in_water if 1/2 of the vehicle is in water at all
