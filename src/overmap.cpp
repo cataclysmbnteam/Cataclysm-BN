d9aef17576c3228c263dd0d3a386dda4e8296b4c
Vollch <volk_pk@mail.ru>
2023-12-16 04:41:53 +0300

    feat: mainlined elevated bridges (#3913)
    * Fix underbridge shore rotation
    
    * Mainlined elevated bridges
    
    * Elevate post-mapgen bridges
    
    * Fix minefield test

--- src/overmap.cpp
+++ src/overmap.cpp
@@ -74,6 +74,8 @@ static const mongroup_id GROUP_SWAMP( "GROUP_SWAMP" );
 static const mongroup_id GROUP_WORM( "GROUP_WORM" );
 static const mongroup_id GROUP_ZOMBIE( "GROUP_ZOMBIE" );
 
+static const oter_type_str_id oter_type_bridge( "bridge" );
+
 class map_extra;
 
 #define dbg(x) DebugLogFL((x),DC::MapGen)
@@ -3453,10 +3455,6 @@ bool overmap::generate_over( const int z )
     bool requires_over = false;
     std::vector<point_om_omt> bridge_points;
 
-    if( !get_option<bool>( "ELEVATED_BRIDGES" ) ) {
-        return requires_over;
-    }
-
     // These are so common that it's worth checking first as int.
     const std::set<oter_id> skip_below = {
         oter_id( "empty_rock" ), oter_id( "forest" ), oter_id( "field" ),
@@ -3476,7 +3474,7 @@ bool overmap::generate_over( const int z )
                     continue;
                 }
 
-                if( is_ot_match( "bridge", oter_ground, ot_match_type::type ) ) {
+                if( oter_ground->get_type_id() == oter_type_bridge ) {
                     bridge_points.emplace_back( i, j );
                 }
             }
@@ -3485,12 +3483,8 @@ bool overmap::generate_over( const int z )
 
     elevate_bridges(
         *this, bridge_points,
-        "bridge_road",
-        "bridge_under",
-        "bridgehead_ground",
-        "bridgehead_ramp",
-        "road_ew",
-        "road_ns"
+        "bridge_road", "bridge_under", "bridgehead_ground",
+        "bridgehead_ramp", "road_ew", "road_ns"
     );
 
     return requires_over;
@@ -5097,6 +5091,22 @@ bool overmap::build_connection(
 
     if( connection_cache ) {
         connection_cache->add( connection.id, z, start.pos );
+    } else if( z == 0 && connection.id.str() == "local_road" ) {
+        // If there's no cache, it means we're placing road after
+        // normal mapgen, and need to elevate bridges manually
+        std::vector<point_om_omt> bridge_points;
+        for( const auto &node : path.nodes ) {
+            const tripoint_om_omt pos( node.pos, z );
+            if( ter( pos )->get_type_id() == oter_type_bridge ) {
+                bridge_points.emplace_back( pos.xy() );
+            }
+        }
+
+        elevate_bridges(
+            *this, bridge_points,
+            "bridge_road", "bridge_under", "bridgehead_ground",
+            "bridgehead_ramp", "road_ew", "road_ns"
+        );
     }
     return true;
 }
