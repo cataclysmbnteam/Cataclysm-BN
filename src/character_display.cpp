19da502a8e4c338a2cca6cc9302b6e807f3ab6dc
Chaosvolt <chaosvolt@users.noreply.github.com>
2024-02-20 22:00:59 -0600

    Update src/character_display.cpp
    Co-authored-by: scarf <greenscarf005@gmail.com>

--- src/character_display.cpp
+++ src/character_display.cpp
@@ -646,7 +646,7 @@ struct HeaderSkill {
     }
 };
 
-int display_empty_handed_base_damage( const Character &you )
+int character_display::display_empty_handed_base_damage( const Character &you )
 {
     int empty_hand_base_damage = you.get_skill_level( skill_unarmed ) * 2;
     const bool left_empty = !you.natural_attack_restricted_on( bodypart_id( "hand_l" ) );
