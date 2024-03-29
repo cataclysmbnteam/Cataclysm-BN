3ef8c1754866891a613ce2fd7a5cca3aa9fa1199
Zlorthishen <79779913+Zlorthishen@users.noreply.github.com>
2024-03-28 19:55:09 -0500

    Update handle_action.cpp

--- src/handle_action.cpp
+++ src/handle_action.cpp
@@ -124,7 +124,6 @@ static const quality_id qual_CUT( "CUT" );
 
 static const bionic_id bio_remote( "bio_remote" );
 
-static const trait_id trait_BRAWLER( "BRAWLER" );
 static const trait_id trait_HIBERNATE( "HIBERNATE" );
 static const trait_id trait_PROF_CHURL( "PROF_CHURL" );
 static const trait_id trait_SHELL2( "SHELL2" );
@@ -1339,18 +1338,15 @@ static void fire()
     // Use vehicle turret or draw a pistol from a holster if unarmed
     if( !u.is_armed() ) {
 
-
         const optional_vpart_position vp = here.veh_at( u.pos() );
 
         turret_data turret;
         if( vp && ( turret = vp->vehicle().turret_query( u.pos() ) ) ) {
-
             avatar_action::fire_turret_manual( u, here, turret );
             return;
         }
 
         if( vp.part_with_feature( "CONTROLS", true ) ) {
-
             if( vp->vehicle().turrets_aim_and_fire_all_manual() ) {
                 return;
             }
