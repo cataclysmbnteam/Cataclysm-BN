0da6238ed3854dfaa4f99dde1d499dd47ce9468b
Chaosvolt <chaosvolt@users.noreply.github.com>
2024-07-01 13:37:48 -0500

    smolfix

--- src/newcharacter.cpp
+++ src/newcharacter.cpp
@@ -75,8 +75,6 @@ static const std::string flag_SECRET( "SECRET" );
 static const flag_id json_flag_no_auto_equip( "no_auto_equip" );
 static const flag_id json_flag_auto_wield( "auto_wield" );
 
-static const trait_id trait_SMELLY( "SMELLY" );
-static const trait_id trait_WEAKSCENT( "WEAKSCENT" );
 static const trait_id trait_XS( "XS" );
 static const trait_id trait_XXXL( "XXXL" );
 
