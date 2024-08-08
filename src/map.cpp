1d692d7679509afabe11e7c4d077e2a41cd66e0c
Ethan Meade <ethanmeade@ucsb.edu>
2024-08-08 17:10:50 -0500

    make neighboring treetops not automatically self-destruct on tree destroy

--- src/map.cpp
+++ src/map.cpp
@@ -2925,27 +2925,34 @@ int map::collapse_check( const tripoint &p )
     // if there's support below, things are less likely to collapse
     if( p.z > -OVERMAP_DEPTH ) {
         const tripoint &pbelow = tripoint( p.xy(), p.z - 1 );
-        for( const tripoint &tbelow : points_in_radius( pbelow, 1 ) ) {
+        if( has_flag( TFLAG_SINGLE_SUPPORT, p ) ) {
             if( has_flag( TFLAG_SUPPORTS_ROOF, pbelow ) ) {
-                num_supports += 1;
-                if( has_flag( TFLAG_WALL, pbelow ) ) {
-                    num_supports += 2;
-                }
-                if( tbelow == pbelow ) {
-                    num_supports += 2;
-                }
-                if( has_flag( TFLAG_SINGLE_SUPPORT, p ) ) {
-                    num_supports = 0;
+                num_supports = 3;
+            } else {
+                num_supports = 0;
+            }
+        } else {
+            for( const tripoint &tbelow : points_in_radius( pbelow, 1 ) ) {
+                if( has_flag( TFLAG_SUPPORTS_ROOF, pbelow ) ) {
+                    num_supports += 1;
+                    if( has_flag( TFLAG_WALL, pbelow ) ) {
+                        num_supports += 2;
+                    }
+                    if( tbelow == pbelow ) {
+                        num_supports += 2;
+                    }
                 }
             }
         }
     }
 
     for( const tripoint &t : points_in_radius( p, 1 ) ) {
+        if( has_flag( TFLAG_SINGLE_SUPPORT, p ) ) {
+            break;
+        }
         if( p == t ) {
             continue;
         }
-
         if( collapses ) {
             if( has_flag( TFLAG_COLLAPSES, t ) ) {
                 num_supports++;
@@ -2960,9 +2967,6 @@ int map::collapse_check( const tripoint &p )
                     num_supports += 3;
                 }
             }
-        }
-        if( has_flag( TFLAG_SINGLE_SUPPORT, p ) ) {
-            num_supports = 0;
         }
     }
 
