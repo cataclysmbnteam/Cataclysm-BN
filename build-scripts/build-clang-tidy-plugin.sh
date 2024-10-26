c068e89a4385938db0a1cd3ad070471b65f8d4fe
Coolthulhu <Coolthulhu@gmail.com>
2024-10-26 10:54:08 +0200

    Try explicit LLVM_ROOT=llvm-config --prefix

--- build-scripts/build-clang-tidy-plugin.sh
+++ build-scripts/build-clang-tidy-plugin.sh
@@ -19,6 +19,7 @@ cmake \
   -DBACKTRACE=ON \
   -DCMAKE_C_COMPILER=/usr/bin/clang \
   -DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
+  -DLLVM_ROOT=$(llvm-config --prefix) \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
   -DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
   -DTILES="$TILES" \
