ba18d305461c44b39c7d93425d78587312ee60c2
Coolthulhu <Coolthulhu@gmail.com>
2024-10-26 11:01:45 +0200

    Try LLVM_DIR=llvm-config --cmakedir

--- build-scripts/build-clang-tidy-plugin.sh
+++ build-scripts/build-clang-tidy-plugin.sh
@@ -20,6 +20,7 @@ cmake \
   -DCMAKE_C_COMPILER=/usr/bin/clang \
   -DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
   -DLLVM_ROOT=$(llvm-config --prefix) \
+  -DLLVM_DIR=$(llvm-config --cmakedir) \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
   -DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
   -DTILES="$TILES" \
