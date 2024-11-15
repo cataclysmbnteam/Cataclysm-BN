845d47c52b97f760ad1b18618b71d9c7b8124854
Chaosvolt <chaosvolt@users.noreply.github.com>
2024-11-14 23:40:07 -0600

    Update item.cpp

--- src/item.cpp
+++ src/item.cpp
@@ -3696,8 +3696,22 @@ void item::combat_info( std::vector<iteminfo> &info, const iteminfo_query *parts
         // Moves
         if( parts->test( iteminfo_parts::DESCRIPTION_MELEEDMG_MOVES ) ) {
             info.emplace_back( "BASE", _( "Moves per attack: " ), "<num>",
-                               iteminfo::lower_is_better, attack_cost );
+                               iteminfo::no_flags, attack_cost );
         }
+    }
+    item &tmp = *item::spawn_temporary( type );
+
+    if( tmp.count_by_charges() && tmp.charges > 1 ) {
+        tmp.charges = 1;
+    }
+    float throw_amt = ranged::throw_damage( tmp,
+                                            get_avatar().get_skill_level( skill_throw ),
+                                            get_avatar().get_str() );
+    if( throw_amt > 0 ) {
+        info.emplace_back( "BASE", _( "Damage if thrown: " ), "<num>",
+                           iteminfo::no_flags, throw_amt );
+    }
+    if( print_attacks || debug_mode ) {
         insert_separation_line( info );
     }
 }
