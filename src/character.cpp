c53d15e5173c436e6668f180557c19b9938bf3de
Alec White Valentine <doncuentacuentos@hotmail.com>
2024-12-13 22:04:16 +0100

    Astyling

--- src/character.cpp
+++ src/character.cpp
@@ -6683,7 +6683,7 @@ int Character::visibility( bool, int ) const
     // if ( dark_clothing() && light check ...
     int stealth_modifier = std::floor( mutation_value( "stealth_modifier" ) );
     int const crouching_bonus = 30;
-    if ((g->u.movement_mode_is(CMM_CROUCH))) {
+    if( ( g->u.movement_mode_is( CMM_CROUCH ) ) ) {
         stealth_modifier += crouching_bonus;
     };
     return clamp( 100 - stealth_modifier, 20, 160 );
