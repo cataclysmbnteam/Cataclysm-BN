4b3e7538d07888fa4d271a1fd11c00bce8469588
FriedLongJohns <81837862+FriedLongJohns@users.noreply.github.com>
2024-12-18 17:12:01 -0800

    use official sol fix
    use fix from
    ThePhD/sol2@d805d02
    at
    ThePhD/sol2#1617

--- src/sol/sol.hpp
+++ src/sol/sol.hpp
@@ -6756,8 +6756,8 @@ namespace sol {
 			static_assert(std::is_constructible<T, Args&&...>::value, "T must be constructible with Args");
 
 			*this = nullopt;
-			m_value = &T( std::forward<Args>( args )... );
-			return *m_value;
+			new (static_cast<void*>(this)) optional(std::in_place, std::forward<Args>(args)...);
+			return **this;
 		}
 
 		/// Swaps this optional with the other.
