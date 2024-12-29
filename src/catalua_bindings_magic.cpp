530ccf7a1d76726363652bb15ee6e185ef858d3c
karxi <grimAdjutrix@gmail.com>
2024-12-29 09:32:11 -0500

    Further binding work, touchups, and a convenience function for quicker SpellSimple invocation.

--- src/catalua_bindings_magic.cpp
+++ src/catalua_bindings_magic.cpp
@@ -9,6 +9,7 @@
 #include "catalua_luna.h"
 #include "catalua_luna_doc.h"
 
+#include "avatar.h"
 #include "creature.h"
 #include "magic.h"
 
@@ -27,6 +28,7 @@ void cata::detail::reg_spell_type( sol::state &lua )
          * Because spell_type is bound as an ID, the actual object is
          * shoved into a 'Raw' binding.
          */
+        DOC( "The 'raw' type for storing the information defining every spell in the game. It's not possible to cast directly from this type; check SpellSimple and Spell." );
         sol::usertype<UT_CLASS> ut =
         luna::new_usertype<UT_CLASS>(
             lua,
@@ -36,10 +38,9 @@ void cata::detail::reg_spell_type( sol::state &lua )
 
         // The string conversion function references this object's str_id.
         luna::set_fx( ut, sol::meta_function::to_string,
-        []( const UT_CLASS & id ) -> std::string {
-            return string_format( "%s[%s]", luna::detail::luna_traits<UT_CLASS>::name, id.id.c_str() );
+            []( const UT_CLASS & id ) -> std::string {
+                return string_format( "%s[%s]", luna::detail::luna_traits<UT_CLASS>::name, id.id.c_str() );
         } );
-
 
         SET_MEMB_RO( id );
         DOC( "The name of the primary effect this spell will enact." );
@@ -47,8 +48,8 @@ void cata::detail::reg_spell_type( sol::state &lua )
         DOC( "Specifics about the effect this spell will enact." );
         SET_MEMB_RO( effect_str );
 
-        // Need to see how the code handles this...
-        //SET_MEMB_RO( field );
+        // Currently unclear on how to implement 'field'; it's std::optional,
+        // while we want to return sol::optional.
         SET_MEMB_RO( field_chance );
         SET_MEMB_RO( min_field_intensity );
         SET_MEMB_RO( field_intensity_increment );
@@ -81,8 +82,6 @@ void cata::detail::reg_spell_type( sol::state &lua )
         SET_MEMB_RO( energy_increment );
         SET_MEMB_RO( final_energy_cost );
 
-        //DOC( "The class necessary to cast this spell, if any." );
-        //SET_MEMB_RO( spell_class );
         SET_MEMB_RO( difficulty );
         SET_MEMB_RO( max_level );
         SET_MEMB_RO( base_casting_time );
@@ -90,8 +89,9 @@ void cata::detail::reg_spell_type( sol::state &lua )
         SET_MEMB_RO( final_casting_time );
 
         DOC( "Other spells cast by this spell." );
-        luna::set_fx( ut, "additional_spells", []( const UT_CLASS & spid ) -> std::vector<fake_spell> {
-            std::vector<fake_spell> rv = spid.additional_spells; return rv;
+        luna::set_fx( ut, "additional_spells",
+            []( const UT_CLASS & spid ) -> std::vector<fake_spell> {
+                std::vector<fake_spell> rv = spid.additional_spells; return rv;
         } );
 
         DOC( "Returns a (long) list of every spell in the game." );
@@ -105,6 +105,7 @@ void cata::detail::reg_spell_fake( sol::state &lua )
 {
 #define UT_CLASS fake_spell
     {
+        DOC( "The type for basic spells. If you don't need to track XP from casting (e.g., if a spell is intended to be cast by anything *other than* a player), this is likely the appropriate type. Otherwise, see the Spell type." );
         sol::usertype<UT_CLASS> ut =
         luna::new_usertype<UT_CLASS>(
             lua,
@@ -116,26 +117,66 @@ void cata::detail::reg_spell_fake( sol::state &lua )
         );
 
         luna::set_fx( ut, sol::meta_function::to_string,
-        []( const UT_CLASS & id ) -> std::string {
-            return string_format( "%s[%s]", luna::detail::luna_traits<UT_CLASS>::name, id.id.c_str() );
+             []( const UT_CLASS & id ) -> std::string {
+                return string_format( "%s[%s]", luna::detail::luna_traits<UT_CLASS>::name, id.id.c_str() );
         } );
 
         SET_MEMB_RO( id );
-        // TODO: Look into binding std::optional<int> max_level
-        // Also consider making this SET_MEMB for modification.
+
+        DOC( "Returns the defined maximum level of this SpellSimple instance, if defined. Otherwise, returns 0." );
+        luna::set_fx( ut, "max_level", []( UT_CLASS & sp ) -> int {
+            return sp.max_level.has_value() ? *sp.max_level : 0;
+        } );
+
+        // Perhaps this should be writeable?
         SET_MEMB_RO( level );
-        SET_MEMB_N_RO( self, "target_is_source" );
+        DOC( "Whether or not the target point is *locked* to the source's location." );
+        SET_MEMB_N_RO( self, "force_target_source" );
+        DOC( "Used for enchantments; the spell's *chance* to trigger every turn." );
+        SET_MEMB_RO( trigger_once_in );
 
         // TODO: Support min_level_override
         luna::set_fx( ut, "cast",
-         []( UT_CLASS & sp, 
-                    Creature & source,
-                    const tripoint & target,
-                    std::optional<int> min_level_override )
+            []( UT_CLASS & sp,
+                Creature & source,
+                const tripoint & target,
+                sol::optional<int> min_lvl_override )
+            {
+                int mlo = min_lvl_override.has_value() ? *min_lvl_override : 0;
+                sp.get_spell( mlo ).cast_all_effects( source, target );
+            }
+        );
+
+        DOC( "Static function: Creates and immediately casts a SimpleSpell, then returns the new spell for potential reuse. If the given tripoint is the player's location, the spell will be locked to the player (this does not necessarily mean friendly fire). If an integer is specified, the spell will be cast at that level." );
+        luna::set_fx( ut, "prompt_cast",
+            []( spell_id spid,
+                tripoint & target,
+                sol::optional<int> level ) -> fake_spell
+            {
+                // This will be our return value, as well as the spell we cast.
+                fake_spell sp;
+                /* Without a specified Creature, we assume the player is the
+                 * source.
+                 * I'd prefer to call gapi.get_avatar, but this will do for now.
+                 */
+                avatar & avvy = get_avatar();
+                // If target is avatar's location, assume we want to hit self
+                bool hit_self = avvy.pos() == target;
+                sp = fake_spell( spid, hit_self );
+
+                // If a level is given, forcefully clamp to that level.
+                if ( level.has_value() )
                 {
-                    int mlo = min_level_override ? *min_level_override : 0;
-                    sp.get_spell( mlo ).cast_all_effects( source, target );
-                });
+                    sp.level = *level;
+                    sp.max_level = *level;
+                }
+
+                // Now that the spell is configured, we cast it as usual...
+                sp.get_spell().cast_all_effects( avvy, target );
+                // ...and return the spell we made for reuse.
+                return sp;
+            }
+        );
     }
 #undef UT_CLASS // #define UT_CLASS fake_spell
 }
@@ -147,8 +188,11 @@ void cata::detail::reg_spell( sol::state &lua )
         /* NOTE: This is the actual 'spell type', which is fully-featured and
          * intended for use with players. As such, it tracks things like xp
          * and level, which you may not care about.
-         * Make sure to look at SpellFake (i.e., fake_spell) too.
+         * This functionality isn't presently important, particularly since
+         * there are currently no methods to access the spells in a player's
+         * spellbook (known_magic).
          */
+        DOC( "The class used for spells that *a player* knows, casts, and gains experience for using. If a given spell is not supposed to be directly cast by a player, consider using SpellSimple instead." );
         sol::usertype<UT_CLASS> ut =
         luna::new_usertype<UT_CLASS>(
             lua,
@@ -160,8 +204,6 @@ void cata::detail::reg_spell( sol::state &lua )
 
         // Lets us grab the ID from the object.
         SET_MEMB_N_RO( type, "id" );
-        // ↑ I have no clue why that works, even after 'type' is made private
-        // again. I do not presently have the patience to investigate further.
         
         SET_FX_T( xp, int() const );
         SET_FX_T( gain_exp, void( int ) );
@@ -173,7 +215,7 @@ void cata::detail::reg_spell( sol::state &lua )
         SET_FX_T( name, std::string() const );
         SET_FX_N_T( description, "desc", std::string() const );
 
-        // Present goal is basic functionality.
+        // Present priority is basic functionality.
 
         DOC( "Cast this spell, as well as any sub-spells." );
         SET_FX_N_T( cast_all_effects, "cast", void( Creature & source, const tripoint & target ) const );
