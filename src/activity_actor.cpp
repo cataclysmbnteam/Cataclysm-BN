e1265b0c323faae0e8367318bbbdf8ec2d5b319a
Artem <Xa4yM6eK@gmail.com>
2025-02-18 03:03:30 +0300

    Update src/activity_actor.cpp
    Co-authored-by: scarf <greenscarf005@gmail.com>

--- src/activity_actor.cpp
+++ src/activity_actor.cpp
@@ -409,8 +409,7 @@ void dig_activity_actor::start( player_activity &act, Character & )
 {
     map &here = get_map();
     const bool grave = here.ter( location ) == t_grave;
-    std::string name;
-    name = grave
+    const std::string name = grave
            ? "grave"
            : here.ter( location ).obj().name();
     act.progress.emplace( name, moves_total );
