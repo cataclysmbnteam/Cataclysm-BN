cf22563afb0964fb80e2a5d21a88836197e5c523
Chaosvolt <chaosvolt@users.noreply.github.com>
2025-03-25 18:43:11 -0500

    feat(balance): automatically drop harvested items that aren't comestibles (#6231)
    * feat(balance): automatically drop harvested items that aren't comestibles
    
    * Update iexamine.cpp
    
    * Update iexamine.cpp

--- src/iexamine.cpp
+++ src/iexamine.cpp
@@ -1847,7 +1847,8 @@ static bool drink_nectar( player &p )
 static void handle_harvest( player &p, const std::string &itemid, bool force_drop )
 {
     detached_ptr<item> harvest = item::spawn( itemid );
-    if( !force_drop && p.can_pick_volume( *harvest ) &&
+    // Drop items that're exceed available space and things that aren't comestibles
+    if( !force_drop && harvest->get_comestible() && p.can_pick_volume( *harvest ) &&
         p.can_pick_weight( *harvest, !get_option<bool>( "DANGEROUS_PICKUPS" ) ) ) {
 
         p.add_msg_if_player( _( "You harvest: %s." ), harvest->tname() );
