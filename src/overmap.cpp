cdfae6c5ae7b3da4375d2ef5a6449a583d9031ca
Oren Audeles <orenaudeles@gmail.com>
2025-03-30 18:34:00 -0500

    fix: remove layer backup from class overmap
    It's used during city placement as temporary storage and nowhere else. Does not need to be a part of the class.

--- src/overmap.cpp
+++ src/overmap.cpp
@@ -2944,10 +2944,6 @@ void overmap::init_layers()
                 layer[k].visible[i][j] = false;
                 layer[k].explored[i][j] = false;
                 layer[k].path[i][j] = false;
-                layer_backup[k].terrain[i][j] = tid;
-                layer_backup[k].visible[i][j] = false;
-                layer_backup[k].explored[i][j] = false;
-                layer_backup[k].path[i][j] = false;
             }
         }
     }
@@ -4700,7 +4696,7 @@ void overmap::place_cities()
         // don't draw cities across the edge of the map, they will get clipped
         const tripoint_om_omt p{ rng( size - 1, OMAPX - size ), rng( size - 1, OMAPY - size ), 0 };
         //make a backup of the map
-        std::copy( std::begin( layer ), std::end( layer ), std::begin( layer_backup ) );
+        std::array<map_layer, 21UL> layer_backup = layer;
         tmp.finale_placed = false;
         int finale_attempts = 0;
         int finale_max_tries = 1500;
@@ -4730,7 +4726,7 @@ void overmap::place_cities()
 
                 //if the city finale failed to place, restore from last backup and try again at the top of the loop
                 if( !tmp.finale_placed  && tmp.attempt_finale && finale_attempts < finale_max_tries ) {
-                    std::copy( std::begin( layer_backup ), std::end( layer_backup ), std::begin( layer ) );
+                    layer = layer_backup;
                 }
             }
             finale_attempts++;
