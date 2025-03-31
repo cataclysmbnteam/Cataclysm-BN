5aa2a56abf347fa3f81f132cdf2d7d8bf638a549
Reisen Usagi <usagirei@users.noreply.github.com>
2025-03-30 22:53:00 -0300

    perf: parallelize overmap generation / overmap tile searches (#6215)
    * replace all overmapbuffer::find_x 'shortcuts' calls with their omt_find_params counterparts
    change search_range to be a min-max instead of two separate fields
    include search_layers in preparation for function unification
    
    * remove caching of last_requested_overmap, it breaks concurrency, and unordered_set (overmaps) has O(1) access on average
    add recursive_mutex for overmap modifying calls
    
    * add parallelizable function to overmapbuffer to generate multiple overmap tiles
    
    * add missing z constraint to mission defs
    
    * use unordered_set instead of set for faster known non existing lookup
    
    * fix concurrency issue on overmapbuffer::get
    reduce the amount of redundant overmap lookups on overmapbuffer::is_findable_location
    
    * make find_random and find_closest use find_all internally
    
    * split is_findable_location into two functions, with one that accepts an overmap_with_local_coords directly to avoid looking up an overmap tile if unnecessary
    
    * replace cant_see/must_see with an optional bool
    add explored as an optional bool
    
    * parallelize overmapbuffer::find_all
    use the new parallel find_all on reveal_map iuse_actor
    
    * style(autofix.ci): automated formatting
    
    * missing mutex include
    
    * add iterator style closest point generator
    
    * use parallel map generator on reveal_map_actor::reveal_targets
    
    * redo find_all parallelization to allow for early-returning and control parallelization count
    revert known_not_existing to a set instead of unordered_set to avoid some rogue concurrency issues
    
    * missions now pass their specified target terrain z-level to the overmap search
    
    * set find concurrency to hw - 1
    
    * Use suggested C++17/20 Features
    
    Co-authored-by: scarf <greenscarf005@gmail.com>
    
    * turns out equal_range does not return a range
    
    * add lock_guard to overmapbuffer::save for good measure
    
    * clip hw concurrency to at least 1 thread
    
    * constrain mission overmap find z-layers
    
    * constrain more overmap searches
    increase find_closest random sample size with number of searched terrain types
    
    * cleanup
    
    * rename closest_point_generator to spiral_generator
    add closest_points_generator method
    simplify closest_points_first to create a vector from closest_points_generator
    
    * fix spiral_generator initial value initialization
    
    * remove old leftover throbber code from when the scan was done in the main thread
    
    * dummy commit to trigger build bot
    
    * add missing fix_mongroups and fix_npcs to newly generated maps from parallel generation
    
    * Update reveal_map_actor::reveal_targets to generate maps in a bounding box fitting search radius
    
    * add synchronous version of find_all
    
    * add closing detail namespace comment
    
    * update overmapbuffer::get mutex to a read-write lock instead
    
    * no deadlock please
    
    * revert read-write lock changes until i can figure out why it only Works On My Machine (tm)
    
    * replace mutex with read-write lock with no deadlocks this time probably
    
    * include must_see param in target_om_ter_random find_params
    
    * stop CI warnings
    
    * remove unused find_params::post_filter
    
    * reduce unnecessary memory allocations on overmapbuffer::find_closest
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
    Co-authored-by: scarf <greenscarf005@gmail.com>

--- src/point.cpp
+++ src/point.cpp
@@ -48,69 +48,74 @@ std::ostream &operator<<( std::ostream &os, const tripoint &pos )
     return os << "(" << pos.x << "," << pos.y << "," << pos.z << ")";
 }
 
-std::vector<tripoint> closest_points_first( const tripoint &center, int max_dist )
+namespace detail
 {
-    return closest_points_first( center, 0, max_dist );
+spiral_generator_impl::spiral_generator_impl( point center, int min_dist, int max_dist )
+    : center( center )
+    , min_dist( min_dist )
+    , max_dist( max_dist )
+    , min_edge( min_dist * 2 + 1 )
+    , max_edge( max_dist * 2 + 1 )
+    , n( ( max_edge * max_edge ) - ( min_edge - 2 ) * ( min_edge - 2 ) )
+    , is_center_included( min_dist == 0 )
+{
+    i = is_center_included ? -1 : 0;
+    x = std::max( min_dist, 1 );
+    y = 1 - x;
+    dx = 1;
+    dy = 0;
+
+    p = is_center_included
+        ? center
+        : center + point{ x, y };
 }
 
-std::vector<tripoint> closest_points_first( const tripoint &center, int min_dist, int max_dist )
+spiral_generator_impl spiral_generator_impl::exhausted( point center, int min_dist, int max_dist )
 {
-    const std::vector<point> points = closest_points_first( center.xy(), min_dist, max_dist );
-
-    std::vector<tripoint> result;
-    result.reserve( points.size() );
-
-    for( point p : points ) {
-        result.emplace_back( p, center.z );
-    }
-
-    return result;
+    auto q = spiral_generator_impl( center, min_dist, max_dist );
+    q.i = q.n;
+    return q;
 }
 
-std::vector<point> closest_points_first( point center, int max_dist )
+spiral_generator_impl::operator bool() const noexcept
 {
-    return closest_points_first( center, 0, max_dist );
+    return i < n;
 }
 
-std::vector<point> closest_points_first( point center, int min_dist, int max_dist )
+bool spiral_generator_impl::next()
 {
-    min_dist = std::max( min_dist, 0 );
-    max_dist = std::max( max_dist, 0 );
-
-    if( min_dist > max_dist ) {
-        return {};
+    if( i >= n ) {
+        return false;
     }
 
-    const int min_edge = min_dist * 2 + 1;
-    const int max_edge = max_dist * 2 + 1;
-
-    const int n = max_edge * max_edge - ( min_edge - 2 ) * ( min_edge - 2 );
-    const bool is_center_included = min_dist == 0;
-
-    std::vector<point> result;
-    result.reserve( n + ( is_center_included ? 1 : 0 ) );
-
-    if( is_center_included ) {
-        result.push_back( center );
+    if( i++ < 0 ) {
+        p = center + point{ x, y };
+        return true;
     }
 
-    int x = std::max( min_dist, 1 );
-    int y = 1 - x;
-
-    int dx = 1;
-    int dy = 0;
-
-    for( int i = 0; i < n; i++ ) {
-        result.push_back( center + point{ x, y } );
-
-        if( x == y || ( x < 0 && x == -y ) || ( x > 0 && x == 1 - y ) ) {
-            std::swap( dx, dy );
-            dx = -dx;
-        }
-
-        x += dx;
-        y += dy;
+    if( x == y || ( x < 0 && x == -y ) || ( x > 0 && x == 1 - y ) ) {
+        std::swap( dx, dy );
+        dx = -dx;
     }
 
-    return result;
+    x += dx;
+    y += dy;
+
+    p = center + point{ x, y };
+
+    return true;
 }
+
+const point &spiral_generator_impl::current() const
+{
+    return p;
+}
+
+bool spiral_generator_impl::operator==( const spiral_generator_impl &other ) const
+{
+    return i == other.i
+           && center == other.center
+           && min_dist == other.min_dist
+           && max_dist == other.max_dist;
+}
+} // namespace detail
