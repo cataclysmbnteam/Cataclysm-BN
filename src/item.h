daba1bf6aa4a2930d4da7fc7dd74b6f6cf16c096
Karthmanter <56327189+Karthmanter@users.noreply.github.com>
2025-04-02 08:19:29 -0400

    make those two optional args

--- src/item.h
+++ src/item.h
@@ -1517,7 +1517,7 @@ class item : public location_visitable<item>, public game_object<item>
         /** Removes all item variables. */
         void clear_vars();
         /** Adds child items to the contents of this one. */
-        void add_item_with_id( const itype_id &itype, int count );
+        void add_item_with_id( const itype_id &itype, int count = 1 );
         /** Checks if this item contains an item with itype. */
         bool has_item_with_id( const itype_id &itype ) const;
         /*@}*/
