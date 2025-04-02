cfb1e4f6d964c1c87e6d77f5868738aaabf6612c
Karthmanter <56327189+Karthmanter@users.noreply.github.com>
2025-04-02 06:27:58 -0400

    fix create_item

--- src/catalua_bindings.cpp
+++ src/catalua_bindings.cpp
@@ -779,8 +779,8 @@ void cata::detail::reg_game_api( sol::state &lua )
         hooks.push_back( on_every_x_hooks{ interval, vec } );
     } );
 
-    luna::set_fx( lib, "create_item", []( const itype_id & itype, int count ) -> detached_ptr<item> {
-        return item::spawn( itype, calendar::turn, count );
+    luna::set_fx( lib, "create_item", []( const itype_id & itype, int count ) -> item {
+        return item( itype, calendar::turn, count );
     } );
 
     luna::set_fx( lib, "get_creature_at", []( const tripoint & p,
