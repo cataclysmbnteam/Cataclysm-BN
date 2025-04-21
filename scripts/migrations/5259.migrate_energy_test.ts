d79687783648398affdf3e1d6b9e5c7e56fcbb7b
scarf <greenscarf005@gmail.com>
2025-04-21 20:08:22 +0900

    test: more tests

--- scripts/migrations/5259.migrate_energy_test.ts
+++ scripts/migrations/5259.migrate_energy_test.ts
@@ -80,3 +80,68 @@ Deno.test("ItemMigrateEnergy migrates TOOL_ARMOR", () => {
   }
   assertEquals(v.parse(ItemMigrateEnergy, given), expected)
 })
+
+Deno.test("ItemMigrateEnergy migrates plastic jack o lantern", () => {
+  const given = {
+    "id": "plastic_jack_o_lantern",
+    "type": "TOOL",
+    "initial_charges": 100,
+    "max_charges": 100,
+    "ammo": ["battery"],
+    "use_action": {
+      "transform_charges": 1,
+      "type": "transform",
+    },
+  }
+  const expected = {
+    "id": "plastic_jack_o_lantern",
+    "type": "TOOL",
+    "use_action": {
+      "type": "transform",
+      "transform_power": "1 kJ",
+    },
+    "initial_power": "100 kJ",
+    "max_power": "100 kJ",
+  }
+  assertEquals(v.parse(ItemMigrateEnergy, given), expected)
+})
+
+Deno.test("ItemMigrateEnergy migrates test soldering iron", () => {
+  const given = {
+    "id": "test_soldering_iron",
+    "type": "TOOL",
+    "ammo": "battery",
+    "charges_per_use": 1,
+    "magazines": [
+      [
+        "battery",
+        [
+          "light_minus_battery_cell",
+          "light_battery_cell",
+          "light_plus_battery_cell",
+          "light_atomic_battery_cell",
+          "light_minus_atomic_battery_cell",
+          "light_minus_disposable_cell",
+          "light_disposable_cell",
+        ],
+      ],
+    ],
+    "magazine_well": 1,
+  }
+  const expected = {
+    "id": "test_soldering_iron",
+    "type": "TOOL",
+    "power_draw": "1 kJ",
+    "batteries": [
+      "light_minus_battery_cell",
+      "light_battery_cell",
+      "light_plus_battery_cell",
+      "light_atomic_battery_cell",
+      "light_minus_atomic_battery_cell",
+      "light_minus_disposable_cell",
+      "light_disposable_cell",
+    ],
+    battery_well: "250 ml",
+  }
+  assertEquals(v.parse(ItemMigrateEnergy, given), expected)
+})
