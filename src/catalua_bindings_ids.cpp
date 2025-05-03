e7009ef2f6826d7555fdb2620ac3d3fb17280437
NappingOcean <129575271+NappingOcean@users.noreply.github.com>
2025-05-03 20:52:53 +0900

    feat: add lua binding for `mtype_id` (#6471)
    * feat: Add lua binding for mtype
    
    * add-lua-doc
    
    * fix-enters
    
    * fix-docs
    
    * style(autofix.ci): automated formatting
    
    * Revert "Merge branch 'main' into add-lua-binding-for-mtype"
    
    This reverts commit f380f59b1c76e95e5942a3659559108ae8dd35ce, reversing
    changes made to fb3793e590ae161464a59aa3bb96e7f853dc33a2.
    
    * Reapply "Merge branch 'main' into add-lua-binding-for-mtype"
    
    This reverts commit 376fd2a17fefb2bd68178190557c07662e516c0f.
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- src/catalua_bindings_ids.cpp
+++ src/catalua_bindings_ids.cpp
@@ -19,6 +19,7 @@
 #include "monfaction.h"
 #include "monstergenerator.h"
 #include "morale_types.h"
+#include "mtype.h"
 #include "mutation.h"
 #include "recipe.h"
 #include "skill.h"
@@ -119,6 +120,7 @@ void cata::detail::reg_game_ids( sol::state &lua )
     reg_id<ma_buff, false>( lua );
     reg_id<monfaction, true>( lua );
     reg_id<morale_type_data, false>( lua );
+    reg_id<mtype, false>( lua );
     reg_id<mutation_branch, false>( lua );
     reg_id<mutation_category_trait, false>( lua );
     reg_id<recipe, false>( lua );
