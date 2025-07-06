ce3d6bcfefb21ed29902a3cc458bc1c7417255b1
Retagin <retagin@live.com>
2025-07-05 23:40:11 -0400

    feat(balance): Makes shooter skills+stats more important for hit location and potential aimed critical damage.
    Added two optional load json entries for ammo/projectiles
    Increases the maximum potential damage of aimed shots depending on a variety of factors
    Hit location influenced by a variety of factors instead just a random roll

---
+++ projectile.cpp
@@ -0,0 +1,170 @@
+#include "projectile.h"
+
+#include <algorithm>
+#include <memory>
+#include <utility>
+#include <vector>
+
+#include "ammo_effect.h"
+#include "explosion.h"
+#include "game.h"
+#include "json.h"
+#include "item.h"
+#include "map.h"
+#include "map_iterator.h"
+#include "rng.h"
+#include "string_id.h"
+
+projectile::projectile() : custom_explosion( nullptr )
+{ }
+
+projectile::~projectile() = default;
+
+projectile::projectile( projectile && )  noexcept = default;
+
+
+projectile::projectile( const projectile &other )
+{
+    *this = other;
+}
+
+projectile &projectile::operator=( const projectile &other )
+{
+    impact = other.impact;
+    speed = other.speed;
+    range = other.range;
+    proj_effects = other.proj_effects;
+    set_drop( item::spawn( *other.get_drop() ) );
+    set_custom_explosion( other.get_custom_explosion() );
+
+    return *this;
+}
+
+detached_ptr<item> projectile::unset_drop()
+{
+    detached_ptr<item> ret;
+    ret = std::move( drop );
+    return ret;
+}
+
+void projectile::set_drop( detached_ptr<item> &&it )
+{
+    drop = std::move( it );
+}
+
+item *projectile::get_drop() const
+{
+    if( !drop ) {
+        return &null_item_reference();
+    }
+    return &*drop;
+}
+
+const explosion_data &projectile::get_custom_explosion() const
+{
+    if( custom_explosion != nullptr ) {
+        return *custom_explosion;
+    }
+
+    static const explosion_data null_explosion{};
+    return null_explosion;
+}
+
+void projectile::set_custom_explosion( const explosion_data &ex )
+{
+    custom_explosion = std::make_unique<explosion_data>( ex );
+}
+
+void projectile::unset_custom_explosion()
+{
+    custom_explosion.reset();
+}
+
+void projectile::deserialize( JsonIn &jsin )
+{
+    JsonObject jo = jsin.get_object();
+    load( jo );
+}
+
+void projectile::load( JsonObject &jo )
+{
+    jo.read( "impact", impact );
+    range = jo.get_int( "range" );
+    speed = jo.get_int( "speed", 1000 );
+    aimedcritmaxbonus = jo.get_float( "aimedcritmaxbonus", 0.0 );
+    aimedcritbonus = jo.get_float( "aimedcritbonus", 0.0 );
+    jo.read( "proj_effects", proj_effects );
+}
+
+void apply_ammo_effects( const tripoint &p, const std::set<ammo_effect_str_id> &effects,
+                         Creature *source )
+{
+    map &here = get_map();
+    for( const ammo_effect_str_id &ae_id : effects ) {
+        const ammo_effect &ae = *ae_id;
+        if( ae.aoe_field_type )
+            for( auto &pt : here.points_in_radius( p, ae.aoe_radius, ae.aoe_radius_z ) ) {
+                if( x_in_y( ae.aoe_chance, 100 ) ) {
+                    const bool check_sees = !ae.aoe_check_sees || here.sees( p, pt, ae.aoe_check_sees_radius );
+                    const bool check_passable = !ae.aoe_check_passable || here.passable( pt );
+                    if( check_sees && check_passable ) {
+                        here.add_field( pt, ae.aoe_field_type, rng( ae.aoe_intensity_min, ae.aoe_intensity_max ) );
+                    }
+                }
+            }
+        if( ae.aoe_explosion_data ) {
+            explosion_handler::explosion( p, ae.aoe_explosion_data, source );
+        }
+        if( ae.do_flashbang ) {
+            explosion_handler::flashbang( p, false, "explosion" );
+        }
+        if( ae.do_emp_blast ) {
+            explosion_handler::emp_blast( p );
+        }
+    }
+}
+
+void apply_ammo_effects( const tripoint &p, const std::set<std::string> &effects, Creature *source )
+{
+    std::set<ammo_effect_str_id> effect_ids;
+    for( const std::string &s : effects ) {
+        ammo_effect_str_id id( s );
+        if( id.is_valid() ) {
+            effect_ids.emplace( id );
+        }
+    }
+    apply_ammo_effects( p, effect_ids, source );
+}
+
+static int aoe_of( const ammo_effect_str_id &ae_id )
+{
+    return std::max( ae_id->aoe_size, ae_id->aoe_explosion_data.safe_range() - 1 );
+}
+
+static int aoe_of( const std::string &s )
+{
+    ammo_effect_str_id ae_id( s );
+    if( ae_id.is_valid() ) {
+        return std::max( ae_id->aoe_size, ae_id->aoe_explosion_data.safe_range() - 1 );
+    } else {
+        return 0;
+    }
+}
+
+int max_aoe_size( const std::set<ammo_effect_str_id> &tags )
+{
+    int ret = 0;
+    for( const ammo_effect_str_id &ae_id : tags ) {
+        ret = std::max( ret, aoe_of( ae_id ) );
+    }
+    return ret;
+}
+
+int max_aoe_size( const std::set<std::string> &tags )
+{
+    int aoe_size = 0;
+    for( const std::string &s : tags ) {
+        aoe_size = std::max( aoe_size, aoe_of( s ) );
+    }
+    return aoe_size;
+}
---
+++ projectile.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include <memory>
+#include <set>
+#include <string>
+
+#include "damage.h"
+#include "point.h"
+#include "string_id.h"
+#include "detached_ptr.h"
+
+class Creature;
+struct explosion_data;
+class item;
+struct ammo_effect;
+
+using ammo_effect_str_id = string_id<ammo_effect>;
+
+struct projectile {
+        damage_instance impact;
+        // how hard is it to dodge? essentially rolls to-hit,
+        // bullets have arbitrarily high values but thrown objects have dodgeable values.
+        // TODO: Get rid of this, replace with something sane (or just get rid)
+        int speed = 0;
+        int range = 0;
+        double aimedcritmaxbonus = 0.0;
+        double aimedcritbonus = 0.0;
+
+        /**
+         * Returns an item that should be dropped or an item for which is_null() is true
+         *  when item to drop is unset.
+         */
+        item *get_drop() const;
+        /** Copies item `it` as a drop for this projectile. */
+        void set_drop( detached_ptr<item> &&it );
+        detached_ptr<item> unset_drop();
+
+        const explosion_data &get_custom_explosion() const;
+        void set_custom_explosion( const explosion_data &ex );
+        void unset_custom_explosion();
+
+        const std::set<ammo_effect_str_id> &get_ammo_effects() const {
+            return proj_effects;
+        }
+
+        projectile();
+        projectile( const projectile & );
+        projectile( projectile && ) noexcept ;
+        projectile &operator=( const projectile & );
+        ~projectile();
+
+        void deserialize( JsonIn &jsin );
+        void load( JsonObject &jo );
+
+        bool has_effect( const ammo_effect_str_id &id ) const {
+            return proj_effects.contains( id );
+        }
+        void add_effect( const ammo_effect_str_id &id ) {
+            proj_effects.insert( id );
+        }
+
+    private:
+        // Actual item used (to drop contents etc.).
+        // Null in case of bullets (they aren't "made of cartridges").
+        detached_ptr<item> drop;
+        std::unique_ptr<explosion_data> custom_explosion;
+        std::set<ammo_effect_str_id> proj_effects;
+};
+
+struct dealt_projectile_attack {
+    projectile proj; // What we used to deal the attack
+    Creature *hit_critter; // The critter that stopped the projectile or null
+    dealt_damage_instance dealt_dam; // If hit_critter isn't null, hit data is written here
+    tripoint end_point; // Last hit tile (is hit_critter is null, drops should spawn here)
+    double missed_by; // Accuracy of dealt attack
+};
+
+void apply_ammo_effects( const tripoint &p, const std::set<ammo_effect_str_id> &effects,
+                         Creature *source );
+// Legacy. TODO: Remove
+void apply_ammo_effects( const tripoint &p, const std::set<std::string> &effects,
+                         Creature *source );
+int max_aoe_size( const std::set<ammo_effect_str_id> &tags );
+int max_aoe_size( const std::set<std::string> &tags );
+
+
