9046bd6d5a608cdb9545316f80ba60c7b89bc1be
scarf <greenscarf005@gmail.com>
2025-08-03 14:21:07 +0900

    refactor: organize plugins

--- docs/_config.ts
+++ docs/_config.ts
@@ -1,36 +1,16 @@
 import lume from "lume/mod.ts"
-import date from "lume/plugins/date.ts"
 import wiki from "wiki/mod.ts"
-import { slugify } from "lume/cms/core/utils/string.ts"
 
-import { SEPARATOR } from "lume/deps/path.ts"
-
-import "npm:prismjs/components/prism-c.js"
-import "npm:prismjs/components/prism-cpp.js"
-import "npm:prismjs/components/prism-diff.js"
-import "npm:prismjs/components/prism-bash.js"
+import date from "lume/plugins/date.ts"
 
 import { ko } from "npm:date-fns/locale/ko"
+
+import { autoId } from "./plugins/auto_id.ts"
+import "./plugins/prism.ts"
 
 const site = lume()
 
 const languages = ["en", "ko", "ru", "de"]
-
-/**
- * Assigns default `id` to files based on their path excluding the language code.
- * e.g `/ko/foo/bar.md` will have `id` of `foo/bar`
- */
-const autoId = (options: { languages: string[] }) => {
-  const LANGUAGE_CODE = new RegExp(
-    `^\\${SEPARATOR}(${options.languages.join("|")})(?=\\${SEPARATOR})`,
-  )
-  return (site: Lume.Site) => {
-    site.preprocess([".md"], (files) =>
-      files.forEach((file) => {
-        if (!file.data.id) file.data.id = slugify(file.src.path).replace(LANGUAGE_CODE, "")
-      }))
-  }
-}
 
 site
   .ignore("README.md")
