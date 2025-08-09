5367dd53ada9c4e3fb99fe57bc176e9d27cfa9d0
scarf <greenscarf005@gmail.com>
2025-08-09 13:38:49 +0900

    feat: run hooks with custom parameter tables
    now even more configurable (without generic shenanigans)!

--- src/catalua_hooks.h
+++ src/catalua_hooks.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <functional>
 #include <string_view>
+
+#include "catalua_sol.h"
 
 class lua_state;
 
@@ -10,6 +13,13 @@ namespace cata
 /// Run Lua hooks registered with given name.
 /// an empty table needs to be initialized in `init_global_state_tables` first.
 void run_hooks( std::string_view hooks_table );
+
+/// Run Lua hooks registered with given name.
+/// an empty table needs to be initialized in `init_global_state_tables` first.
+///
+/// To pass parameters to the hook, use `init` function and set values to table.
+void run_hooks( std::string_view hooks_table,
+                std::function < auto( sol::table &params ) -> void > init );
 
 /// Define all hooks that are used in the game.
 void define_hooks( lua_state &state );
