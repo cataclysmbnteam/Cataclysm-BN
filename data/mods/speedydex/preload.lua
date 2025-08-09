ede5155d8456d33447ba34c9ae6fc3fc05e24460
scarf <greenscarf005@gmail.com>
2025-08-09 09:24:26 +0900

    fix(mods/speedydex): make speedup actually work again (#6916)
    fix: use closure
    
    `preload.lua` runs _before_ `main.lua`. unless wrapped with closure, mod.speedydex is `nil`

--- data/mods/speedydex/preload.lua
+++ data/mods/speedydex/preload.lua
@@ -3,4 +3,4 @@ gdebug.log_info("SpeedyDex: Preload")
 ---@class ModSpeedyDex
 local mod = game.mod_runtime[game.current_mod]
 
-table.insert(game.hooks.on_character_reset_stats, mod.speedydex)
+table.insert(game.hooks.on_character_reset_stats, function(...) return mod.speedydex(...) end)
