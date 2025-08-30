ce5bbc669bf1b50c8cb8b6db9dce103725ff6316
WishDuck <github@johnosshiggins.com>
2025-08-30 14:43:18 -0700

    The mod was totally suppost to be UPS toggleable

--- src/avatar.cpp
+++ src/avatar.cpp
@@ -1332,7 +1332,8 @@ bool avatar::invoke_item( item *used, const tripoint &pt )
 {
     std::map<std::string, use_function> use_methods;
     use_methods.insert( used->type->use_methods.begin(), used->type->use_methods.end() );
-    if( used->has_flag( flag_ADD_UPS_TOGGLE ) ) {
+    // Make sure the flag is that of the mod, not of the item being used :P
+    if( used->has_flag( flag_ADD_UPS_TOGGLE ) && !used->type->has_flag( flag_ADD_UPS_TOGGLE ) ) {
         use_methods["TOGGLE_UPS_CHARGING"] = item_controller->usage_from_string( "TOGGLE_UPS_CHARGING" );
     }
     if( use_methods.empty() ) {
