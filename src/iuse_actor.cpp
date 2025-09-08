7e8aff30d45e67efe01ee2c6e11cdcf2a3094759
Autumn <55225562+AutumnNova@users.noreply.github.com>
2025-09-08 14:38:55 +1000

    float hell

--- src/iuse_actor.cpp
+++ src/iuse_actor.cpp
@@ -5357,7 +5357,9 @@ int multicooker_iuse::use( player &p, item &it, bool t, const tripoint &pos ) co
 
             return 0;
         } else {
-            it.ammo_consume( charges_per_turn, pos );
+            int to_consume = charges_per_turn;
+            to_consume += x_in_y( int (charges_per_turn * 100) % 100, 100 ) ? 1 : 0;
+            it.ammo_consume( to_consume, pos );
             it.set_var( "COOKTIME", cooktime );
             return 0;
         }
@@ -5462,7 +5464,7 @@ int multicooker_iuse::use( player &p, item &it, bool t, const tripoint &pos ) co
                     const bool can_make = r->deduped_requirements().can_make_with_inventory(
                                               crafting_inv, r->get_component_filter() );
                     dmenu.addentry( counter++, can_make, -1, string_format( _( "%s ( %1.f charges)" ), r->result_name(),
-                                    ( r->time * time_mult / ( charges_per_turn * 100 ) ) + 50 ) );
+                                    r->time * time_mult / 100 * charges_per_turn + charges_to_start ) );
                 }
             }
 
@@ -5481,7 +5483,7 @@ int multicooker_iuse::use( player &p, item &it, bool t, const tripoint &pos ) co
             } else {
                 const recipe *meal = dishes[choice];
                 int mealtime = meal->time * time_mult;
-                int all_charges = charges_to_start + ( mealtime / ( charges_per_turn * 100 ) );
+                int all_charges = mealtime / 100 * charges_per_turn + charges_to_start;
 
                 if( it.ammo_remaining() < all_charges ) {
 
