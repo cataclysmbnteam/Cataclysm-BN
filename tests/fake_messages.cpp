333e9a10363af31160dc66da555928fff0a0979b
shmakota <47482311+shmakota@users.noreply.github.com>
2025-09-09 17:19:19 -0500

    fix linker error? don't know what this is actually doing under the hood

--- tests/fake_messages.cpp
+++ tests/fake_messages.cpp
@@ -39,5 +39,19 @@ void Messages::display_messages( const catacurses::window &, int, int, int, int 
 void Messages::serialize( JsonOut & ) {}
 void Messages::deserialize( const JsonObject & ) {}
 
+// Returns pairs of message log type id and untranslated name
+const std::vector<std::pair<game_message_type, const char *>> &msg_type_and_names()
+{
+    static const std::vector<std::pair<game_message_type, const char *>> stub_type_n_names = {
+        { m_good, "good" },
+        { m_bad, "bad" },
+        { m_mixed, "mixed" },
+        { m_warning, "warning" },
+        { m_info, "info" },
+        { m_neutral, "neutral" },
+        { m_debug, "debug" }
+    };
+    return stub_type_n_names;
+}
 void add_msg( std::string ) {}
 void add_msg( const game_message_params &, std::string ) {}
