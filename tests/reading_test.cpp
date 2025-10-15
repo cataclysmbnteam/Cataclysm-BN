c0ea1c0f8c8d7fe04d244552b7beed9d0ce75f8f
Reisen Usagi <usagirei@users.noreply.github.com>
2025-10-15 03:37:27 -0300

    update reading_test.cpp to use book_recipe instead of islot_book::recipe_with_description_t

--- tests/reading_test.cpp
+++ tests/reading_test.cpp
@@ -375,7 +375,7 @@ TEST_CASE( "Learning recipes from books", "[reading][book][recipe]" )
     REQUIRE( alpha.type->book );
     const auto book_recipes = alpha.type->book->recipes;
     bool book_has_recipe = std::find_if( book_recipes.begin(),
-    book_recipes.end(), [rec]( const islot_book::recipe_with_description_t &rec_d ) {
+    book_recipes.end(), [rec]( const book_recipe &rec_d ) {
         return rec_d.recipe == rec;
     } ) != book_recipes.end();
     REQUIRE( book_has_recipe );
