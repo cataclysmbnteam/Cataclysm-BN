fc774727ae38186cb87d0b4397a15b06e0f436d6
Reisen Usagi <usagirei@users.noreply.github.com>
2025-10-18 14:15:35 -0300

    fix json floating point parsing

--- tests/json_test.cpp
+++ tests/json_test.cpp
@@ -2,6 +2,7 @@
 
 #include <list>
 #include <sstream>
+#include <utility>
 
 #include "bodypart.h"
 #include "json.h"
@@ -479,5 +480,255 @@ TEST_CASE( "serialize_optional", "[json]" )
     SECTION( "vector_of_optional_of_int" ) {
         std::vector<std::optional<int>> v{ { 1 }, { 2 }, { 3 } };
         test_serialization( v, "[1,2,3]" );
+    }
+}
+
+// Float comparison woes
+template<typename T>
+requires std::is_floating_point_v<T>
+static bool approximatelyEqual( T a, T b, T eps, const unsigned ulps )
+{
+    using limits = std::numeric_limits<T>;
+
+    if( std::isnan( a ) || std::isnan( b ) ) {
+        return false;
+    }
+
+    if( std::signbit( a ) != std::signbit( b ) ) {
+        return false;
+    }
+
+    if( std::abs( a - b ) <= ulps * limits::denorm_min() ) {
+        return true;
+    }
+
+    int min_exp, max_exp;
+    T min_frac = std::frexp( a, &min_exp );
+    T max_frac = std::frexp( b, &max_exp );
+    if( min_exp > max_exp ) {
+        std::swap( min_frac, max_frac );
+        std::swap( min_exp, max_exp );
+    }
+
+    const T min_frac2 = std::ldexp( min_frac, min_exp - max_exp );
+    const T delta = std::abs( max_frac - min_frac2 );
+
+    return delta <= ulps * eps / 2;
+}
+
+template<typename T>
+requires std::is_integral_v<T>
+constexpr static bool approximatelyEqual( T a, T b, T eps, const unsigned)
+{
+    if (b > a)
+        std::swap( a, b );
+    return static_cast<T>(a - b) <= eps;
+}
+
+template<typename T>
+static void test_number_roundtrip( T val, T eps, int ulps )
+{
+    T read_val;
+    CAPTURE( val );
+    {
+        INFO( "test_round_trip" );
+        std::ostringstream os;
+        JsonOut jsout( os );
+        jsout.write( val );
+        const auto repr = os.str();
+        CAPTURE( repr );
+
+        std::istringstream is( repr );
+        JsonIn jsin( is );
+        CHECK( jsin.read( read_val ) );
+        CAPTURE( read_val );
+        double delta = read_val - val;
+        CAPTURE( delta );
+        bool flag = approximatelyEqual( val, read_val, eps, ulps );
+        if( !flag ) {
+            CAPTURE( val );
+            std::istringstream is2( repr );
+            JsonIn jsin2( is2 );
+            jsin2.read( read_val );
+        }
+        CHECK( flag );
+    }
+}
+
+TEST_CASE( "serialize_integers", "[json]" )
+{
+    SECTION( "bad_values" ) {
+        std::vector<std::string> to_check = {"", " ", "1e", "2E", "00", "a", "1e-4" };
+        for( const auto &str : to_check ) {
+            std::stringstream is { str };
+            uint64_t read_val;
+            JsonIn jsin( is );
+            CHECK_THROWS( jsin.read( read_val, true ) );
+            CAPTURE( read_val );
+            CAPTURE( str );
+        }
+    }
+
+    SECTION( "space_padded" ) {
+        std::vector<std::string> to_check = {"   123   ", "  456", "789   "  };
+        for( const auto &str : to_check ) {
+            std::stringstream is { str };
+            uint64_t read_val;
+            JsonIn jsin( is );
+            CHECK( jsin.read( read_val, true ) );
+            CAPTURE( read_val );
+            CAPTURE( str );
+        }
+    }
+
+    SECTION( "single_digit" ) {
+        for( uint32_t i = -9; i <= 9; i++ ) {
+            std::stringstream os;
+            JsonOut jsout( os );
+            jsout.write( i );
+            auto str = os.str();
+            CAPTURE( i );
+            CAPTURE( str );
+
+            std::stringstream is { str };
+            uint64_t read_val;
+            JsonIn jsin( is );
+            CHECK( jsin.read( read_val ) );
+            CAPTURE( read_val );
+
+            CHECK( read_val == i );
+        }
+    }
+
+    SECTION( "exponent" ) {
+        for( uint32_t i = 1; i <= 9; i++ ) {
+            std::string str = string_format("1e-%d", i);
+            std::stringstream is { str };
+            uint64_t read_val;
+            JsonIn jsin( is );
+            CHECK_THROWS( jsin.read( read_val, true ) );
+            CAPTURE( read_val );
+            CAPTURE( str );
+        }
+        for( uint32_t i = 0; i <= 9; i++ ) {
+            std::string str = string_format("1e%d", i);
+            std::stringstream is { str };
+            uint64_t read_val;
+            JsonIn jsin( is );
+            CHECK( jsin.read( read_val, true ) );
+            CAPTURE( read_val );
+            CAPTURE( str );
+        }
+    }
+
+    SECTION( "random_unsigned" ) {
+        std::random_device rd;
+        std::mt19937 gen( rd() );
+        std::uniform_int_distribution<uint64_t> distrib( std::numeric_limits<uint64_t>::min(), std::numeric_limits<uint64_t>::max() );
+
+        for( int i = 0; i < 100000; i++ ) {
+            test_number_roundtrip<uint64_t>( distrib( gen ), 0, 0 );
+        }
+    }
+
+    SECTION( "random_signed" ) {
+        std::random_device rd;
+        std::mt19937 gen( rd() );
+        std::uniform_int_distribution<int64_t> distrib( std::numeric_limits<int64_t>::min(), std::numeric_limits<int64_t>::max() );
+
+        for( int i = 0; i < 100000; i++ ) {
+            test_number_roundtrip<int64_t>( distrib( gen ), 0, 0 );
+        }
+    }
+}
+
+TEST_CASE( "serialize_doubles", "[json]" )
+{
+    // 64-bit IEEE 754 float -> text -> float roundtrip is only guaranteed for 15 decimal digits
+    constexpr double eps = 1E-15;
+
+    SECTION( "validate_checker" ) {
+        for( int i = 0; i < std::numeric_limits<double>::max_exponent10; i++ ) {
+            double a = std::pow( 10, i );
+            double b = std::pow( 10, i - 1 );
+            bool flag = approximatelyEqual( a, b, eps, 4 );
+            double delta = fabs( b - a );
+            CAPTURE( delta );
+            CHECK_FALSE( flag );
+        }
+    }
+
+    SECTION( "bad_values" ) {
+        std::vector<std::string> to_check = {"", " ", "1e", "2E", "00", "a" };
+        for( const auto &str : to_check ) {
+            std::stringstream is { str };
+            double read_val;
+            JsonIn jsin( is );
+            CHECK_THROWS( jsin.read( read_val, true ) );
+            CAPTURE( read_val );
+            CAPTURE( str );
+        }
+    }
+
+    SECTION( "space_padded" ) {
+        std::vector<std::string> to_check = {"   123   ", "  456", "789   "  };
+        for( const auto &str : to_check ) {
+            std::stringstream is { str };
+            double read_val;
+            JsonIn jsin( is );
+            CHECK( jsin.read( read_val, true ) );
+            CAPTURE( read_val );
+            CAPTURE( str );
+        }
+    }
+
+    SECTION( "single_digit" ) {
+        for( int i = 0; i <= 9; i++ ) {
+            std::stringstream os;
+            JsonOut jsout( os );
+            jsout.write( i );
+            auto str = os.str();
+            CAPTURE( i );
+            CAPTURE( str );
+
+            std::stringstream is { str };
+            double read_val;
+            JsonIn jsin( is );
+            CHECK( jsin.read( read_val ) );
+            CAPTURE( read_val );
+
+            CHECK( read_val == i );
+        }
+    }
+
+    SECTION( "test_exponents" ) {
+        for( int i = 0; i < std::numeric_limits<double>::max_exponent10; i++ ) {
+            test_number_roundtrip( std::pow( 10, i ), eps, 4 );
+        }
+    }
+
+    SECTION( "random_doubles_large" ) {
+        std::random_device rd;
+        std::mt19937 gen( rd() );
+        std::uniform_real_distribution<> distrib( std::numeric_limits<double>::min(),
+                std::numeric_limits<double>::max() );
+
+        for( int i = 0; i < 100000; i++ ) {
+            test_number_roundtrip( distrib( gen ), eps, 4 );
+        }
+    }
+    SECTION( "random_doubles_small" ) {
+        std::random_device rd;
+        std::mt19937 gen( rd() );
+        std::uniform_int_distribution<uint64_t> distrib( 0, 999999999999999UL );
+
+        for( int i = 0; i < 100000; i++ ) {
+            // Generate a synthetic double with 15 decimal digits
+            // so it can survive the round trip from text and back
+            std::string str = string_format( "0.%015d", distrib( gen ) );
+            const double dbl = std::atof( str.c_str() );
+
+            test_number_roundtrip( dbl, eps, 4 );
+        }
     }
 }
