8977e6e97f859831fa293a6b28a4904ba9632d9f
Reisen Usagi <usagirei@users.noreply.github.com>
2025-10-23 13:39:50 -0300

    wrap json_converter read/write in a try-catch block because throw_on_error = false is a suggestion

--- src/data_vars_cvt.h
+++ src/data_vars_cvt.h
@@ -14,18 +14,32 @@ template<typename T>
 struct json_converter {
     using value_type = T;
 
+    // Need a try-catch blocks because JsonIn/Out
+    // Doesn't care if you ask it to not throw on error
+    // And may just do so anyway
+
     bool operator()( const T &in_val, std::string &out_val ) const {
-        std::ostringstream os;
-        JsonOut jsout{os};
-        jsout.write( in_val );
-        out_val = os.str();
-        return true;
+        try {
+            std::ostringstream os;
+            JsonOut jsout{os};
+            jsout.write( in_val );
+            out_val = os.str();
+            return true;
+        } catch( JsonError &e ) {
+            debugmsg( "Error writing value: %s", e.what() );
+            return false;
+        }
     }
 
     bool operator()( const std::string &in_val, T &out_val ) const {
-        std::istringstream is{in_val};
-        JsonIn jsin{is};
-        return jsin.read( out_val, false );
+        try {
+            std::istringstream is{in_val};
+            JsonIn jsin{is};
+            return jsin.read( out_val, false );
+        } catch( JsonError &e ) {
+            debugmsg( "Error reading value '%s': %s", in_val, e.what() );
+            return false;
+        }
     }
 };
 
