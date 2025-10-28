ca048796c3df457a9bb81c605fedebc28fe1ce6c
Reisen Usagi <usagirei@users.noreply.github.com>
2025-10-28 15:53:08 -0300

    add sdl render state save/restore helper function

--- src/sdl_utils.h
+++ src/sdl_utils.h
@@ -27,6 +27,84 @@ inline bool is_black( const SDL_Color &color )
         color.b == 0x00;
 }
 
+template<bool RT = true, bool CR = false, bool VP = false, bool DC = false, bool BM = false>
+struct sdl_render_state {
+    using tRT = std::conditional_t<RT, std::tuple<SDL_Texture*>, std::tuple<>>;
+    using tCR = std::conditional_t<CR, std::tuple<SDL_Rect>, std::tuple<>>;
+    using tVP = std::conditional_t<VP, std::tuple<SDL_Rect>, std::tuple<>>;
+    using tDC = std::conditional_t<DC, std::tuple<SDL_Color>, std::tuple<>>;
+    using tBM = std::conditional_t<BM, std::tuple<SDL_BlendMode>, std::tuple<>>;
+
+    constexpr static size_t RT_IDX = 0;
+    constexpr static size_t CR_IDX = CR ? (RT_IDX + 1) : RT_IDX;
+    constexpr static size_t VP_IDX = VP ? (CR_IDX + 1) : CR_IDX;
+    constexpr static size_t DC_IDX = DC ? (VP_IDX + 1) : VP_IDX;
+    constexpr static size_t BM_IDX = BM ? (DC_IDX + 1) : DC_IDX;
+
+    using tuple_type = decltype(std::tuple_cat(tRT{}, tCR{}, tVP{}, tDC{}, tBM{}));
+
+    tuple_type value;
+};
+
+template<bool RT = true, bool CR = false, bool VP = false, bool DC = false, bool BM = false>
+auto sdl_save_render_state( SDL_Renderer* r ) -> sdl_render_state<RT, CR, VP, DC, BM > {
+
+    using type = sdl_render_state<RT, CR, VP, DC, BM >;
+
+    typename type::tuple_type res;
+    if constexpr (RT) {
+        std::get<type::RT_IDX>(res) = SDL_GetRenderTarget( r );
+    }
+    if constexpr (CR) {
+        SDL_Rect& v = std::get<type::CR_IDX>(res);
+        SDL_RenderGetClipRect( r, &v );
+    }
+    if constexpr (VP) {
+        SDL_Rect& v = std::get<type::VP_IDX>(res);
+        SDL_RenderGetViewport( r, &v );
+    }
+    if constexpr (DC) {
+        SDL_Color& v = std::get<type::DC_IDX>(res);
+        SDL_GetRenderDrawColor(r, &v.r, &v.g, &v.b, &v.a);
+    }
+    if constexpr (BM) {
+        SDL_BlendMode& v = std::get<type::BM_IDX>(res);
+        SDL_GetRenderDrawBlendMode(r, &v);
+    }
+    return type{res};
+}
+
+template<bool RT = true, bool CR = false, bool VP = false, bool DC = false, bool BM = false>
+auto sdl_restore_render_state( SDL_Renderer* r, const sdl_render_state<RT, CR, VP, DC, BM> &state ) {
+    auto& t = state.value;
+    using type = sdl_render_state<RT, CR, VP, DC, BM>;
+    if constexpr (RT) {
+        const auto i = type::RT_IDX;
+        SDL_Texture* v = std::get<type::RT_IDX>(t);
+        SDL_SetRenderTarget(r, v);
+    }
+    if constexpr (CR) {
+        const auto i = type::CR_IDX;
+        const SDL_Rect& v = std::get<type::CR_IDX>(t);
+        SDL_RenderSetClipRect(r, &v);
+    }
+    if constexpr (VP) {
+        const auto i = type::VP_IDX;
+        const SDL_Rect& v = std::get<type::VP_IDX>(t);
+        SDL_RenderSetViewport(r, &v);
+    }
+    if constexpr (DC) {
+        const auto i = type::DC_IDX;
+        const SDL_Color& v = std::get<type::DC_IDX>(t);
+        SDL_SetRenderDrawColor(r, v.r, v.g, v.b, v.a);
+    }
+    if constexpr (BM) {
+        const auto i = type::BM_IDX;
+        const SDL_BlendMode& v = std::get<type::BM_IDX>(t);
+        SDL_SetRenderDrawBlendMode(r, v);
+    }
+}
+
 inline Uint8 average_pixel_color( const SDL_Color &color )
 {
     return 85 * ( color.r + color.g + color.b ) >> 8; // 85/256 ~ 1/3
