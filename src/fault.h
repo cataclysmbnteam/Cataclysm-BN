d4d2bbca9cae998be04549ab6b5c16d0e831f213
Reisen Usagi <usagirei@users.noreply.github.com>
2025-12-05 02:13:30 -0300

    fault is class not struct

--- src/fault.h
+++ src/fault.h
@@ -27,14 +27,14 @@ struct mending_method {
 class fault
 {
     public:
-        fault() : id_( fault_id( "null" ) ) {}
+        fault() : id_( fault_id::NULL_ID() ) {}
 
         const fault_id &id() const {
             return id_;
         }
 
         bool is_null() const {
-            return id_ == fault_id( "null" );
+            return id_ == fault_id::NULL_ID();
         }
 
         std::string name() const {
--- src/string_id_null_ids.cpp
+++ src/string_id_null_ids.cpp
@@ -45,8 +45,7 @@ MAKE_CLASS_NULL_ID( zone_type, "null" )
 MAKE_CLASS_NULL_ID( profession, "" )
 MAKE_CLASS_NULL_ID( vitamin, "" )
 MAKE_CLASS_NULL_ID( gun_mode, "" )
-
-
+MAKE_CLASS_NULL_ID( fault, "null" )
 
 #define MAKE_STRUCT_NULL_ID( type, ... ) \
     struct type; \
@@ -79,6 +78,5 @@ MAKE_STRUCT_NULL_ID( trap, "tr_null" )
 MAKE_STRUCT_NULL_ID( vehicle_prototype, "null" )
 MAKE_STRUCT_NULL_ID( weather_type, "null" )
 MAKE_STRUCT_NULL_ID( MOD_INFORMATION, "" )
-MAKE_STRUCT_NULL_ID( fault, "null" )
 MAKE_STRUCT_NULL_ID( quality, "" )
 MAKE_STRUCT_NULL_ID( weapon_category, "" )
