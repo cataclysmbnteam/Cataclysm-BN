fbbf284a67f2989c32447acb4632a9bc85c91dd5
RobbieNeko <30732426+RobbieNeko@users.noreply.github.com>
2025-12-07 22:08:36 -0800

    feat: Add `DIVIDE_DAMAGE` spell flag (#7565)
    * feat: Add `DIVIDE_DAMAGE` spell flag
    
    * perf: calculate affected creatures once
    
    We don't need to go over the list of potential targets *for every target*, we can just do it once.
    
    This also lets the number of affected be of use to other functions, although for the moment it is a slight negative to the perf for spells that don't do divided damage
    
    * style(autofix.ci): automated formatting
    
    * refactor: use std::ranges::count_if
    
    Co-authored-by: scarf <greenscarf005@gmail.com>
    
    * style(autofix.ci): automated formatting
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
    Co-authored-by: scarf <greenscarf005@gmail.com>

--- src/magic_spell_effect.cpp
+++ src/magic_spell_effect.cpp
@@ -481,6 +481,7 @@ static void damage_targets( const spell &sp, Creature &caster,
                             const std::set<tripoint> &targets )
 {
     bool sound_played = false;
+    const int affected = std::ranges::count_if( targets, [&]( const auto & target ) { return g->critter_at<Creature>( target ) != nullptr; } );
     for( const tripoint &target : targets ) {
         if( !sp.is_valid_target( caster, target ) ) {
             continue;
@@ -499,6 +500,9 @@ static void damage_targets( const spell &sp, Creature &caster,
         bolt.speed = 10000;
         bolt.impact = ( caster.is_monster() ) ? sp.get_damage_instance() : sp.get_damage_instance(
                           *caster.as_character() );
+        if( sp.has_flag( spell_flag::DIVIDE_DAMAGE ) ) {
+            bolt.impact.mult_damage( 1.0f / affected );
+        }
         bolt.add_effect( ammo_effect_magic );
 
         dealt_projectile_attack atk;
