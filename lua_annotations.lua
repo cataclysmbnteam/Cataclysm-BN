cataclysmbnteam/Cataclysm-BN

commit c6d9aae68cbb504f9fd431bf8f21da1f11f249fc

feat(lua)!: add more bindings (#7365)
* Allow for Lua table iuse_actor with separate `use` and `can_use` predicate

* Add tick function support to lua iuse_actor

* Add missing parameters to can_use call

* Add LUA_TYPE_OPS and register ids to ammunition_type and martialarts

* move recipe_with_description_t into its own type (book_recipe) and register it with lua

* separate catalua item bindings into its own file

* Add Islot_* members to ItypeRaw
expose Islot types to lua

* add members for IslotContainer

* add members for IslotTool

* add members for IslotPetArmor

* add members for IslotBook

* add members for IslotSeed

* add members for IslotBrewable

* add members for IslotArmor

* register remaining Islot types (without members)

* rename item::set_countdown to item::set_charges, since it's what it does, and what iuse_explosion expects to trigger.
expose set_charges to lua

* implement an actual set_countdown, that sets the timer until countdown_action is triggered.
expose it to lua

* add items_with to character
add get_item_at_with, get_items_in_radius, get_items_in_radius_with to map

* chore: disable clang-tidy operator parentheses warning (#7362)

1. it's highly annoying
2. there's already too much missing parentheses
3. not much benefit in fixing them all taking the huge diff toll

* feat(i18n): routine i18n updates on 2025-10-14

* style(autofix.ci): automated formatting

* update reading_test.cpp to use book_recipe instead of islot_book::recipe_with_description_t

* add bindidings for book_recipe

* update docs

* style(autofix.ci): automated formatting

* add std::optional to sol doc gen
use sol demangler for unnamed c++ types instead of typeid

* more bindings

* style(autofix.ci): automated formatting

* move member comments below member type on lua docs
add extra newline so they're rendered separately

* update doc generator
* hide operators functions (__eq, __lt, __tostring)
* hide serialization functions
* place docstring inside blockquotes

src/item.cpp
@@ -610,9 +610,7 @@ void item::activate()
         return; // no-op
     }
 
-    if( type->countdown_interval > 0 ) {
-        item_counter = type->countdown_interval;
-    }
+    set_countdown( -1 );
 
     active = true;
 
@@ -3154,7 +3152,7 @@ void item::book_info( std::vector<iteminfo> &info, const iteminfo_query *parts, 
     }
 
     std::vector<std::string> recipe_list;
-    for( const islot_book::recipe_with_description_t &elem : book.recipes ) {
+    for( const book_recipe &elem : book.recipes ) {
         const bool knows_it = you.knows_recipe( elem.recipe );
         const bool can_learn = you.get_skill_level( elem.recipe->skill_used )  >= elem.skill_level;
         // If the player knows it, they recognize it even if it's not clearly stated.
@@ -7454,9 +7452,9 @@ float item::fuel_energy() const
     return is_fuel() ? type->fuel->energy : 0.0f;
 }
 
-std::string item::fuel_pump_terrain() const
+ter_id item::fuel_pump_terrain() const
 {
-    return is_fuel() ? type->fuel->pump_terrain : "t_null";
+    return is_fuel() ? type->fuel->pump_terrain : t_null;
 }
 
 bool item::has_explosion_data() const
@@ -7745,7 +7743,7 @@ std::vector<std::pair<const recipe *, int>> item::get_available_recipes( const C
 {
     std::vector<std::pair<const recipe *, int>> recipe_entries;
     if( is_book() ) {
-        for( const islot_book::recipe_with_description_t &elem : type->book->recipes ) {
+        for( const book_recipe &elem : type->book->recipes ) {
             if( u.get_skill_level( elem.recipe->skill_used ) >= elem.skill_level ) {
                 recipe_entries.emplace_back( elem.recipe, elem.skill_level );
             }
@@ -9328,17 +9326,27 @@ detached_ptr<item> item::fill_with( detached_ptr<item> &&liquid, int amount )
     return detached_ptr<item>();
 }
 
-void item::set_countdown( int num_turns )
+void item::set_countdown( int value )
 {
-    if( num_turns < 0 ) {
-        debugmsg( "Tried to set a negative countdown value %d.", num_turns );
+    if( value < 0 ) {
+        item_counter = type->countdown_interval;
+        return;
+    }
+
+    item_counter = value;
+}
+
+void item::set_charges( int value )
+{
+    if( value < 0 ) {
+        debugmsg( "Tried to set a negative charges value %d.", value );
         return;
     }
     if( !ammo_types().empty() ) {
-        debugmsg( "Tried to set countdown on an item with ammo." );
+        debugmsg( "Tried to set charges on an item with ammo." );
         return;
     }
-    charges = num_turns;
+    charges = value;
 }
 
 detached_ptr<item> item::use_charges( detached_ptr<item> &&self, const itype_id &what, int &qty,

src/item.h
@@ -723,9 +723,15 @@ class item : public location_visitable<item>, public game_object<item>
         int reach_range( const Character &guy ) const;
 
         /**
-         * Sets time until activation for an item that will self-activate in the future.
+         * Sets item charges.
          **/
-        void set_countdown( int num_turns );
+        void set_charges( int value );
+
+        /**
+         * Sets time until countdown_action will trigger, maybe re-set for continuous activation.
+         * A negative value will set the timer to the item's default countdown_interval
+         **/
+        void set_countdown( int value );
 
         /**
          * Consumes specified charges (or fewer) from this and any contained items
@@ -1304,8 +1310,8 @@ class item : public location_visitable<item>, public game_object<item>
 
         /** Returns energy of one charge of this item as fuel for an engine. */
         float fuel_energy() const;
-        /** Returns the string of the id of the terrain that pumps this fuel, if any. */
-        std::string fuel_pump_terrain() const;
+        /** Returns the id of the terrain that pumps this fuel, if any. */
+        ter_id fuel_pump_terrain() const;
         bool has_explosion_data() const;
         struct fuel_explosion get_explosion_data();
         float get_kcal_mult() const;

src/iuse_actor.cpp
@@ -334,7 +334,7 @@ int iuse_transform::use( player &p, item &it, bool t, const tripoint &pos ) cons
             } else if( !it.ammo_current().is_null() ) {
                 it.ammo_set( it.ammo_current(), qty );
             } else {
-                it.set_countdown( qty );
+                it.set_charges( qty );
             }
             // If we're setting target charges then check for integral mods too.
             if( it.type->gun ) {

src/ammo.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "catalua_type_operators.h"
+
 #include <string>
 #include <utility>
 
@@ -27,5 +29,8 @@ class ammunition_type
         static void load_ammunition_type( const JsonObject &jsobj );
         static void reset();
         static void check_consistency();
+
+    public:
+        LUA_TYPE_OPS( ammunition_type, default_ammotype_ );
 };
 

src/ammo_effect.h
@@ -44,11 +44,13 @@ struct ammo_effect {
 
     public:
         // Used by generic_factory
-        string_id<ammo_effect> id;
+        ammo_effect_str_id id;
         bool was_loaded = false;
 
     public:
         static size_t count();
+
+        LUA_TYPE_OPS( ammo_effect, id );
 };
 
 namespace ammo_effects

src/armor_layers.cpp
@@ -68,7 +68,7 @@ struct item_penalties {
 
 // Figure out encumbrance penalties this clothing is involved in
 item_penalties get_item_penalties( const location_vector<item>::const_iterator &worn_item_it,
-                                   const Character &c, const bodypart_id &_bp )
+                                   const Character &c, const bodypart_id & )
 {
     item *const &worn_item = *worn_item_it;
     layer_level layer = worn_item->get_layer();

commit 7eab11cb9bfa9efdb926370ba16c90f72356e7ee

fix: resolve item_counter / countdown issues introduced in #7365 (#7613)
* * wrap item_counter in getter/setters
* place set_counter calls after activate calls

* update lua docs

lua_annotations.lua
@@ -1100,6 +1100,7 @@ function IslotWheel.new() end
 ---@field erase_var fun(self: Item, arg2: string) @Erase variable
 ---@field get_category_id fun(self: Item): string @Gets the category id this item is in
 ---@field get_comestible_fun fun(self: Item): integer
+---@field get_counter fun(self: Item): integer
 ---@field get_kcal fun(self: Item): integer
 ---@field get_mtype fun(self: Item): MonsterTypeId @Almost for a corpse.
 ---@field get_owner fun(self: Item): FactionId @Gets the faction id that owns this item
@@ -1183,7 +1184,7 @@ function IslotWheel.new() end
 ---@field remaining_capacity_for_id fun(self: Item, arg2: ItypeId, arg3: boolean): integer @Gets the remaining space available for a type of liquid
 ---@field remove_technique fun(self: Item, arg2: MartialArtsTechniqueId) @Removes the additional technique. Doesn't affect originial techniques.
 ---@field set_charges fun(self: Item, arg2: integer)
----@field set_countdown fun(self: Item, arg2: integer)
+---@field set_counter fun(self: Item, arg2: integer)
 ---@field set_flag fun(self: Item, arg2: JsonFlagId)
 ---@field set_flag_recursive fun(self: Item, arg2: JsonFlagId)
 ---@field set_owner fun(self: Item, arg2: Character) @Sets the ownership of this item to a character

src/activity_handlers.cpp
@@ -3765,7 +3765,7 @@ void activity_handlers::craft_do_turn( player_activity *act, player *p )
 
     // item_counter represents the percent progress relative to the base batch time
     // stored precise to 5 decimal places ( e.g. 67.32 percent would be stored as 6'732'000 )
-    const int old_counter = craft->item_counter;
+    const int old_counter = craft->get_counter();
 
     // Base moves for batch size with no speed modifier or assistants
     // Must ensure >= 1 so we don't divide by 0;
@@ -3778,37 +3778,37 @@ void activity_handlers::craft_do_turn( player_activity *act, player *p )
                                   ? p->get_moves() * base_total_moves / cur_total_moves
                                   : 0;
     // Current progress in moves
-    const double current_progress = craft->item_counter * base_total_moves / 10'000'000.0 +
-                                    delta_progress;
+    const double current_progress = old_counter * base_total_moves / 10'000'000.0 + delta_progress;
     // Current progress as a percent of base_total_moves to 2 decimal places
-    craft->item_counter = std::round( current_progress / base_total_moves * 10'000'000.0 );
+    const auto new_counter_f = current_progress / base_total_moves * 10'000'000.0;
+    // This is to ensure we don't over count skill steps
+    const auto new_counter = std::min( static_cast<int>( std::round( new_counter_f ) ), 10'000'000 );
+    craft->set_counter( new_counter );
+
     p->set_moves( 0 );
 
-    // This is to ensure we don't over count skill steps
-    craft->item_counter = std::min( craft->item_counter, 10'000'000 );
-
     // Skill and tools are gained/consumed after every 5% progress
-    int five_percent_steps = craft->item_counter / 500'000 - old_counter / 500'000;
+    int five_percent_steps = craft->get_counter() / 500'000 - old_counter / 500'000;
     if( five_percent_steps > 0 ) {
         p->craft_skill_gain( *craft, five_percent_steps );
     }
 
     // Unlike skill, tools are consumed once at the start and should not be consumed at the end
-    if( craft->item_counter >= 10'000'000 ) {
+    if( craft->get_counter() >= 10'000'000 ) {
         --five_percent_steps;
     }
 
     if( five_percent_steps > 0 ) {
         if( !p->craft_consume_tools( *craft, five_percent_steps, false ) ) {
             // So we don't skip over any tool comsuption
-            craft->item_counter -= craft->item_counter % 500000 + 1;
+            craft->set_counter( craft->get_counter() - ( craft->get_counter() % 500'000 + 1 ) );
             p->cancel_activity();
             return;
         }
     }
 
     // if item_counter has reached 100% or more
-    if( craft->item_counter >= 10'000'000 ) {
+    if( craft->get_counter() >= 10'000'000 ) {
         //TODO!: CHEEKY check
         item *craft_copy = craft;
         p->cancel_activity();
@@ -3819,7 +3819,7 @@ void activity_handlers::craft_do_turn( player_activity *act, player *p )
                 p->last_craft->execute( bench_pos );
             }
         }
-    } else if( craft->item_counter >= craft->get_next_failure_point() ) {
+    } else if( craft->get_counter() >= craft->get_next_failure_point() ) {
         bool destroy = craft->handle_craft_failure( *p );
         // If the craft needs to be destroyed, do it and stop crafting.
         if( destroy ) {

src/catalua_bindings_item.cpp
@@ -139,7 +139,9 @@ void reg_item( sol::state &lua )
         SET_FX( activate );
         SET_FX( deactivate );
         SET_FX( set_charges );
-        SET_FX( set_countdown );
+
+        SET_FX( set_counter );
+        SET_FX( get_counter );
 
         DOC( "Is this item an effective melee weapon for the given damage type?" );
         luna::set_fx( ut, "is_melee", sol::resolve<bool( damage_type ) const>

src/iexamine.cpp
@@ -5632,8 +5632,8 @@ static void mill_activate( player &p, const tripoint &examp )
     }
     here.furn_set( examp, next_mill_type );
     detached_ptr<item> result = item::spawn( "fake_milling_item", calendar::turn );
-    result->item_counter = to_turns<int>( milling_time );
     result->activate();
+    result->set_counter( to_turns<int>( milling_time ) );
     here.add_item( examp, std::move( result ) );
     add_msg( _( "You remove the brake on the millstone and it slowly starts to turn." ) );
 }
@@ -5737,8 +5737,8 @@ static void smoker_activate( player &p, const tripoint &examp )
         charcoal->charges -= char_charges;
     }
     detached_ptr<item> result = item::spawn( "fake_smoke_plume", calendar::turn );
-    result->item_counter = to_turns<int>( 6_hours );
     result->activate();
+    result->set_counter( to_turns<int>( 6_hours ) );
     here.add_item( examp, std::move( result ) );
     add_msg( _( "You light a small fire under the rack and it starts to smoke." ) );
 }
@@ -6147,7 +6147,7 @@ void iexamine::quern_examine( player &p, const tripoint &examp )
             f_volume += it->volume();
         }
         if( active && it->typeId() == itype_fake_milling_item ) {
-            time_left = time_duration::from_turns( it->item_counter );
+            time_left = time_duration::from_turns( it->get_counter() );
         }
     }
 
@@ -6315,7 +6315,7 @@ void iexamine::smoker_options( player &p, const tripoint &examp )
             f_volume += it->volume();
         }
         if( active && it->typeId() == itype_fake_smoke_plume ) {
-            time_left = time_duration::from_turns( it->item_counter );
+            time_left = time_duration::from_turns( it->get_counter() );
             hours_left = to_hours<int>( time_left );
             minutes_left = to_minutes<int>( time_left ) + 1;
         }

src/item.cpp
@@ -610,7 +610,9 @@ void item::activate()
         return; // no-op
     }
 
-    set_countdown( -1 );
+    if( type->countdown_interval > 0 ) {
+        set_counter( type->countdown_interval );
+    }
 
     active = true;
 
@@ -9326,14 +9328,14 @@ detached_ptr<item> item::fill_with( detached_ptr<item> &&liquid, int amount )
     return detached_ptr<item>();
 }
 
-void item::set_countdown( int value )
+void item::set_counter( const int value )
 {
-    if( value < 0 ) {
-        item_counter = type->countdown_interval;
-        return;
-    }
-
     item_counter = value;
+}
+
+int item::get_counter() const
+{
+    return item_counter;
 }
 
 void item::set_charges( int value )
@@ -9922,7 +9924,7 @@ detached_ptr<item> item::process_fake_mill( detached_ptr<item> &&self, player * 
     map &here = get_map();
     if( here.furn( pos ) != furn_str_id( "f_wind_mill_active" ) &&
         here.furn( pos ) != furn_str_id( "f_water_mill_active" ) ) {
-        self->item_counter = 0;
+        self->set_counter( 0 );
         return detached_ptr<item>(); //destroy fake mill
     }
     if( self->age() >= 6_hours || self->item_counter == 0 ) {
@@ -9943,7 +9945,7 @@ detached_ptr<item> item::process_fake_smoke( detached_ptr<item> &&self, player *
     map &here = get_map();
     if( here.furn( pos ) != furn_str_id( "f_smoking_rack_active" ) &&
         here.furn( pos ) != furn_str_id( "f_metal_smoking_rack_active" ) ) {
-        self->item_counter = 0;
+        self->set_counter( 0 );
         return detached_ptr<item>(); //destroy fake smoke
     }
 

src/item.h
@@ -728,10 +728,11 @@ class item : public location_visitable<item>, public game_object<item>
         void set_charges( int value );
 
         /**
-         * Sets time until countdown_action will trigger, maybe re-set for continuous activation.
-         * A negative value will set the timer to the item's default countdown_interval
+         * Sets a generic counter to be used with item flags
+         * Also used by countdown activation and crafting progress
          **/
-        void set_countdown( int value );
+        void set_counter( int value );
+        int get_counter() const;
 
         /**
          * Consumes specified charges (or fewer) from this and any contained items
@@ -2431,7 +2432,7 @@ class item : public location_visitable<item>, public game_object<item>
         int frequency = 0;         // Radio frequency
         snippet_id snip_id = snippet_id::NULL_ID(); // Associated dynamic text snippet id.
         int irradiation = 0;       // Tracks radiation dosage.
-        int item_counter = 0;      // generic counter to be used with item flags
+
         int mission_id = -1;       // Refers to a mission in game's master list
         int player_id = -1;        // Only give a mission to the right player!
 
@@ -2441,6 +2442,8 @@ class item : public location_visitable<item>, public game_object<item>
         bool encumbrance_update_ = false;
 
     private:
+        // generic counter to be used with item flags
+        int item_counter = 0;
         /**
          * Accumulated rot, expressed as time the item has been in standard temperature.
          * It is compared to shelf life (@ref islot_comestible::spoils) to decide if

src/iuse.cpp
@@ -5103,7 +5103,7 @@ int iuse::towel_common( player *p, item *it, bool t )
 
         towelUsed = true;
         if( it ) {
-            it->item_counter = to_turns<int>( 30_minutes );
+            it->set_counter( to_turns<int>( 30_minutes ) );
         }
 
         // default message

src/iuse_actor.cpp
@@ -362,8 +362,8 @@ int iuse_transform::use( player &p, item &it, bool t, const tripoint &pos ) cons
     p.inv_update_invlet_cache_with_item( it );
     // Update luminosity as object is "added"
     get_map().update_lum( it, true );
-    it.item_counter = countdown > 0 ? countdown : it.type->countdown_interval;
-    ( active || it.item_counter ) ? it.activate() : it.deactivate();
+    ( active || countdown ) ? it.activate() : it.deactivate();
+    it.set_counter( countdown > 0 ? countdown : it.type->countdown_interval );
     // Check for gaining or losing night vision, eye encumbrance effects, clairvoyance from transforming relics, etc.
     p.recalc_sight_limits();
 
@@ -519,8 +519,8 @@ int countdown_actor::use( player &p, item &it, bool t, const tripoint &pos ) con
         p.add_msg_if_player( m_neutral, _( message ), it.tname() );
     }
 
-    it.item_counter = interval > 0 ? interval : it.type->countdown_interval;
     it.activate();
+    it.set_counter( interval > 0 ? interval : it.type->countdown_interval );
     return 0;
 }
 
@@ -929,8 +929,9 @@ int consume_drug_iuse::use( player &p, item &it, bool, const tripoint & ) const
         cig = item::spawn( lit_item, calendar::turn );
         time_duration converted_time = time_duration::from_minutes( smoking_duration );
 
-        cig->item_counter = to_turns<int>( converted_time );
         cig->activate();
+        cig->set_counter( to_turns<int>( converted_time ) );
+
         p.i_add( std::move( cig ) );
     }
 
@@ -6187,7 +6188,7 @@ auto iuse_flowerpot_plant::on_use_harvest( player &p, item &i, const tripoint & 
 
 auto iuse_flowerpot_plant::on_tick( player &, item &i, const tripoint & ) const -> int
 {
-    if( i.item_counter != 0 ) {
+    if( i.get_counter() != 0 ) {
         return 0;
     }
 
@@ -6200,7 +6201,7 @@ void iuse_flowerpot_plant::update( item &i ) const
     const auto info = get_info( i );
     if( !info.seed_id.is_valid() ) {
         clear_growing_plant( i );
-        i.item_counter = 0;
+        i.set_counter( 0 );
         i.convert( stages[0] );
         i.erase_var( "item_label" );
         i.deactivate();
@@ -6211,16 +6212,16 @@ void iuse_flowerpot_plant::update( item &i ) const
     i.set_var( "item_label", string_format( "%s (%s)", stages[0]->nname( 1 ), info.plant_name() ) );
     switch( info.stage() ) {
         case 0:
-            i.item_counter = 0;
             i.deactivate();
+            i.set_counter( 0 );
             break;
         case 4:
-            i.item_counter = 0;
             i.deactivate();
+            i.set_counter( 0 );
             break;
         default:
-            i.item_counter = to_turns<int>( std::min( info.remaining_time(), 1_hours ) );
             i.activate();
+            i.set_counter( to_turns<int>( std::min( info.remaining_time(), 1_hours ) ) );
             break;
     }
 }

src/newcharacter.cpp
@@ -610,7 +610,7 @@ bool avatar::create( character_type type, const std::string &tempname )
     for( detached_ptr<item> &it : prof_items ) {
         if( it->has_flag( STATIC( flag_id( "WET" ) ) ) ) {
             it->activate();
-            it->item_counter = 450; // Give it some time to dry off
+            it->set_counter( 450 ); // Give it some time to dry off
         }
         if( it->is_book() ) {
             items_identified.insert( it->typeId() );

src/player_activity.cpp
@@ -224,7 +224,7 @@ static std::string craft_progress_message( const avatar &u, const player_activit
     const float total_mult = light_mult * bench_mult * morale_mult * assist_mult * speed_mult *
                              mutation_mult * game_opt_mult;
 
-    const double remaining_percentage = 1.0 - craft->item_counter / 10'000'000.0;
+    const double remaining_percentage = 1.0 - craft->get_counter() / 10'000'000.0;
     int remaining_turns = remaining_percentage * base_total_moves / 100 / std::max( 0.01f, total_mult );
     std::string time_desc = string_format( _( "Time left: %s" ),
                                            to_string( time_duration::from_turns( remaining_turns ) ) );