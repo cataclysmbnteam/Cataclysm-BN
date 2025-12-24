cataclysmbnteam/Cataclysm-BN

commit 7adbf51caca54458fdb4cfcba458f73cec1f0c21

feat: Add `DROPPER` Vehicle Part flag (#7647)
* distracted

* IT FINALLY WORKS

* Docs

* Activation & Single Items

* Add bomber controls

* THAT WASN'T SUPPOST TO BE GIVEN TO CARBON BOXES

* resolve suggestion

* add all the keybindings

* NO TREE

* stupid item positions

* I HATE POINTERS WITH A PASSION

src/vehicle.cpp
@@ -1,4 +1,5 @@
 #include "vehicle.h"
+#include "detached_ptr.h"
 #include "units_mass.h"
 #include "vehicle_part.h" // IWYU pragma: associated
 #include "vpart_position.h" // IWYU pragma: associated
@@ -70,7 +71,7 @@
 #include "units_utility.h"
 #include "veh_type.h"
 #include "weather.h"
-
+#include "ui.h"
 /*
  * Speed up all those if ( blarg == "structure" ) statements that are used everywhere;
  *   assemble "structure" once here instead of repeatedly later.
@@ -299,6 +300,7 @@ void vehicle::copy_static_from( const vehicle &source )
     propellers = source.propellers;
     wings = source.wings;
     balloons = source.balloons;
+    droppers = source.droppers;
     rail_wheelcache = source.rail_wheelcache;
     steering = source.steering;
     speciality = source.speciality;
@@ -6273,6 +6275,7 @@ void vehicle::refresh()
     rotors.clear();
     wings.clear();
     propellers.clear();
+    droppers.clear();
     balloons.clear();
     steering.clear();
     speciality.clear();
@@ -6348,6 +6351,10 @@ void vehicle::refresh()
         }
         if( vpi.has_flag( VPFLAG_BALLOON ) ) {
             balloons.push_back( p );
+        }
+
+        if( vpi.has_flag( VPFLAG_DROPPER ) ) {
+            droppers.push_back( p );
         }
         if( vpi.has_flag( "WIND_TURBINE" ) ) {
             wind_turbines.push_back( p );
@@ -7903,4 +7910,114 @@ int vehicle::get_part_id_hack( int id )
     }
     debugmsg( "Could not find part id via hack id" );
     return -1;
+}
+
+// The mythical invokation to summon the cargo part on the same tile
+vehicle_part *vehicle::get_cargo_part( vehicle_part *part )
+{
+    vehicle_part *vp;
+    int vpr = part_with_feature( part->mount, "CARGO", false );
+    if( vpr != -1 ) {
+        vp = &parts[ vpr ];
+    }
+    return vp;
+}
+bool vehicle::has_item_stored( vehicle_part *part )
+{
+    vehicle_part *vp = get_cargo_part( part );
+    if( vp ) {
+        return stored_volume( index_of_part( vp ) ) > 0_ml;
+    }
+    return false;
+}
+
+void vehicle::item_dropper_drop( std::vector<vehicle_part *> droppers, bool single )
+{
+    if( single ) {
+        vehicle_part *part = get_cargo_part( droppers[0] );
+        std::vector<std::string> option_names;
+        std::vector<item *> options;
+        for( item *it : part->items ) {
+            option_names.push_back( it->display_name() );
+            options.push_back( it );
+        }
+        const int idx = uilist( _( "Drop which item?" ), option_names );
+        if( idx < 0 ) {
+            return;
+        }
+        map &here = get_map();
+        tripoint pos = global_part_pos3( index_of_part( part ) );
+        while( here.has_flag_ter_or_furn( TFLAG_NO_FLOOR, pos ) ) {
+            pos.z -= 1;
+        }
+        item *dropper = options[idx];
+        if( dropper->get_use( "transform" ) ) {
+            g->u.invoke_item( dropper, "transform" );
+        }
+        g->m.add_item_or_charges( pos, part->remove_item( *dropper ) );
+    } else {
+        for( vehicle_part *d : droppers ) {
+            vehicle_part *part = get_cargo_part( d );
+            map &here = get_map();
+            if( part ) {
+                tripoint pos = global_part_pos3( index_of_part( part ) );
+                while( here.has_flag_ter_or_furn( TFLAG_NO_FLOOR, pos ) ) {
+                    pos.z -= 1;
+                }
+                // DANGER: DO NOT PUT THIS IN THE FOR LOOP
+                const std::vector<item *> items = part->get_items();
+                for( item *it : items ) {
+                    if( it->get_use( "transform" ) ) {
+                        g->u.invoke_item( it, "transform" );
+                    }
+                    g->m.add_item_or_charges( pos, part->remove_item( *it ) );
+                }
+            }
+        }
+    }
+}
+
+void vehicle::item_dropper_drop_single( bool single )
+{
+    std::vector<std::string> option_names;
+    std::vector<vehicle_part *> options;
+
+    // Find all droppers that are loaded
+    for( int idx : droppers ) {
+        vehicle_part *d = &parts[ idx ];
+        if( has_item_stored( d ) ) {
+            option_names.push_back( d->name() );
+            options.push_back( d );
+        }
+    }
+
+    // Select one
+    if( options.empty() ) {
+        add_msg( m_warning, _( "None of the droppers are loaded." ) );
+        return;
+    }
+    const int idx = uilist( _( "Drop from which dropper?" ), option_names );
+    if( idx < 0 ) {
+        return;
+    }
+    vehicle_part *dropper = options[idx];
+
+    std::vector<vehicle_part *> droppers;
+    droppers.push_back( dropper );
+    item_dropper_drop( droppers, single );
+}
+
+void vehicle::item_dropper_drop_all( )
+{
+    std::vector<vehicle_part *> ret;
+
+    // Find all droppers that are loaded
+    for( int idx : droppers ) {
+        vehicle_part *d = &parts[ idx ];
+        if( has_item_stored( d ) ) {
+            ret.push_back( d );
+        }
+    }
+
+    item_dropper_drop( ret, false );
 }

src/veh_type.cpp
@@ -111,7 +111,8 @@ static const std::unordered_map<std::string, vpart_bitflags> vpart_bitflag_map =
     { "WING", VPFLAG_WING },
     { "PROPELLER", VPFLAG_PROPELLER },
     { "EXTENDABLE", VPFLAG_EXTENDABLE },
-    { "NOFIELDS", VPFLAG_NOFIELDS }
+    { "NOFIELDS", VPFLAG_NOFIELDS },
+    { "DROPPER", VPFLAG_DROPPER }
 };
 
 static const std::vector<std::pair<std::string, int>> standard_terrain_mod = {{

src/veh_type.h
@@ -76,6 +76,7 @@ enum vpart_bitflags : int {
     VPFLAG_PROPELLER,
     VPFLAG_EXTENDABLE,
     VPFLAG_NOFIELDS,
+    VPFLAG_DROPPER,
 
     NUM_VPFLAGS
 };

src/vehicle.h
@@ -1393,6 +1393,20 @@ class vehicle
         // Be aware this will not consider damage to more outside parts such as inner parts protected by an outer wall, only armor effects are considered
         unsigned int hits_to_destroy( int p, int dmg, damage_type type ) const;
 
+        /**
+         * @name Vehicle Droppers
+         *
+         *@{*/
+
+        vehicle_part *get_cargo_part( vehicle_part *part );
+
+        bool has_item_stored( vehicle_part *part );
+
+        void item_dropper_drop( std::vector<vehicle_part *> droppers, bool single );
+
+        void item_dropper_drop_all();
+
+        void item_dropper_drop_single( bool single );
     private:
         /*
          * Find all turrets that are ready to fire.
@@ -1608,11 +1622,12 @@ class vehicle
         std::vector<int> loose_parts;      // List of UNMOUNT_ON_MOVE parts
         std::vector<int> wheelcache;       // List of wheels
         std::vector<int> rotors;           // List of rotors
-        std::vector<int> balloons;         // List of repulsors
+        std::vector<int> balloons;         // List of balloons
         std::vector<int> wings;            // List of wings
         std::vector<int> propellers;       // List of propellerrs
         std::vector<int> rail_wheelcache;  // List of rail wheels
         std::vector<int> steering;         // List of STEERABLE parts
+        std::vector<int> droppers;         // List of droppers
         // List of parts that will not be on a vehicle very often, or which only one will be present
         std::vector<int> speciality;
         std::vector<int> floating;         // List of parts that provide buoyancy to boats

src/vehicle_use.cpp
@@ -875,6 +875,27 @@ void vehicle::use_controls( const tripoint &pos )
 
     }
 
+    if( has_part( "DROPPER" ) ) {
+        std::vector<vehicle_part *> droppers;
+        for( auto &p : parts ) {
+            if( p.has_flag( VPFLAG_DROPPER ) && !p.removed ) {
+                droppers.push_back( &p );
+            }
+        }
+        if( !droppers.empty() ) {
+            options.emplace_back( _( "Activate all item droppers (Drop Everything)" ),
+                                  keybind( "DROPPER_ALL" ) );
+            actions.emplace_back( [&] { item_dropper_drop_all(); refresh(); } );
+
+            options.emplace_back( _( "Activate one item dropper (Drop Everything)" ),
+                                  keybind( "DROPPER_SINGLE_ALL" ) );
+            actions.emplace_back( [&] { item_dropper_drop_single( false ); refresh(); } );
+
+            options.emplace_back( _( "Activate one item dropper (Drop One Thing)" ),
+                                  keybind( "DROPPER_SINGLE" ) );
+            actions.emplace_back( [&] { item_dropper_drop_single( true ); refresh(); } );
+        }
+    }
     uilist menu;
     menu.text = _( "Vehicle controls" );
     menu.entries = options;

commit 6031ca2ec2115508e71883f1c3ebdbb77575c712

build: fix build warnings
regression introduced in https://github.com/cataclysmbn/cataclysm-BN/pull/7647

src/vehicle.cpp
@@ -7915,7 +7915,7 @@ int vehicle::get_part_id_hack( int id )
 // The mythical invokation to summon the cargo part on the same tile
 vehicle_part *vehicle::get_cargo_part( vehicle_part *part )
 {
-    vehicle_part *vp;
+    vehicle_part *vp = nullptr;
     int vpr = part_with_feature( part->mount, "CARGO", false );
     if( vpr != -1 ) {
         vp = &parts[ vpr ];